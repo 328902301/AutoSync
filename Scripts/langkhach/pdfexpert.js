df8b0b841963afa700479858c01dd076276fcb79
Lãng Khách <langkhach270389@users.noreply.github.com>
2020-11-07 10:23:32 +0700

    update pdfexpert documents

--- Scripts/langkhach/pdfexpert.js
+++ Scripts/langkhach/pdfexpert.js
@@ -1,12 +1,41 @@
 var obj= {
-  "originalTransactionId": "20000618444996",
-  "subscriptionState": "trial",
-  "isInGracePeriod": false,
+  "productId": "com.readdle.PDFExpert5.subscription.year50_pe6",
+  "subscriptionExpirationIntent": "userCancelled",
+  "receiptStatus": "ok",
   "subscriptionExpirationDate": "13:15 03/11/2099",
-  "subscriptionAutoRenewStatus": "autoRenewOn",
-  "isEligibleForIntroPeriod": false,
   "isPDFExpert6User": false,
-  "subscriptionReceiptId": "1572178404000"
+  "inAppStates": [
+    {
+      "productId": "com.readdle.PDFExpert5.subscription.year50_pe6",
+      "subscriptionExpirationIntent": "userCancelled",
+      "receiptStatus": "ok",
+      "subscriptionExpirationDate": "13:15 03/11/2099",
+      "isEligibleForIntroPeriod": false,
+      "originalTransactionId": "20000618444996",
+      "productName": "subscription",
+      "isInBillingRetryPeriod": false,
+      "type": "subscription",
+      "subscriptionState": "active",
+      "subscriptionAutoRenewStatus": "autoRenewOff",
+      "isInGracePeriod": false
+    }
+  ],
+  "isEligibleForIntroPeriod": false,
+  "originalTransactionId": "20000618444996",
+  "isEligibleFor": [
+    "winback"
+  ],
+  "isInBillingRetryPeriod": false,
+  "type": "subscription",
+  "inAppPurchased": [
+    "com.readdle.PDFExpert5.subscription.year50_pe6"
+  ],
+  "bundleId": "com.readdle.PDFExpert5",
+  "receiptId": 1572178404000,
+  "chargingPlatform": "iOS AppStore",
+  "subscriptionState": "active",
+  "subscriptionAutoRenewStatus": "autoRenewOff",
+  "isInGracePeriod": false
 };
 
 $done({body: JSON.stringify(obj)});

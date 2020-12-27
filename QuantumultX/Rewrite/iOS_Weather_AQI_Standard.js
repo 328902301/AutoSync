5ac6f38bbb591ffe365a8b37b4e4a3b3feadb31e
2K1BK <lvlinru@qq.com>
2020-12-28 04:31:42 +0800

    Update iOS_Weather_AQI_Standard.js

--- QuantumultX/Rewrite/iOS_Weather_AQI_Standard.js
+++ QuantumultX/Rewrite/iOS_Weather_AQI_Standard.js
@@ -1,6 +1,6 @@
 // Developed by Hackl0us (https://github.com/hackl0us)
 
-// STEP 1: 前往 https://aqicn.org/data-platform/token/ 注册账户，将申请的 API Token 填入 ''
+// STEP 1: 前往 https://aqicn.org/data-platform/token/ 注册账户，将申请的 API Token 填入 = 'https://aqicn.org/data-platform/token-confirm/fa281bbe-4ba1-44db-80ea-0e5d5131e347'
 
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*

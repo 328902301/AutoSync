5e0963c777395a8543b111cccd638ad7ea1b134f
2K1BK <lvlinru@qq.com>
2020-12-28 04:36:38 +0800

    Update iOS_Weather_AQI_Standard.js

--- QuantumultX/Rewrite/iOS_Weather_AQI_Standard.js
+++ QuantumultX/Rewrite/iOS_Weather_AQI_Standard.js
@@ -1,12 +1,12 @@
 #Developed by Hackl0us (https://github.com/hackl0us)
 #STEP 1: 前往 https://aqicn.org/data-platform/token/ 注册账户，将申请的 API Token 填入 = 'https://aqicn.org/data-platform/token-confirm/fa281bbe-4ba1-44db-80ea-0e5d5131e347'
 #STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
-
-	[Script]
-	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?include=, requires-body=true, script-path=/path/to/https://raw.githubusercontent.com/2k1bk/Rules/1/QuantumultX/Rewrite/iOS_Weather_AQI_Standard.js
-
-	[MITM]
-	hostname = weather-data.apple.com
+#
+#	[Script]
+#	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?include=, requires-body=true, script-path=/path/to/https://raw.githubusercontent.com/2k1bk/Rules/1/QuantumultX/Rewrite/iOS_Weather_AQI_Standard.js
+#
+#	[MITM]
+#	hostname = weather-data.apple.com
 
 
 const AirQualityStandard = {

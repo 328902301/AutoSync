8f15478da3777c72fc78e430e8ecd41ddff2f5ff
byour13 <78316297+byour13@users.noreply.github.com>
2021-06-03 13:33:16 +0800

    Delete wps.js

--- wps.js
+++
@@ -1,3 +0,0 @@
-body =
-$response.body.replace(/deduct_time":\d+/g,'deduct_time":1905602340').replace(/price":\d+/g,'price":0').replace(/expire_time":\d+/g,'expire_time":1905688740').replace(/super":\d/g,'super":1').replace(/docer":\d/g,'docer":1').replace(/wps":\d/g,'wps":1').replace(/validate":\d/g,'validate":999').replace(/total":\d+/g,'total":1000135966720')
-$done({body});

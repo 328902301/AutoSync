6c782566932a7d427e7f9b886a785bdb69f13974
byour13 <78316297+byour13@users.noreply.github.com>
2021-06-04 08:25:10 +0800

    Update BaiduCloud.js

--- BaiduCloud.js
+++ BaiduCloud.js
@@ -1,11 +1,7 @@
-/*
-Baidu netdisc unlocks online video play speed.
-Key data from thor filter
-Surge4.0:
-http-response https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
 
+/*
 QX1.0.0:
-https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
+https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user* url script-response-body https://raw.githubusercontent.com/byour13/Script/main/BaiduCloud.js
 
 MITM = pan.baidu.com
 */

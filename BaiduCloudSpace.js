28eec6983c7da3d09aa423b80439b58572f570fa
byour13 <78316297+byour13@users.noreply.github.com>
2021-06-19 16:02:13 +0800

    Update BaiduCloudSpace.js

--- BaiduCloudSpace.js
+++ BaiduCloudSpace.js
@@ -10,7 +10,7 @@ var obj = JSON.parse($response.body);
 obj = {
   "errno": 0,
   "newno": "",
-  "request_id": 2625847181883700000,
+  "request_id": 2625847181883792529,
   "reward": {
     "total": 0,
     "used": 0

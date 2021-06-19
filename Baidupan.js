4bc0d17bbd7f2ce33ff3c6daa5fb6c571d3bafbc
byour13 <78316297+byour13@users.noreply.github.com>
2021-06-19 16:16:00 +0800

    Update Baidupan.js

--- Baidupan.js
+++ Baidupan.js
@@ -9,7 +9,7 @@ var obj = JSON.parse($response.body);
 obj = {
   "errno": 0,
   "used": 384777965185,
-  "total": 394777965185,
+  "total": 11107027820544,
   "request_id": 7501873289383874371
 };
 

4fdd725ba3a58ce8ca5bc98cbb27b40118e41525
byour13 <78316297+byour13@users.noreply.github.com>
2021-06-24 17:54:46 +0800

    Update wukongplay.js

--- wukongplay.js
+++ wukongplay.js
@@ -1,8 +1,10 @@
 /*
 QX：
-^https:\/\/iosv2\.cjapi\.papa21\.com\/user\/ios_store_pay_edition\/members_login\/check_members_identity_v2 url script-response-body https://raw.githubusercontent.com/byour13/Script/main/wukongplay.js
+[rewrite_local]
+^http:\/\/iosv2\.cjapi\.papa21\.com\/user\/ios_store_pay_edition\/members_login\/check_members_identity_v2 url script-response-body https://raw.githubusercontent.com/byour13/Script/main/wukongplay.js
 
-QX MITM = iosv2.cjapi.papa21.com
+[MITM]
+hostname = iosv2.cjapi.papa21.com
 by byour13
 */
 

bf337d70c77ae52fb33774fb5a3a15cf3aaa8eed
byour13 <78316297+byour13@users.noreply.github.com>
2021-06-26 09:56:07 +0800

    Update baiduzip.js

--- baiduzip.js
+++ baiduzip.js
@@ -1,8 +1,8 @@
 /*
 QX：
-^https:\/\/viva\.v21xy\.com\/api\/rest\/u\/vip url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/vivavideo.js
+^https:\/\/pan\.baidu\.com\/api\/user\/getinfo* url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/vivavideo.js
 
-QX MITM = viva.v21xy.com
+QX MITM = pan.baidu.com
 */
 
 var obj = JSON.parse($response.body);

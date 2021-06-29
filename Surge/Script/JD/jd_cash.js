b911444f34da4bdc18f7419632a3856c6b3a8d29
James <zhuzhijun1983@gmail.com>
2021-06-29 11:10:47 +0800

    Update jd_cash.js

--- Surge/Script/JD/jd_cash.js
+++ Surge/Script/JD/jd_cash.js
@@ -31,8 +31,8 @@ let helpAuthor = true;
 const randomCount = $.isNode() ? 5 : 5;
 let cash_exchange = false;//是否消耗2元红包兑换200京豆，默认否
 const inviteCodes = [
-      `IRM2b-y0YPgh7GrSwnQW1w`,
       `YU5rML3dLA`,
+      `IRM2b-y0YPgh7GrSwnQW1w`,
 ]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {

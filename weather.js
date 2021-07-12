10b4ffa3676708c7f98c4a4e6ec4c25b6feaf44c
byour13 <78316297+byour13@users.noreply.github.com>
2021-07-12 14:22:38 +0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
 /*
 QX：
-^https:\/\/viva\.v21xy\.com\/api\/rest\/u\/vip url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/vivavideo.js
+^https:\/\/one\.boohee\.com\/api\/v1\/users\/vip_status* url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/vivavideo.js
 
-QX MITM = viva.v21xy.com
+QX MITM = one.boohee.com
 */
 
 var obj = JSON.parse($response.body);

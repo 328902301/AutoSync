65fdd7abf28bac5d17929c43ff689489553f9ab7
byour13 <78316297+byour13@users.noreply.github.com>
2021-07-15 23:12:50 +0800

    Add files via upload

---
+++ bilibili.js
@@ -0,0 +1,25 @@
+/*
+QuantumultX:
+
+[rewrite_local]
+^https:\/\/api\.bilibili\.com\/x\/member\/v2\notice* url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wnyd.js
+
+[mitm]
+hostname = p.du.163.com
+
+***************************
+Surge4 or Loon:
+
+[Script]
+http-response ^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wnyd.js
+
+[MITM]
+hostname = api.bilibili.com
+
+**************************/
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj.data.status = 1;
+body = JSON.stringify(obj);
+$done({body});

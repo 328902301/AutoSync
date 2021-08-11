e69fe05e1ae90b48922d1706f0f67aee60ac6d38
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2021-08-11 14:09:21 +0800

    Update WeChat.js

--- Scripts/WeChat.js
+++ Scripts/WeChat.js
@@ -1,28 +1,10 @@
 /*
-微信去除公众号文章底部广告
-
-***************************
-QuantumultX:
-
-[rewrite_local]
-^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
-
-[mitm]
-hostname = mp.weixin.qq.com
-
-***************************
-Surge4 or Loon:
-
-[Script]
-http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
-
-[MITM]
-hostname = mp.weixin.qq.com
-
-**************************/
+去除微信公众号文章底部广告
+*/
 
 var obj = JSON.parse($response.body);
 obj.advertisement_num = 0;
 obj.advertisement_info = [];
 delete obj.appid;
 $done({body: JSON.stringify(obj)}); 
+

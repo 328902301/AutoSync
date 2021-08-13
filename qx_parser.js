2fa8ac4d2142ca8b4602554be3c7b64a058decd2
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2021-08-13 18:12:12 +0800

    Update qx_parser.js

--- qx_parser.js
+++ qx_parser.js
@@ -84,19 +84,6 @@
 */
 
 
-/**
-* 使用说明，
-0️⃣ 在QuantumultX 配置文件中[general] 部分，加入 
-resource_parser_url = https://github.com/RuCu6/QuanX/raw/main/qx_parser.js
-⚠️⚠️如提示"没有自定义解析器"，请长按右下角图标后点击左侧刷新按钮，更新资源，后台退出 app，直到出现解析器说明
-1️⃣ 假设原始订阅连接为: https://xxx.txt
-2️⃣ 假设你想要保留的参数为 in=tls+ss, 想要过滤的参数为 out=http+2, 请注意下面订阅链接后一定要加 ”#“ 符号
-3️⃣ 则填入 Quanx 节点引用的的总链接为  https://xxx.txt#in=tls+ss&out=http+2
-4️⃣ 填入上述链接, 并打开的资源解析器开关
-------------------------------
-*/
-
-
 //beginning 解析器正常使用，調試註釋此部分
 
 let [link0, content0, subinfo] = [$resource.link, $resource.content, $resource.info]

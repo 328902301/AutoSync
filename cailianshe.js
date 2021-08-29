4b9b1345e75d9ed450e5ffddee32f9f939148249
byour13 <78316297+byour13@users.noreply.github.com>
2021-08-29 17:57:16 +0800

    Update cailianshe.js

--- cailianshe.js
+++ cailianshe.js
@@ -1,4 +1,11 @@
-
-body =
+/*
+有待验证
+Loon：
+http-response ^https:\/\/i\.cls\.cn\/featured\/v2\/home\/recommend\/article* requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/byour13/Script/main/1Blocker.js
+QX：
+^https:\/\/i\.cls\.cn\/featured\/v2\/home\/recommend\/article* url script-response-body https://raw.githubusercontent.com/byour13/Script/main/1Blocker.js
+Loon&QX = i.cls.cn
+*/
+var body =
 $response.body.replace(/\u0075\u006e\u006c\u006f\u0063\u006b\u0022\u003a\u005c\u0064/g,'\u0075\u006e\u006c\u006f\u0063\u006b\u0022\u003a\u0032').replace(/\u0075\u006e\u006c\u006f\u0063\u006b\u005f\u006e\u0075\u006d\u0022\u003a\u005c\u0064\u002b/g,'\u0075\u006e\u006c\u006f\u0063\u006b\u005f\u006e\u0075\u006d\u0022\u003a\u0030')
 $done({body});

ad9f2bd6f1f10b08cdbc9f146039f4a74eaaf263
byour13 <78316297+byour13@users.noreply.github.com>
2021-08-29 18:45:44 +0800

    Update cailianshe.js

--- cailianshe.js
+++ cailianshe.js
@@ -1,11 +1,3 @@
-/*
-有待验证
-Loon：
-http-response ^https:\/\/i\.cls\.cn\/featured\/v2\/home\/recommend\/article* requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/byour13/Script/main/cailianshe.js
-QX：
-^https:\/\/i\.cls\.cn\/featured\/v2\/home\/recommend\/article* url script-response-body https://raw.githubusercontent.com/byour13/Script/main/cailianshe.js
-Loon&QX = i.cls.cn
-*/
-var body =
+body =
 $response.body.replace(/unlock":\d/g,'unlock":2').replace(/unlock_num":\d+/g,'unlock_num":0')
 $done({body});

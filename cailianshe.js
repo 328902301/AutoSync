91cbc9f1557a2721291cd265f2d5e8ba89513592
byour13 <78316297+byour13@users.noreply.github.com>
2021-08-29 18:48:11 +0800

    Update cailianshe.js

--- cailianshe.js
+++ cailianshe.js
@@ -1,3 +1,3 @@
 body =
-$response.body.replace(/unlock":\d/g,'unlock":2').replace(/unlock_num":\d+/g,'unlock_num":0')
+$response.body.replace(/unlock":\d/g,'unlock":1').replace(/unlock_num":\d+/g,'unlock_num":0')
 $done({body});

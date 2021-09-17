1f793d20368e9adbfe0bd4692990f2cd716f91b6
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2021-09-18 00:19:04 +0800

    Create ad.js

---
+++ Scripts/ad.js
@@ -0,0 +1,7 @@
+^https?:\/\/youtubei\.googleapis\.com\/youtubei\/v1\/player\/ad_ url reject
+^https?:\/\/.+?\.googlevideo\.com\/ptracking\?pltype=adhost url reject-img
+^https?:\/\/.+?\.youtube\.com\/get_midroll url reject-img
+^https?:\/\/.+?\.youtube\.com\/ptracking\? url reject-img
+^https?:\/\/m\.youtube\.com\/_get_ads url reject-img
+^https?:\/\/pagead2\.googlesyndication\.com\/pagead\/ url reject-img
+^https?:\/\/s\.youtube\.com\/api\/stats\/watchtime?adformat url reject-img

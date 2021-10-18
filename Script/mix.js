db7fd34e9297d039080a5e82a3c1c81b1a365ea0
https://github.com/iEwha/Profiles.git <royiewha@gmail.com>
2021-10-18 08:36:15 +0800

    +

--- Script/mix.js
+++ Script/mix.js
@@ -3,15 +3,14 @@
 =======Quantumult X=======
 
 [rewrite_local]
-^https:\/\/bmall\.camera360\.com\/api\/mix\/recovery$ url script-response-body https://raw.githubusercontent.com/githubdulong/Script/master/mix.js
-
+^https:\/\/bmall\.camera360\.com\/api\/mix\/recovery$ url script-response-body https://raw.githubusercontent.com/iEwha/Profiles/master/Script/mix.js
 [MITM]
 hostname = bmall.camera360.com
 
 ========Surge==========
 
 [Script]
-camera360-Mix = requires-body=1,script-path= https://raw.githubusercontent.com/githubdulong/Script/master/mix.js,type=http-response,pattern= ^https:\/\/bmall\.camera360\.com\/api\/mix\/recovery$
+camera360-Mix = requires-body=1,script-path= https://raw.githubusercontent.com/iEwha/Profiles/master/Script/mix.js,type=http-response,pattern= ^https:\/\/bmall\.camera360\.com\/api\/mix\/recovery$
 
 [MITM]
 hostname = bmall.camera360.com

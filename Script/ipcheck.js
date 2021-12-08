df6f4148387718a89a3d41a113e0f441c2f30001
iEwha <81971510+iEwha@users.noreply.github.com>
2021-12-08 10:55:32 +0800

    Update ipcheck.js

--- Script/ipcheck.js
+++ Script/ipcheck.js
@@ -2,14 +2,17 @@ let url = "http://ip-api.com/json/?lang=zh-CN"
 
 $httpClient.get(url, function(error, response, data){
     let jsonData = JSON.parse(data)
+    let ip = jsonData.query
     let country = jsonData.country
     let emoji = getFlagEmoji(jsonData.countryCode)
     let city = jsonData.city
     let isp = jsonData.isp
+    
   body = {
-    title: "节点相关信息",
-    content: `IP位置: ${emoji}${country} - ${city}\n运营商: ${isp}`,
-    icon: "globe.asia.australia.fill"
+    title: "网络信息",
+    content: `IP信息: ${ip}\n运营商: ${isp}\nIP位置: ${emoji}${country} - ${city}`,
+    icon: "link.icloud",
+    'icon-color': "#0089A7"
   }
   $done(body);
 });

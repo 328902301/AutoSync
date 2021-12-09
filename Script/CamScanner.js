afb897a85dc7bde7c2e53e31466d1554f558c21e
https://github.com/iEwha/Profiles.git <royiewha@gmail.com>
2021-12-09 11:45:59 +0800

    +

--- Script/CamScanner.js
+++ Script/CamScanner.js
@@ -5,8 +5,7 @@ CamScanner 解锁部分高级特权
 Quantumult X:
 
 [rewrite_local]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
-
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/id77/QuantumultX/master/Script/camscanner.js
 [mitm]
 hostname = ap*.intsig.net
 
@@ -14,17 +13,7 @@ hostname = ap*.intsig.net
 Surge4 or Loon:
 
 [Script]
-http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
-
-[MITM]
-hostname = ap*.intsig.net
-
-***************************
-Quantumult:  
-
-[REWRITE]
-https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
-
+扫描全能王= type=http-response,pattern=https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\?, requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/id77/QuantumultX/master/Script/camscanner.js
 [MITM]
 hostname = ap*.intsig.net
 

5b370e7647fdecafeaa6861a384d8bae438a6e4e
iEwha <81971510+iEwha@users.noreply.github.com>
2021-12-24 00:25:55 +0800

    Update ipcheck.js

--- Surge/PanelScripts/ipcheck.js
+++ Surge/PanelScripts/ipcheck.js
@@ -10,7 +10,8 @@ $httpClient.get(url, function(error, response, data){
   body = {
     title: "节点信息",
     content: `IP: ${ip}\nISP: ${isp}\n位置: ${emoji}${country} - ${city}`,
-    icon: "personalhotspot"
+    icon: "personalhotspot",
+    'icon-color': "#5AC8FA"
   }
   $done(body);
 });

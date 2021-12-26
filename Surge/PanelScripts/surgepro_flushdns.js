52dcfd6b633a3af449dae131d4cd8637ada76d5d
iEwha <81971510+iEwha@users.noreply.github.com>
2021-12-26 21:19:03 +0800

    Update and rename surgepro_reloadprofile.js to surgepro_flushdns.js

--- Surge/PanelScripts/surgepro_flushdns.js
+++ Surge/PanelScripts/surgepro_flushdns.js
@@ -10,7 +10,7 @@ let startTime = timeTransform(dateNow,dateTime)
 if ($trigger == "button") await httpAPI("/v1/profiles/reload");
 
   $done({
-      title:"Surge Pro",
+      title:title,
       content:`启动时长: ${startTime}`,
 		icon: params.icon,
 		"icon-color":params.color

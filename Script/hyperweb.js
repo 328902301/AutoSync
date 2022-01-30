e1c50d27e02c2deea8556fd80d57e4159603410d
https://github.com/iEwha/Profiles.git <royiewha@gmail.com>
2022-01-30 16:44:06 +0800

    +

--- Script/hyperweb.js
+++ Script/hyperweb.js
@@ -1,7 +1,10 @@
 /*
-#hyperweb
-^https://zy6kcqa01a.execute-api.us-east-2.amazonaws.com/prod/verifyReceipt"
+[rewrite_local]
+^https://zy6kcqa01a.execute-api.us-east-2.amazonaws.com/prod/verifyReceipt url script-response-body Hyperweb.js
+[MITM]
+hostname =zy6kcqa01a.execute-api.us-east-2.amazonaws.com 
 */
+
 var url = $request.url;
 let obj = JSON.parse($response.body);
 

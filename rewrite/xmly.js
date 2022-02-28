da7221ef987ea75807a15b13fce7e66245d7fafe
king <77053523+nameking77@users.noreply.github.com>
2022-02-28 13:33:15 +0800

    Update xmly.js

--- rewrite/xmly.js
+++ rewrite/xmly.js
@@ -4,14 +4,15 @@
 脚本功能：
 下载地址：商店下载
 脚本作者：King
-更新时间：
+更新时间：2022.2.28
 脚本发布：https://t.me/yqc_123
 问题反馈：https://t.me/yqc_007
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
 ********************************
 [rewrite_local]
-#
+#喜马拉雅
+^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)) url script-request-header https://raw.githubusercontent.com/nameking77/Qx/main/rewrite/xmly.js
 
 [mitm] 
 hostname = *.*xima*.*, *.xmcdn.com, *.*.*.*,

21e4867884838e1f97097a6f1d1015c184bcfd14
Rubik's Cube <dorking@139.com>
2022-04-11 20:57:47 +0800

    Update bilibili_diy.js

--- Scripts/bilibili_diy.js
+++ Scripts/bilibili_diy.js
@@ -141,10 +141,14 @@ if (magicJS.read(blackKey)) {
             for (let ii = 0; ii < obj["data"]["sections_v2"].length; ii++) {
               if(obj.data.sections_v2[ii].title=='推荐服务'||obj.data.sections_v2[ii].title=='推薦服務'){
                 obj.data.sections_v2[ii].items[0].title='个性装扮';
+                obj.data.sections_v2[ii].items[1].title='\u58A8\u9B5A\u624B\u8A18';
               }
               if(obj.data.sections_v2[ii].title=='更多服務'||obj.data.sections_v2[ii].title=='更多服务'){
                   if(obj.data.sections_v2[ii].items[0].id==500){
                       obj.data.sections_v2[ii].items[0].title='个性装扮';
+                  }
+                  if(obj.data.sections_v2[ii].items[1].id==501){
+                      obj.data.sections_v2[ii].items[1].title='\u58A8\u9B5A\u624B\u8A18';
                   }
               }
               if(obj.data.sections_v2[ii].title=='创作中心'||obj.data.sections_v2[ii].title=='創作中心'){

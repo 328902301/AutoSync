dc913ad62b65b6bfa1f581b5a7923ebb59beddc5
Kyoo.kk <futur.kk@gmail.com>
2022-04-27 19:51:01 +0800

    Update sub_info.js

--- sub_info.js
+++ sub_info.js
@@ -33,7 +33,7 @@ Sub_info = script-name=Sub_info,update-interval=86400
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let content = [`Used：${toPercent(proportion)｜Avl：${bytesToSize(total-used)}`];
+  let content = [`Used：${toPercent(proportion) | Avl：${bytesToSize(total-used)}`];
   let proportion = used / total;
   content.push(`futurkk is God Now!`);
 

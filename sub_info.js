3bab8dc67bd5b5ef45f2aeae9fc1fde2b8ad2d4f
Kyoo.kk <futur.kk@gmail.com>
2022-04-27 20:11:57 +0800

    Update sub_info.js

--- sub_info.js
+++ sub_info.js
@@ -33,7 +33,7 @@ Sub_info = script-name=Sub_info,update-interval=86400
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let content = ["Used: ${toPercent(proportion)}｜Avl: ${bytesToSize(total-used)}"];
+  let content = [`Used: ${toPercent(proportion)}｜Avl: ${bytesToSize(total-used)}`];
   let proportion = used / total;
 
 /*
@@ -53,7 +53,7 @@ Sub_info = script-name=Sub_info,update-interval=86400
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   $done({
-    title: "${args.title} | reset: ${resetDayLeft} Days",
+    title: `${args.title} | reset: ${resetDayLeft} Days`,
     content: content.join("\n"),
     icon: args.icon || "airplane.circle",
     "icon-color": args.color || "#007aff",

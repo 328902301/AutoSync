e8ee9befaa08f4774778f2238f5c7e38e75ac366
Kyoo.kk <futur.kk@gmail.com>
2022-04-27 20:54:45 +0800

    Update sub_info.js

--- sub_info.js
+++ sub_info.js
@@ -53,7 +53,7 @@ Sub_info = script-name=Sub_info,update-interval=86400
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   $done({
-    title: `${args.title} | reset: ${resetDayLeft} Days`,
+    title: `${args.title} ` + `| reset:` + `${resetDayLeft} Days`,
     content: content.join("\n"),
     icon: args.icon || "airplane.circle",
     "icon-color": args.color || "#007aff",

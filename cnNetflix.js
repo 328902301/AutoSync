7d6043503d6e23736c1b1327d0d789f46977fe69
Kyoo.kk <futur.kk@gmail.com>
2022-05-05 20:50:22 +0800

    Update cnNetflix.js

--- cnNetflix.js
+++ cnNetflix.js
@@ -16,17 +16,17 @@ const AREA_TEST_FILM_ID = 80018499
       if (code === 'Not Found') {
         return test(AREA_TEST_FILM_ID)
       }
-      result['Title'] = "快来看Netflix喔我的宝！"
+      result['title'] = "快来看Netflix喔我的宝！"
       result['icon'] = "checkmark.seal.fill"
 	    result['icon-color'] = '#3CB371'
-      result['content'] = '宝在' + code.toUpperCase() + '可以观看全部的剧集喔！'
+      result['content'] = '宝 在' + code.toUpperCase() + '可以观看全部的剧集喔！'
       return Promise.reject('BreakSignal')
     })
     .then((code) => {
       if (code === 'Not Found') {
         return Promise.reject('Not Available')
       }
-      result['Title'] = "听说你要看Netflix喔！"
+      result['title'] = "听说你要看Netflix喔！"
       result['icon'] = "exclamationmark.triangle"
 	    result['icon-color'] = "#FFD700"
       result['content'] = '可你在' + code.toUpperCase() + '只能看自制的喔！'
@@ -34,7 +34,7 @@ const AREA_TEST_FILM_ID = 80018499
     })
     .catch((error) => {
       if (error === 'Not Available') {
-        result['Title'] = "就你要看Netflix喔？"
+        result['title'] = "就你要看Netflix喔？"
         result['icon'] = "eye.slash"
 	      result['icon-color'] = "#DC143C"
         result['content'] = '在' + code.toUpperCase() + '不能看喔！快去搬砖喔！'

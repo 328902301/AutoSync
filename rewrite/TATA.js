3d33d9e48a2b63451a6ecda88143ab90559102e7
king <77053523+nameking77@users.noreply.github.com>
2022-05-07 12:07:34 +0800

    Create TATA.js

---
+++ rewrite/TATA.js
@@ -0,0 +1,21 @@
+/*
+ *
+ *
+脚本功能：TATA直播🔓
+脚本作者：King
+更新时间：2022.5.7
+下载地址：https://www.tata9.live
+脚本发布：https://t.me/yqc_123
+问题反馈：https://t.me/yqc_007
+使用声明：🈲️此脚本仅供学习与交流，
+        请勿转载与贩卖！🈲️
+********************************
+[rewrite_local]
+#TATA直播解锁付费
+http://j9700.xinxindaka.com/api/public/\?service=Live.checkLive url script-response-body TATA.js
+
+[mitm] 
+hostname = j9700.xinxindaka.com
+*
+*
+*/

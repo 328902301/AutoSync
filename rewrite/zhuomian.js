fc82bca2675c60c79677fc35272aac9e466099e9
king <77053523+nameking77@users.noreply.github.com>
2022-05-08 16:02:41 +0800

    Create zhuomian.js

---
+++ rewrite/zhuomian.js
@@ -0,0 +1,3 @@
+var king = $request.headers;
+    king['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/13.0 Safari/604.1';
+$done({headers : king});

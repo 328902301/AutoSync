45de478ce58a040669c33917af3a935f6742ff6e
futurkk <futur.kk@gmail.com>
2022-05-10 19:45:15 +0800

    Create reload.js

---
+++ Surge for myself/JS/reload.js
@@ -0,0 +1,10 @@
+
+$httpAPI("POST", "/v1/profiles/reload", {}, data => {
+    $notification.post("：就你要配置的喔？","你好，已经重载你的配置。","")
+    $done({
+        title: "不舒服就刷新我一下的喔！",
+        content: "哇！希望你今天也是开开心心的一天！",
+        icon: "globe.asia.australia",
+        "icon-color": "#0047AB",
+     })
+    });

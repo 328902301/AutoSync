c3be2a4b2634680d5e74b93058f2249fdbae322e
futurkk <futur.kk@gmail.com>
2022-05-10 20:10:37 +0800

    Update reload.js

--- Surge for myself/JS/reload.js
+++ Surge for myself/JS/reload.js
@@ -3,7 +3,7 @@ $httpAPI("POST", "/v1/profiles/reload", {}, data => {
     $notification.post("：就是你要重载配置吗？","你好，已经重载你的配置。","")
     $done({
         title: "不开心就刷新我一下的喔！",
-        content: "哇！希望你今天也是开开心心的一天！",
+        content: "宝～ 今天也要开开心心的喔！",
         icon: "hand.thumbsup.circle",
         "icon-color": "#0047AB",
      })

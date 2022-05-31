0bf0b2407e4f6bb53f3284e4ec0c7a7b2ff8f418
nobady <44594562+YAOJIO@users.noreply.github.com>
2022-05-31 09:05:45 +0800

    Update BaimaTodo.js

--- QuantumultX/Scripts/BaimaTodo.js
+++ QuantumultX/Scripts/BaimaTodo.js
@@ -1,17 +1,15 @@
 /*
-微信公众号：ios黑科技
-官方网站：s7aa.cn
+ForOwnUse
 
 圈X：
 [rewrite_local]
 #白马Todo清单
-
 ^https:\/\/www\.evestudio\.cn\/todoList\/getDetailUser url script-response-body baimaTodo.js
 
 [mitm]
 hostname= = www.evestudio.cn
 
-#下载地址：
+# 下载地址：
 https://apps.apple.com/cn/app/todo%E6%B8%85%E5%8D%95-%E8%B7%A8%E5%B9%B3%E5%8F%B0%E5%BE%85%E5%8A%9E%E4%BA%8B%E9%A1%B9%E4%B8%8E%E7%95%AA%E8%8C%84%E9%92%9F/id1566997654
 */
 

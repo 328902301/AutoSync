60ed688178d2727bfe60f593590733c5fbc0627e
nobady <44594562+YAOJIO@users.noreply.github.com>
2022-06-01 22:03:51 +0800

    Create cubox.js

---
+++ QuantumultX/Scripts/cubox.js
@@ -0,0 +1,19 @@
+/*
+@Author  摇光
+Explain  ForOwnUse
+圈X：
+[rewrite_local]
+# 白马Todo清单 Version:1.31
+^https:\/\/www\.cubox\.pro\/c\/api\/userInfo url script-response-body baimaTodo.js
+[mitm]
+hostname= = www.evestudio.cn
+# 下载地址：
+https://apps.apple.com/cn/app/todo%E6%B8%85%E5%8D%95-%E8%B7%A8%E5%B9%B3%E5%8F%B0%E5%BE%85%E5%8A%9E%E4%BA%8B%E9%A1%B9%E4%B8%8E%E7%95%AA%E8%8C%84%E9%92%9F/id1566997654
+*/
+
+var obj = JSON.parse($response.body);
+
+obj.data.isExpire = false;
+obj.data.expireTime = 2029-06-01T21:50:27+08:00;
+
+$done({body: JSON.stringify(obj)}); 

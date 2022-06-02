4b5572016253021cbb03f31a99dcc321e9c9ad18
nobady <44594562+YAOJIO@users.noreply.github.com>
2022-06-02 09:26:43 +0800

    Update cubox.js

--- QuantumultX/Scripts/cubox.js
+++ QuantumultX/Scripts/cubox.js
@@ -1,19 +1,29 @@
-/*
-@Author  摇光
-Explain  ForOwnUse
-圈X：
+/***********************************
+
+> 應用名稱：Cubox
+> 軟件版本：6.10.0
+> 下載地址：https://apps.apple.com/cn/app/cubox-%E6%94%B6%E8%97%8F%E9%98%85%E8%AF%BB-%E6%A0%87%E6%B3%A8%E7%AC%94%E8%AE%B0/id1113361350
+> 腳本作者：摇光
+> 更新時間：2022-06-02
+> 腳本功能：✅Unlock some VIP functions✅
+> 微信賬號：XXXXXXXX
+> 通知頻道：XXXXXXXXXXXXX
+> 問題反饋：XXXXXXXXXXXXXXXXX
+> 特别說明：本腳本僅供學習交流使用，禁止轉載售賣
+
+
 [rewrite_local]
-# 白马Todo清单 Version:1.31
-^https:\/\/www\.cubox\.pro\/c\/api\/userInfo url script-response-body baimaTodo.js
-[mitm]
-hostname= = www.evestudio.cn
-# 下载地址：
-https://apps.apple.com/cn/app/todo%E6%B8%85%E5%8D%95-%E8%B7%A8%E5%B9%B3%E5%8F%B0%E5%BE%85%E5%8A%9E%E4%BA%8B%E9%A1%B9%E4%B8%8E%E7%95%AA%E8%8C%84%E9%92%9F/id1566997654
-*/
+# Flomo（2022-06-02）@ddgksf2013
+^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body cubox.js
+
+
+[mitm] 
+hostname=cubox.pro
+
+***********************************/
 
 var obj = JSON.parse($response.body);
-
 obj.data.isExpire = false;
-obj.data.expireTime = 2029-06-01T21:50:27+08:00;
-
+obj.data.expireTime = "2029-06-01T21:50:27+08:00";
+obj.data.payTime = "2020-01-01T08:00:00+08:00";
 $done({body: JSON.stringify(obj)}); 

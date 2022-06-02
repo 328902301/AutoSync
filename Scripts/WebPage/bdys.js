67d62e5972a82fa86e72768f6f01ca7381aad453
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-06-02 17:13:16 +0800

    Update

---
+++ Scripts/WebPage/bdys.js
@@ -0,0 +1,4 @@
+/* 2022-05-28 */
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/RuCu6/QuanX@master/Scripts/WebPage/CSS/bdys.css" type="text/css">');
+$done({ body });

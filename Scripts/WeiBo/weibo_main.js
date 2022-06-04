9872977544e08a1f87a81ed934336dbc642e2045
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-06-04 17:28:16 +0800

    Update weibo_main.js

--- Scripts/WeiBo/weibo_main.js
+++ Scripts/WeiBo/weibo_main.js
@@ -16,18 +16,18 @@ const mainConfig = storeMainConfig ? JSON.parse(storeMainConfig) : {
 	removeGood: true,			//微博主好物种草
 	removeFollow: true,			//关注博主
 	modifyMenus: true,			//编辑上下文菜单
-	removeRelateItem: false,	//评论区相关内容
+	removeRelateItem: true,	//评论区相关内容
 	removeRecommendItem: true,	//评论区推荐内容
-	removeRewardItem: false,	//微博详情页打赏模块
+	removeRewardItem: true,	//微博详情页打赏模块
 
 	removeLiveMedia: true,		//首页顶部直播
-	removeNextVideo: false,					//关闭自动播放下一个视频
+	removeNextVideo: true,					//关闭自动播放下一个视频
 
 	removeInterestFriendInTopic: false,		//超话：超话里的好友
 	removeInterestTopic: false,				//超话：可能感兴趣的超话 + 好友关注
 	removeInterestUser: false,				//用户页：可能感兴趣的人
 
-	removeLvZhou: false,					//绿洲模块
+	removeLvZhou: true,					//绿洲模块
 
 	profileSkin1: null,						//用户页：自定义图标1
 	profileSkin2: null,						//用户页：自定义图标2

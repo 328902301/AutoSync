18df70fa06cfea24b9dd448d0435cb54731e9185
futurkk <futur.kk@gmail.com>
2022-06-06 19:01:28 +0800

    Update sub_info_K.js

--- Surge for myself/JS/sub_info_K.js
+++ Surge for myself/JS/sub_info_K.js
@@ -8,7 +8,7 @@
   let total = info.total;
   let proportion = used / total;
   let expire = args.expire || info.expire;
-  let content = [`${toPercent(proportion)} quota has been used.`];
+  let content = [`${toPercent(proportion)} quota has been used. Enjoy ur day!`];
 
   let now = new Date();
   let hour = now.getHours();

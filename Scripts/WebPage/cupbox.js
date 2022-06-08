16fca766d9c996038bf7fb72eac98f67c60dd66b
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-06-08 16:51:14 +0800

    Update cupbox.js

--- Scripts/WebPage/cupbox.js
+++ Scripts/WebPage/cupbox.js
@@ -1,5 +1,4 @@
 /* 2022-06-07 */
-
 var body = $response.body
     .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/RuCu6/QuanX@master/Scripts/WebPage/CSS/cupbox.css" type="text/css">');
 $done({ body });

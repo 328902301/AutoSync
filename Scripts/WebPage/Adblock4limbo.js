e10b7f25352d054cfa3291867c9f9d312dd4228f
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-06-08 15:16:13 +0800

    Update Adblock4limbo.js

--- Scripts/WebPage/Adblock4limbo.js
+++ Scripts/WebPage/Adblock4limbo.js
@@ -1,5 +1,5 @@
 let ele = '<head>';
-let eleReplace = '<head><link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" /><script type="text/javascript" async="async" src="https://limbopro.com/Adguard/Adblock4limbo.user.js"></script>'
+let eleReplace = '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/RuCu6/QuanX@master/Scripts/WebPage/CSS/Adblock4limbo.user.css" type="text/css" /><script type="text/javascript" async="async" src="https://cdn.jsdelivr.net/gh/RuCu6/QuanX@master/Scripts/WebPage/Adblock4limbo.user.js"></script>'
 let body = $response.body
     .replace(ele, eleReplace)
 $done({ body });

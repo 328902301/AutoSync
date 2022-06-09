c377ccbdb4a11ca3a3b957acd3b88c922092a93f
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-06-09 16:27:58 +0800

    Update and rename Scripts/WeiXin/UnblockURLinWeChat.js to Scripts/wechat110.js

--- Scripts/wechat110.js
+++ Scripts/wechat110.js
@@ -1,3 +1,8 @@
+/*
+引用地址 https://github.com/zZPiglet/Task/blob/master/UnblockURLinWeChat.conf
+引用地址 https://github.com/zZPiglet/Task/blob/master/asset/UnblockURLinWeChat.js
+*/
+
 let persisVal = read("UnblockURLinWeChat");
 let useCache = persisVal.useCache === "true"; //是否在微信中用快照显示被封禁的链接
 let forceRedirect = persisVal.forceRedirect === "true"; //是否在微信中进行强制重定向，允许的情况下可能出现循环重定向

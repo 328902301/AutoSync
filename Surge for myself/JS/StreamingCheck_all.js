d5c79a686c5a5dc9aefaa7cf7d5892b528d879ed
futurkk <futur.kk@gmail.com>
2022-06-28 20:47:23 +0800

    Update StreamingCheck_all.js

--- Surge for myself/JS/StreamingCheck_all.js
+++ Surge for myself/JS/StreamingCheck_all.js
@@ -22,7 +22,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
 
   ;(async () => {
     let panel_result = {
-      title: '流媒体解锁检测',
+      title: 'All streaming checking!',
       content: '',
       icon: 'xxxx',
       'icon-color': 'xxxx',

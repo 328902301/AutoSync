0cfe155e57de566dbc9d2bffd559fdc7c98171ef
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-07-13 21:24:22 +0800

    Update

---
+++ Scripts/Limbo/baidu.index_qx.js
@@ -0,0 +1,18 @@
+// 引用地址 https://github.com/limbopro/Adblock4limbo/blob/main/Adguard/surge_baidu.index.js
+
+/*
+let Oldone = '</style>'
+let Newone = '.ec_ad_results{display:none!important}</style>';
+let body = $response.body
+.replace(Oldone, Newone);
+$done({body});
+*/
+
+let rHead = '<head>'
+let newStyle = '<head><link rel=\"stylesheet\" href=\"https://raw.githubusercontent.com/RuCu6/QuanX/main/Css/baidu.index.css\" type=\"text/css\">'
+var rBody = '</body>'
+var newJavaScript = '<script type="text/javascript" async="async" src="https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/Limbo/baidu.index.js"></script>'
+let body = $response.body
+    .replace(rHead, newStyle)
+    .replace(rBody, newJavaScript);
+$done({ body });

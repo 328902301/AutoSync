75867a2bf01a33e9cc4edcc802bfa02ec5a9203d
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-07-13 11:43:32 +0800

    Create contentFarm_surge.js

---
+++ Scripts/contentFarm_surge.js
@@ -0,0 +1,10 @@
+// 引用地址 https://github.com/limbopro/Adblock4limbo/blob/main/Adguard/contentFarm/surge_contentFarm.js
+
+let regex_head = '<head>';
+let Style = '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/RuCu6/QuanX/main/Css/contentFarm.css">';
+let regex_body = '</body>';
+let JavaScript = '<script async="async" src="https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/contentFarm.js"></script></body>';
+let body = $response.body
+.replace(regex_head, Style)
+.replace(regex_body, JavaScript);
+$done({body});

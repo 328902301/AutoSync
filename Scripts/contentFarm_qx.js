c25bfb2fdef0cc158b331676c1cb4881e4a8bd28
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-07-13 11:21:30 +0800

    Update contentFarm_qx.js

--- Scripts/contentFarm_qx.js
+++ Scripts/contentFarm_qx.js
@@ -1,7 +1,7 @@
 // 引用地址 https://github.com/limbopro/Adblock4limbo/blob/main/Adguard/contentFarm/surge_contentFarm_qx.js
 
 let rHead = '<head>';
-let newStyle = '<head><link rel="stylesheet" href="https://limbopro.com/CSS/contentFarm.css">';
+let newStyle = '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/RuCu6/QuanX/main/Css/contentFarm.css">';
 let rBody = '</body>';
 let newJavaScript = '<script src="https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/contentFarm.js"></script></body>';
 let body = $response.body

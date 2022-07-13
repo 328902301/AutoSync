2a8590a002406c1e81b5484bd9c3548112b74584
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-07-13 11:14:39 +0800

    Update contentFarm_qx.js

--- Scripts/contentFarm_qx.js
+++ Scripts/contentFarm_qx.js
@@ -3,7 +3,7 @@
 let rHead = '<head>';
 let newStyle = '<head><link rel="stylesheet" href="https://limbopro.com/CSS/contentFarm.css">';
 let rBody = '</body>';
-let newJavaScript = '<script src="https://limbopro.com/Adguard/contentFarm/contentFarm.js"></script></body>';
+let newJavaScript = '<script src="https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/contentFarm.js"></script></body>';
 let body = $response.body
 .replace(rHead, newStyle)
 .replace(rBody, newJavaScript);

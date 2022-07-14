201813314d69a62b4b39220ccec11e99b1ef26b8
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-07-14 15:10:25 +0800

    Update

--- Scripts/BiliBili/bilibili_diy.js
+++ Scripts/BiliBili/bilibili_diy.js
@@ -32,7 +32,7 @@ if (magicJS.read(blackKey)) {
         try {
           // 39直播 40推荐 41热门 545追番 151影视 442动画, 99直播 100推荐 101热门 554动画 556动画
           // 442开始为概念版id，适配港澳台代理模式
-          const tabList = new Set([39, 40, 41, 151, 99, 100, 101, 554]);
+          const tabList = new Set([39, 40, 41, 151, 99, 100, 101]);
           // 尝试使用tab name直观修改
           const tabNameList = new Set(["直播", "推荐", "热门", "影视"]);
           let obj = JSON.parse(magicJS.response.body);

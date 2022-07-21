00dc4819471319d67d8be21ceaab472655c05f1a
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-07-21 15:30:50 +0800

    Update zhihu.js

--- Scripts/zhihu.js
+++ Scripts/zhihu.js
@@ -1,6 +1,6 @@
 /*
 引用地址 https://github.com/blackmatrix7/ios_rule_script/tree/master/script/zhihu
-引用地址 https://gist.githubusercontent.com/blackmatrix7/2e550ebf28ca60d620654e394ec47e0b/raw/zhihu.js
+引用地址 https://github.com/blackmatrix7/ios_rule_script/blob/master/script/zhihu/zhihu.js
 */
 
 const scriptName = "知乎助手";

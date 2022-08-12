2f5778eae5d6b7942249ed8acf8f47f8c47fa5e1
ami@ <ami@futurkk.com>
2022-08-12 20:08:29 +0800

    Update AppleDomain.py

--- action/AppleDomain.py
+++ action/AppleDomain.py
@@ -4,7 +4,5 @@ rawAppleDomain = requests.get("https://ruleset.skk.moe/List/domainset/apple_cdn.
 
 result = rawAppleDomain.split("\n")
 
-result = list(set(result))
-
 with open("./AppleDomain.conf", "w") as f:
     f.write("\n".join(result))

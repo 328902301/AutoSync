36e957d8c53019fcb2d90494b4475ebdfcad2515
ami@ <ami@futurkk.com>
2022-08-12 20:10:02 +0800

    Update RejectDomain.py

--- action/RejectDomain.py
+++ action/RejectDomain.py
@@ -4,7 +4,5 @@ rawRejectDomian = requests.get("https://ruleset.skk.moe/List/domainset/reject.co
 
 result = rawRejectDomian.split("\n")
 
-result = list(set(result))
-
 with open("./RejectDomian.conf", "w") as f:
     f.write("\n".join(result))

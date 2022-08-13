60b59287975bfc17a1abcec529885c74b11f2d6d
ami@ <ami@futurkk.com>
2022-08-13 12:28:34 +0800

    Update RejectDomainClash.py

--- action/RejectDomainClash.py
+++ action/RejectDomainClash.py
@@ -1,6 +1,6 @@
 import requests
 
-rawRejectDomian = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/AdvertisingLite/AdvertisingLite_Domain").text
+rawRejectDomian = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/AdvertisingLite/AdvertisingLite_Domain.yaml").text
 rawPrivacy = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Privacy/Privacy_Domain.yaml").text
 
 result = rawRejectDomian.split("\n") + rawPrivacy.split("\n")

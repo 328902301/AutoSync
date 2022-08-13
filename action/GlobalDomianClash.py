c13961222eae510deaa89f1e9c9d1c71816dfa96
ami@ <ami@futurkk.com>
2022-08-13 12:23:34 +0800

    Update GlobalDomianClash.py

--- action/GlobalDomianClash.py
+++ action/GlobalDomianClash.py
@@ -1,8 +1,8 @@
 import requests
 
-rawGlobalDomian = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Global/Global_Domain.list").text
+rawGlobalDomian = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Global/Global_Domain.yaml").text
 
 result = rawGlobalDomian.split("\n")
 
-with open("./GlobalDomain.list", "w") as f:
+with open("./GlobalDomain.yaml", "w") as f:
     f.write("\n".join(result))

d02934ee87e5ef23458ae40ff0b2776a7c657124
ami@ <ami@futurkk.com>
2022-08-14 11:18:52 +0800

    Update and rename DomainREJECT.py to REJECTDomain.py

--- action/DomainREJECT.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-rawDomainREJECT = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/AdGuard/Advertising/Advertising.txt").text
-
-result = rawDomainREJECT.split("\n")
-
-with open("./DomainREJEC.conf", "w") as f:
-    f.write("\n".join(result))
-   

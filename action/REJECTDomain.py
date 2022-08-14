827673f819ab172991f73a06ee54b3639dc6ce59
ami@ <ami@futurkk.com>
2022-08-14 11:09:47 +0800

    Delete REJECTDomain.py

--- action/REJECTDomain.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawRejectDomian = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Advertising/Advertising_Domain.list").text
-
-result = rawRejectDomian.split("\n")
-
-with open("./RejectDomain", "w") as f:
-    f.write("\n".join(result

e709dba0f00b4068ab49302cab0b7508a09d79de
ami@ <ami@futurkk.com>
2022-08-14 13:38:42 +0800

    Create amiMiTM.py

---
+++ action/amiMiTM.py
@@ -0,0 +1,12 @@
+import requests
+
+rawfirst = requests.get("https://raw.githubusercontent.com/futurkk/Potato/main/Surge%20for%20myself/sgmodule/ami%40Protect%20me.sgmodule").text
+rawBiliBili = requests.get("https://raw.githubusercontent.com/app2smile/rules/master/module/bilibili.sgmodule").text
+rawadMiTM = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Advertising/Advertising_MITM.sgmodule
+").text
+                         
+
+result = rawfirsti.split("\n") + rawBiliBili.split("\n") + rawadMiTM.split("\n")
+
+with open("./amiMiTM.sgmodule", "w") as f:
+    f.write("\n".join(result))

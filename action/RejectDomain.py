9aca2fd796ccf47514afe0e7add7b0ba3a85a21e
ami@ <ami@futurkk.com>
2022-08-14 11:05:29 +0800

    Delete RejectDomain.py

--- action/RejectDomain.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawRejectDomian = requests.get("https://ruleset.skk.moe/List/domainset/reject.conf").text
-
-result = rawRejectDomian.split("\n")
-
-with open("./RejectDomain.conf", "w") as f:
-    f.write("\n".join(result))

d5ead11c2c1696250d0daba563bd29936889ef42
ami@ <ami@futurkk.com>
2022-08-14 10:45:20 +0800

    Create REJECT.py

---
+++ action/REJECT.py
@@ -0,0 +1,8 @@
+import requests
+
+rawRejectDomian = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/AdGuard/Advertising/Advertising.txt").text
+
+result = rawRejectDomian.split("\n")
+
+with open(".REJECT", "w") as f:
+    f.write("\n".join(result))

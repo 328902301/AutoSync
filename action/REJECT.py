6a4188ed737d0f5211fb64f1b08aeb02991c6127
ami@ <ami@futurkk.com>
2022-08-14 10:48:33 +0800

    Update REJECT.py

--- action/REJECT.py
+++ action/REJECT.py
@@ -4,5 +4,5 @@ rawRejectDomian = requests.get("https://raw.githubusercontent.com/blackmatrix7/i
 
 result = rawRejectDomian.split("\n")
 
-with open("./REJECt", "w") as f:
+with open("./REJECT", "w") as f:
     f.write("\n".join(result))

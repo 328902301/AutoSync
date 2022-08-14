9710ca9d7640085097a5262a1f6457e51905f303
ami@ <ami@futurkk.com>
2022-08-14 11:28:43 +0800

    Update RejectDomainClash.py

--- action/RejectDomainClash.py
+++ action/RejectDomainClash.py
@@ -2,7 +2,7 @@ import requests
 
 rawRejectDomian = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Advertising/Advertising_Domain.yaml").text
 
-result = rawRejectDomian.split("\n") + rawPrivacy.split("\n")
+result = rawRejectDomian.split("\n")
 
 with open("./RejectDomain.yaml", "w") as f:
     f.write("\n".join(result))

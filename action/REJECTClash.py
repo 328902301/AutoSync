352c435cf1b1ce9dee452bfdf485bb5383ca1ab2
ami@ <ami@futurkk.com>
2022-08-14 12:21:56 +0800

    Delete REJECTClash.py

--- action/REJECTClash.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawREJECTClash = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Advertising/Advertising.yaml").text
-
-result = rawREJECTClash.split("\n")
-
-with open("./REJECTClash.yaml", "w") as f:
-    f.write("\n".join(result))

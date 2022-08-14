ccbc99009ba35acf0a2a55ee31b57b42d7cb1edc
ami@ <ami@futurkk.com>
2022-08-14 11:28:27 +0800

    Update RejectClash.py

--- action/RejectClash.py
+++ action/RejectClash.py
@@ -1,10 +1,8 @@
 import requests
 
-rawReject = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/AdvertisingLite/AdvertisingLite.yaml").text
-rawPrivacy = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Hijacking/Hijacking.list").text
-rawHijacking = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Privacy/Privacy.list").text
+rawReject = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Advertising/Advertising.list").text
 
-result = rawReject.split("\n") + rawPrivacy.split("\n") + rawHijacking.split("\n")
+result = rawReject.split("\n"
 
 with open("./Reject.yaml", "w") as f:
     f.write("\n".join(result))

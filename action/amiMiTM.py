e1e99a108b3a959c07ea114864484bcdc027654a
ami@ <ami@futurkk.com>
2022-08-14 13:50:05 +0800

    Update amiMiTM.py

--- action/amiMiTM.py
+++ action/amiMiTM.py
@@ -2,8 +2,7 @@ import requests
 
 rawfirst = requests.get("https://raw.githubusercontent.com/futurkk/Potato/main/Surge%20for%20myself/sgmodule/ami%40Protect%20me.sgmodule").text
 rawBiliBili = requests.get("https://raw.githubusercontent.com/app2smile/rules/master/module/bilibili.sgmodule").text
-rawadMiTM = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Advertising/Advertising_MITM.sgmodule
-").text
+rawadMiTM = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Advertising/Advertising_MITM.sgmodule").text
                          
 
 result = rawfirsti.split("\n") + rawBiliBili.split("\n") + rawadMiTM.split("\n")

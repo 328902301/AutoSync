78443c3acd6c22797f6745125ab6ca191bdcf281
ami@ <ami@futurkk.com>
2022-08-14 12:11:18 +0800

    Update REJECTClash.py

--- action/REJECTClash.py
+++ action/REJECTClash.py
@@ -1,6 +1,6 @@
 import requests
 
-rawREJECTClash = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Advertising/Advertising.list").text
+rawREJECTClash = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Advertising/Advertising.yaml").text
 
 result = rawREJECTClash.split("\n")
 

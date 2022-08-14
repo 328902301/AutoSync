bb5499bc7a80ee03de5488cca76f288e7b4c10c1
ami@ <ami@futurkk.com>
2022-08-14 11:31:02 +0800

    Delete RejectDomainClash.py

--- action/RejectDomainClash.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawRejectDomian = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Advertising/Advertising_Domain.yaml").text
-
-result = rawRejectDomian.split("\n")
-
-with open("./RejectDomain.yaml", "w") as f:
-    f.write("\n".join(result))

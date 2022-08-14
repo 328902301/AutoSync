7204b6f536cfe125f27a2551eeb1bf64a249b52e
ami@ <ami@futurkk.com>
2022-08-14 13:51:32 +0800

    Update amiMiTM.py

--- action/amiMiTM.py
+++ action/amiMiTM.py
@@ -5,7 +5,7 @@ rawBiliBili = requests.get("https://raw.githubusercontent.com/app2smile/rules/ma
 rawadMiTM = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Advertising/Advertising_MITM.sgmodule").text
                          
 
-result = rawfirsti.split("\n") + rawBiliBili.split("\n") + rawadMiTM.split("\n")
+result = rawfirst.split("\n") + rawBiliBili.split("\n") + rawadMiTM.split("\n")
 
 with open("./amiMiTM.sgmodule", "w") as f:
     f.write("\n".join(result))

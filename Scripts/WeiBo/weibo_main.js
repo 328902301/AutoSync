e8ca4c4668f11f40bb62bb2973695b556a8b6ded
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-08-15 18:47:35 +0800

    Update weibo_main.js

--- Scripts/WeiBo/weibo_main.js
+++ Scripts/WeiBo/weibo_main.js
@@ -1,6 +1,6 @@
 const version = 'v0712.2';
 
-//const $ = new Env("微博去广告");
+const $ = new Env("微博去广告");
 let storeMainConfig = $.getdata('mainConfig');
 let storeItemMenusConfig = $.getdata('itemMenusConfig');
 

ec6321de599b74a19106969bc7a301e7c6c83d64
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-08-15 18:06:23 +0800

    Update

--- Scripts/WeiBo/weibo_main.js
+++ Scripts/WeiBo/weibo_main.js
@@ -1,37 +1,38 @@
-// 2021-07-12
+// 2021-08-15
 // 引用地址 https:// github.com/zmqcherish/proxy-script/blob/main/weibo_main.js
 
 const version = 'v0712.2';
 
-let $ = new nobyda();
-let storeMainConfig = $.read('mainConfig');
-let storeItemMenusConfig = $.read('itemMenusConfig');
+const $ = new Env("微博去广告");
+let storeMainConfig = $.getdata('mainConfig');
+let storeItemMenusConfig = $.getdata('itemMenusConfig');
 
 //主要的选项配置
 const mainConfig = storeMainConfig ? JSON.parse(storeMainConfig) : {
-	isDebug: false,						//开启调试，会打印运行中部分日志
+	isDebug: false,							//开启调试，会打印运行中部分日志
 	//个人中心配置，其中多数是可以直接在更多功能里直接移除
-	removeHomeVip: true,				//个人中心头像旁边的vip样式
-	removeHomeCreatorTask: true,		//个人中心创作者中心下方的轮播图
+	removeHomeVip: true,					//个人中心头像旁边的vip样式
+	removeHomeCreatorTask: true,			//个人中心创作者中心下方的轮播图
 
 	//微博详情页配置
-	removeRelate: true,			//相关推荐
-	removeGood: true,			//微博主好物种草
-	removeFollow: true,			//关注博主
-	modifyMenus: true,			//编辑上下文菜单
-	removeRelateItem: true,	//评论区相关内容
-	removeRecommendItem: true,	//评论区推荐内容
-	removeRewardItem: true,	//微博详情页打赏模块
+	removeRelate: true,						//相关推荐
+	removeGood: true,						//微博主好物种草
+	removeFollow: true,						//关注博主
+	modifyMenus: true,						//编辑上下文菜单
+	removeRelateItem: true,					//评论区相关内容
+	removeRecommendItem: true,				//评论区推荐内容
+	removeRewardItem: true,					//微博详情页打赏模块
 
-	removeLiveMedia: true,		//首页顶部直播
+	removeLiveMedia: true,					//首页顶部直播
 	removeNextVideo: true,					//关闭自动播放下一个视频
+	removePinedTrending: true,				//删除热搜列表置顶条目
 
 	removeInterestFriendInTopic: true,		//超话：超话里的好友
 	removeInterestTopic: true,				//超话：可能感兴趣的超话 + 好友关注
 	removeInterestUser: true,				//用户页：可能感兴趣的人
 
-	removeLvZhou: true,					//绿洲模块
-	removeSearchWindow: true,			//搜索页滑动窗口，有的不是广告
+	removeLvZhou: true,						//绿洲模块
+	removeSearchWindow: true,				//搜索页滑动窗口，有的不是广告
 
 	profileSkin1: null,						//用户页：自定义图标1
 	profileSkin2: null,						//用户页：自定义图标2
@@ -42,52 +43,53 @@ const mainConfig = storeMainConfig ? JSON.parse(storeMainConfig) : {
 
 //菜单配置
 const itemMenusConfig = storeItemMenusConfig ? JSON.parse(storeItemMenusConfig) : {
-	creator_task:false,					//转发任务
-	mblog_menus_custom:false,				//寄微博
+	creator_task:true,						//转发任务
+	mblog_menus_custom:true,				//寄微博
 	mblog_menus_video_later:true,			//可能是稍后再看？没出现过
 	mblog_menus_comment_manager:true,		//评论管理
-	mblog_menus_avatar_widget:false,		//头像挂件
-	mblog_menus_card_bg: false,			//卡片背景
-	mblog_menus_long_picture:true,		//生成长图
+	mblog_menus_avatar_widget:true,			//头像挂件
+	mblog_menus_card_bg: true,				//卡片背景
+	mblog_menus_long_picture:true,			//生成长图
 	mblog_menus_delete:true,				//删除
-	mblog_menus_edit:true,				//编辑
-	mblog_menus_edit_history:true,		//编辑记录
+	mblog_menus_edit:true,					//编辑
+	mblog_menus_edit_history:true,			//编辑记录
 	mblog_menus_edit_video:true,			//编辑视频
-	mblog_menus_sticking:true,			//置顶
+	mblog_menus_sticking:true,				//置顶
 	mblog_menus_open_reward:true,			//赞赏
-	mblog_menus_novelty:false,			//新鲜事投稿
-	mblog_menus_favorite:true,			//收藏
+	mblog_menus_novelty:true,				//新鲜事投稿
+	mblog_menus_favorite:true,				//收藏
 	mblog_menus_promote:true,				//推广
 	mblog_menus_modify_visible:true,		//设置分享范围
-	mblog_menus_copy_url:true,			//复制链接
+	mblog_menus_copy_url:true,				//复制链接
 	mblog_menus_follow:true,				//关注
 	mblog_menus_video_feedback:true,		//播放反馈
 	mblog_menus_shield:true,				//屏蔽
 	mblog_menus_report:true,				//投诉
-	mblog_menus_apeal:true,				//申诉
+	mblog_menus_apeal:true,					//申诉
 	mblog_menus_home:true					//返回首页
 }
 
-const modifyCardsUrls = ['/cardlist', '/page', 'video/community_tab', '/searchall'];
+const modifyCardsUrls = ['/cardlist', 'video/community_tab', '/searchall'];
 const modifyStatusesUrls = ['statuses/friends/timeline', 'statuses/unread_friends_timeline', 'statuses/unread_hot_timeline', 'groups/timeline'];
 
 const otherUrls = {
-	'/profile/me': 'removeHome',						//个人页模块
+	'/profile/me': 'removeHome',								//个人页模块
 	'/statuses/extend': 'itemExtendHandler',					//微博详情页
-	'/video/remind_info': 'removeVideoRemind',			//tab2菜单上的假通知
-	'/checkin/show': 'removeCheckin',					//签到任务
-	'/live/media_homelist': 'removeMediaHomelist',		//首页直播
-	'/comments/build_comments': 'removeComments',		//微博详情页评论区相关内容
-	'/container/get_item': 'containerHandler',			//列表相关
-	'/profile/statuses': 'userHandler',					//用户主页
-	'/video/tiny_stream_video_list': 'nextVideoHandler',	//取消自动播放下一个视频
-	'/2/statuses/video_mixtimeline': 'nextVideoHandler',	
+	'/video/remind_info': 'removeVideoRemind',					//tab2菜单上的假通知
+	'/checkin/show': 'removeCheckin',							//签到任务
+	'/live/media_homelist': 'removeMediaHomelist',				//首页直播
+	'/comments/build_comments': 'removeComments',				//微博详情页评论区相关内容
+	'/container/get_item': 'containerHandler',					//列表相关
+	'/profile/statuses': 'userHandler',							//用户主页
+	'/video/tiny_stream_video_list': 'nextVideoHandler',		//取消自动播放下一个视频
+	'/2/statuses/video_mixtimeline': 'nextVideoHandler',		//取消自动播放下一个视频
 	'/!/client/light_skin': 'tabSkinHandler',
 	'/littleskin/preview': 'skinPreviewHandler',
 	'/search/finder': 'removeSearchMain',
 	'/search/container_timeline': 'removeSearch',
 	'/search/container_discover': 'removeSearch',
 	'/2/messageflow': 'removeMsgAd',
+	'/page': 'removePage'
 }
 
 function getModifyMethod(url) {
@@ -175,6 +177,17 @@ function removeMsgAd(data) {
 		newMsgs.push(msg)
 	}
 	data.messages = newMsgs;
+	return data;
+}
+
+function removePage(data){
+	removeCards(data);
+
+	//删除热搜列表置顶条目
+	if (mainConfig.removePinedTrending && data.cards && data.cards.length > 0) {
+		data.cards[0].card_group = data.cards[0].card_group.filter(c=>!c.itemid.includes("t:51"));
+	}
+
 	return data;
 }
 
@@ -295,9 +308,9 @@ function itemExtendHandler(data) {
 		if(data.trend && data.trend.titles) {
 			let title = data.trend.titles.title;
 			if(mainConfig.removeRelate && title === '相关推荐') {
-				data.trend = null;
+				delete data.trend;
 			} else if (mainConfig.removeGood && title === '博主好物种草') {
-				data.trend = null;
+				delete data.trend;
 			}
 		}
 	}
@@ -317,7 +330,7 @@ function itemExtendHandler(data) {
 	try {
 		let picUrl = data.trend.extra_struct.extBtnInfo.btn_picurl;
 		if(picUrl.indexOf('timeline_icon_ad_delete') > -1) {
-			data.trend = null;
+			delete data.trend;
 		}
 	} catch (error) {
 		
@@ -394,7 +407,7 @@ function removeHome(data) {
 		let itemId = item.itemId;
 		if(itemId == 'profileme_mine') {
 			if(mainConfig.removeHomeVip) {
-				item = removeHomeVip(item);;
+				item = removeHomeVip(item);
 			}
 			updateFollowOrder(item);
 			newItems.push(item);
@@ -442,7 +455,7 @@ function removeMediaHomelist(data) {
 function removeComments(data) {
 	let delType = ['广告'];
 	if(mainConfig.removeRelateItem) delType.push('相关内容');
-	if(mainConfig.removeRecommendItem) delType.push('推荐');
+	if(mainConfig.removeRecommendItem) delType.push(...['推荐', '热推']);
 	// if(delType.length === 0) return;
 	let items = data.datas || [];
 	if(items.length === 0) return;
@@ -505,7 +518,7 @@ function nextVideoHandler(data) {
 	if(mainConfig.removeNextVideo) {
 		data.statuses = [];
 		data.tab_list = [];
-		log('nextVideoHandler');
+		console.log('nextVideoHandler');
 	}
 }
 
@@ -550,32 +563,7 @@ function log(data) {
 }
 
 
-function nobyda() {
-	const isQuanX = typeof $task != "undefined";
-	const isSurge = typeof $httpClient != "undefined";
-	const isRequest = typeof $request != "undefined";
-	const notify = (title, subtitle='', message='') => {
-		if (isQuanX) $notify(title, subtitle, message)
-		if (isSurge) $notification.post(title, subtitle, message);
-	}
-	const read = (key) => {
-		if (isQuanX) return $prefs.valueForKey(key);
-		if (isSurge) return $persistentStore.read(key);
-	}
-	const done = (value = {}) => {
-		if (isQuanX) return $done(value);
-		if (isSurge) isRequest ? $done(value) : $done();
-	}
-
-	return {
-		isRequest,
-		isSurge,
-		isQuanX,
-		notify,
-		read,
-		done
-	}
-}
+function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.encoding="utf-8",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`🔔${this.name}, 开始!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}isShadowrocket(){return"undefined"!=typeof $rocket}isStash(){return"undefined"!=typeof $environment&&$environment["stash-version"]}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,n]=i.split("@"),a={url:`http://${n}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),n=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(n);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){if(t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status?s.status:s.statusCode,s.status=s.statusCode),e(t,s,i)});else if(this.isQuanX())this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t&&t.error||"UndefinedError"));else if(this.isNode()){let s=require("iconv-lite");this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();s&&this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:i,statusCode:r,headers:o,rawBody:n}=t,a=s.decode(n,this.encoding);e(null,{status:i,statusCode:r,headers:o,rawBody:n,body:a},a)},t=>{const{message:i,response:r}=t;e(i,r,r&&s.decode(r.rawBody,this.encoding))})}}post(t,e=(()=>{})){const s=t.method?t.method.toLocaleLowerCase():"post";if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient[s](t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status?s.status:s.statusCode,s.status=s.statusCode),e(t,s,i)});else if(this.isQuanX())t.method=s,this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t&&t.error||"UndefinedError"));else if(this.isNode()){let i=require("iconv-lite");this.initGotEnv(t);const{url:r,...o}=t;this.got[s](r,o).then(t=>{const{statusCode:s,statusCode:r,headers:o,rawBody:n}=t,a=i.decode(n,this.encoding);e(null,{status:s,statusCode:r,headers:o,rawBody:n,body:a},a)},t=>{const{message:s,response:r}=t;e(s,r,r&&i.decode(r.rawBody,this.encoding))})}}time(t,e=null){const s=e?new Date(e):new Date;let i={"M+":s.getMonth()+1,"d+":s.getDate(),"H+":s.getHours(),"m+":s.getMinutes(),"s+":s.getSeconds(),"q+":Math.floor((s.getMonth()+3)/3),S:s.getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,(s.getFullYear()+"").substr(4-RegExp.$1.length)));for(let e in i)new RegExp("("+e+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?i[e]:("00"+i[e]).substr((""+i[e]).length)));return t}queryStr(t){let e="";for(const s in t){let i=t[s];null!=i&&""!==i&&("object"==typeof i&&(i=JSON.stringify(i)),e+=`${s}=${i}&`)}return e=e.substring(0,e.length-1),e}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl,i=t["update-pasteboard"]||t.updatePasteboard;return{"open-url":e,"media-url":s,"update-pasteboard":i}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};if(this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r))),!this.isMuteLog){let t=["","==============📣系统通知📣=============="];t.push(e),s&&t.push(s),i&&t.push(i),console.log(t.join("\n")),this.logs=this.logs.concat(t)}}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`❗️${this.name}, 错误!`,t.stack):this.log("",`❗️${this.name}, 错误!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`🔔${this.name}, 结束! 🕛 ${s} 秒`),this.log(),this.isSurge()||this.isQuanX()||this.isLoon()?$done(t):this.isNode()&&process.exit(1)}}(t,e)}
 
 var body = $response.body;
 var url = $request.url;
@@ -587,5 +575,5 @@ if(method) {
 	new func(data);
 	body = JSON.stringify(data);
 }
-// $done({ body });
-$.done(body);
+
+$.done({ body });

70f059979254519fae52110a815e21a272d7c260
General℡ <76241047+General74110@users.noreply.github.com>
2022-08-16 22:51:36 +0800

    Delete PROXYPOOL directory

--- PROXYPOOL/config/source.go
+++
@@ -1,8 +0,0 @@
-package config
-
-import "github.com/zu1k/proxypool/pkg/tool"
-
-type Source struct {
-	Type    string       `json:"type" yaml:"type"`
-	Options tool.Options `json:"options" yaml:"options"`
-}

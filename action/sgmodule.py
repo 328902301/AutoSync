fc13552ce29026b6429516226ec79d8fd3d77f6e
ami@ <ami@futurkk.com>
2022-08-24 21:50:50 +0800

    Update sgmodule.py

--- action/sgmodule.py
+++ action/sgmodule.py
@@ -4,12 +4,9 @@ rawboxjs = requests.get("https://github.com/chavyleung/scripts/raw/master/box/re
 rawsubstore = requests.get("https://raw.githubusercontent.com/sub-store-org/Sub-Store/master/config/Surge.sgmodule").text
 rawweather = requests.get("https://raw.githubusercontent.com/VirgilClyne/iRingo/beta/sgmodule/Weather.beta.sgmodule").text
 
-tmpresult = rawboxjs.split("\n") + rawsubstore.split("\n") + rawweather.split("\n")
+result = rawboxjs.split("\n") + rawsubstore.split("\n") + rawweather.split("\n")
 
-tmpresult = list(set(tmpresult))
-
-result = []
-print(tmpresult)
+result = list(set(result))
 
 with open("./aa.sgmodule", "w") as f:
     f.write("\n".join(result))

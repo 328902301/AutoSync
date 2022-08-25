798c931e73a0ec3325ce99080a73a449d9c0ab79
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-08-25 09:22:42 +0800

    Update and rename NotabilityProCrack.js to NotabilityPro.js

--- Scripts/Crack/NotabilityPro.js
+++ Scripts/Crack/NotabilityPro.js
@@ -14,7 +14,7 @@
 
 [rewrite_local]
 # Notability解锁订阅
-^https?:\/\/notability\.com\/subscriptions url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/NotabilityProCrack.js
+^https?:\/\/notability\.com\/subscriptions url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/Crack/NotabilityPro.js
 
 [mitm]
 hostname = notability.com

aeecbc41959faddbd97af20d6c61c8f4638f7f8c
ami@ <ami@futurkk.com>
2022-08-25 23:18:34 +0800

    Update and rename REJECTnonip.py to REJECTDrop.py

---
+++ action/REJECTDrop.py
@@ -0,0 +1,9 @@
+import requests
+
+rawnonip = requests.get("https://ruleset.skk.moe/List/non_ip/reject.conf").text
+rawip = requests.get("https://ruleset.skk.moe/List/ip/reject.conf").text
+
+result = rawnonip.split("\n") + rawip.split("\n")
+
+with open("./REJECTDrop.conf", "w") as f:
+    f.write("\n".join(result))
--- action/REJECTnonip.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawnonip = requests.get("https://ruleset.skk.moe/List/non_ip/reject.conf").text
-
-result = rawnonip.split("\n")
-
-with open("./REJECTnonip.conf", "w") as f:
-    f.write("\n".join(result))

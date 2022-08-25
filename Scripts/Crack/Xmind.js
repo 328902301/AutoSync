932d552bf3f0b0b3df35d8e2f67c285e0580d203
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-08-25 13:18:33 +0800

    Update Xmind.js

--- Scripts/Crack/Xmind.js
+++ Scripts/Crack/Xmind.js
@@ -2,11 +2,9 @@
 脚本功能：解锁Xmind思维导图订阅到2030年
 脚本作者：R·E
 脚本发布：https://t.me/Functional_Store_Hub
-
 更新时间：2022.08.13
 支持版本：商店版本 
 下载地址：https://apps.apple.com/app/id1286983622
-
 使用声明：⚠️⚠️⚠️此脚本仅供学习与交流,禁止转载与贩卖,需在24小时内从设备中删除！⚠️⚠️⚠️
 
 [rewrite_local]

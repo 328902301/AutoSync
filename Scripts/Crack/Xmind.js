7c9f82ec93671ca79ea5267615db100b614a179e
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-08-25 09:21:41 +0800

    Update Xmind.js

--- Scripts/Crack/Xmind.js
+++ Scripts/Crack/Xmind.js
@@ -10,11 +10,10 @@
 使用声明：⚠️⚠️⚠️此脚本仅供学习与交流,禁止转载与贩卖,需在24小时内从设备中删除！⚠️⚠️⚠️
 
 [rewrite_local]
-https?:\/\/www\.xmind\.(cn|net)\/\_res\/devices url script-response-body https://raw.githubusercontent.com/I-am-R-E/Functional-Store-Hub/Master/Xmind/Script/Xmind.js
+https?:\/\/www\.xmind\.(cn|net)\/\_res\/devices url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/Crack/Xmind.js
 
 [mitm]
 hostname = www.xmind.cn, www.xmind.net
-
 */
 
 var _0x49754b=JSON['parse']($response['body']);_0x49754b['license']['status']='sub';_0x49754b['license']['expireTime']=0x1b8d90e4000;$done({'body':JSON['stringify'](_0x49754b)});

0492789b1de40e8b5973bc747ccdf5e2c5efa254
ami@ <ami@futurkk.com>
2022-08-25 23:21:30 +0800

    Delete GlobalDomianClash.py

--- action/GlobalDomianClash.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawGlobalDomian = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Global/Global_Domain.yaml").text
-
-result = rawGlobalDomian.split("\n")
-
-with open("./GlobalDomain.yaml", "w") as f:
-    f.write("\n".join(result))

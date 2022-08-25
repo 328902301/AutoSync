383d384a8bd0631310ac511815e74b5d4c330135
ami@ <ami@futurkk.com>
2022-08-25 23:18:50 +0800

    Delete REJECTip.py

--- action/REJECTip.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawip = requests.get("https://ruleset.skk.moe/List/ip/reject.conf").text
-
-result = rawip.split("\n")
-
-with open("./REJECTip.conf", "w") as f:
-    f.write("\n".join(result))

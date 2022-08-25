3d36b839f7e6f140d967dd62c6abd99972f1282b
ami@ <ami@futurkk.com>
2022-08-25 20:51:27 +0800

    Update MiTMSukka.py

--- action/MiTMSukka.py
+++ action/MiTMSukka.py
@@ -4,5 +4,5 @@ rawMiTM = requests.get("https://ruleset.skk.moe/Modules/sukka_mitm_hostnames.sgm
 
 result = rawMiTM.split("\n")
 
-with open("./sgmodule/MiMTSukka.sgmodule", "w") as f:
+with open("./sgmodule/MiTMSukka.sgmodule", "w") as f:
     f.write("\n".join(result))

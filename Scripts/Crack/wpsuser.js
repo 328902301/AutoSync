aa34c700f4cc6272bf34964b34e7afe4b6782a56
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-08-25 13:38:34 +0800

    Update wpsuser.js

--- Scripts/Crack/wpsuser.js
+++ Scripts/Crack/wpsuser.js
@@ -12,13 +12,11 @@
         请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
 [rewrite_local]
-# > WPS解锁稻壳会员
 ^https?:\/\/account\.wps\.cn\/api\/v3\/mine\/vips url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPuserCrack.js
 ^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPowerCrack.js
 
 [mitm] 
 hostname = *.docer.wps.cn, vipapi.wps.cn, account.wps.cn
-
 *******************************/
 
 var body = $response.body;
@@ -38,4 +36,3 @@ body = JSON.stringify(objc);
 $done({ 
     body 
 });
-

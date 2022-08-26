b98abe09e386ab6995ac44450fd6f0e548f38a96
Rubik's Cube <49463634+RuCu6@users.noreply.github.com>
2022-08-26 13:03:06 +0800

    Update weibo_main.js

--- Scripts/WeiBo/weibo_main.js
+++ Scripts/WeiBo/weibo_main.js
@@ -46,25 +46,25 @@ const itemMenusConfig = storeItemMenusConfig ? JSON.parse(storeItemMenusConfig) 
 	mblog_menus_video_later:false,			//可能是稍后再看？没出现过
 	mblog_menus_comment_manager:false,		//评论管理
 	mblog_menus_avatar_widget:false,		//头像挂件
-	mblog_menus_card_bg: false,				//卡片背景
+	mblog_menus_card_bg:false,				//卡片背景
 	mblog_menus_long_picture:true,			//生成长图
 	mblog_menus_delete:true,				//删除
 	mblog_menus_edit:true,					//编辑
 	mblog_menus_edit_history:true,			//编辑记录
 	mblog_menus_edit_video:true,			//编辑视频
-	mblog_menus_sticking:true,				//置顶
+	mblog_menus_sticking:false,				//置顶
 	mblog_menus_open_reward:false,			//赞赏
 	mblog_menus_novelty:false,				//新鲜事投稿
-	mblog_menus_favorite:true,				//收藏
+	mblog_menus_favorite:false,				//收藏
 	mblog_menus_promote:false,				//推广
-	mblog_menus_modify_visible:true,		//设置分享范围
+	mblog_menus_modify_visible:false,		//设置分享范围
 	mblog_menus_copy_url:true,				//复制链接
-	mblog_menus_follow:true,				//关注
+	mblog_menus_follow:false,				//关注
 	mblog_menus_video_feedback:false,		//播放反馈
 	mblog_menus_shield:true,				//屏蔽
 	mblog_menus_report:true,				//投诉
-	mblog_menus_apeal:true,					//申诉
-	mblog_menus_home:true					//返回首页
+	mblog_menus_apeal:false,					//申诉
+	mblog_menus_home:false					//返回首页
 }
 
 const modifyCardsUrls = ['/cardlist', 'video/community_tab', '/searchall'];

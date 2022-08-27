00189f6b4169ed35176d5ea168e3e7abed2f7827
ami@ <ami@futurkk.com>
2022-08-27 12:09:40 +0800

    Create qxamiProxy.py

---
+++ action/qxamiProxy.py
@@ -0,0 +1,11 @@
+import requests
+
+rawMicrosoft = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Microsoft/Microsoft.list").text
+rawYouTube = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/YouTube/YouTube.list").text
+rawGoogle = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Google/Google.list").text
+
+
+result = rawMicrosoft.split("\n") + rawYouTube.split("\n") + rawGoogle.split("\n")
+
+with open("./quanx/amiProxy.conf", "w") as f:
+    f.write("\n".join(result))

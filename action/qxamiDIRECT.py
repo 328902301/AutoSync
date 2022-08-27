ea28cf86241eeebb4a3855dabcf9c9e8fe33de94
ami@ <ami@futurkk.com>
2022-08-27 11:38:17 +0800

    Create qxamiDIRECT.py

---
+++ action/qxamiDIRECT.py
@@ -0,0 +1,9 @@
+import requests
+
+rawWeChat = requests.get("https://raw.githubusercontent.com/NobyDa/Script/master/Surge/WeChat.list").text
+rawApple = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Apple/Apple.list").text
+
+result = rawWeChat.split("\n") + rawApple.split("\n")
+
+with open("./quanx/amiDIRECT.list", "w") as f:
+    f.write("\n".join(result))

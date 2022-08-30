016761766bf659666ae187189a32c824f0052c59
ami@ <ami@futurkk.com>
2022-08-30 10:14:13 +0800

    Update SurgeAppleDomain.py

--- action/SurgeAppleDomain.py
+++ action/SurgeAppleDomain.py
@@ -2,7 +2,10 @@ import requests
 
 rawAppleDomain = requests.get("https://ruleset.skk.moe/List/domainset/apple_cdn.conf").text
 
-result = rawAppleDomain.split("\n")
+result = list()
+for rawresult in [rawAppleDomain]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
 
 with open("./AppleDomain.conf", "w") as f:
     f.write("\n".join(result))

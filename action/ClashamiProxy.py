f97b9a1f74367432d91ed5e100a97c238a706419
ami@ <ami@futurkk.com>
2022-08-30 09:55:56 +0800

    Update ClashamiProxy.py

--- action/ClashamiProxy.py
+++ action/ClashamiProxy.py
@@ -4,9 +4,9 @@ rawMicrosoft = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_
 rawYouTube = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/YouTube/YouTube.yaml").text
 rawGoogle = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Google/Google.yaml").text
 
-result = ['palyload:']
+result = ['payload:']
 for rawresult in [rawMicrosoft, rawYouTube, rawGoogle]:
-    result.extend([item.rstrip() for item in rawresult.split('\n') if not (item.startswith('#') or item.startswith('palyload'))])
+    result.extend([item.rstrip() for item in rawresult.split('\n') if not (item.startswith('#') or item.startswith('payload:'))])
 result_text = '\n'.join(result)
 
 with open("./clash/amiProxy.yaml", "w") as f:

a9b4505821fb00cc45fc212d826a6cab73f9f58e
ami@ <ami@futurkk.com>
2022-08-30 10:24:13 +0800

    Update SurgeamiProxy.py

--- action/SurgeamiProxy.py
+++ action/SurgeamiProxy.py
@@ -5,7 +5,10 @@ rawYouTube = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_ru
 rawGoogle = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Google/Google.list").text
 
 
-result = rawMicrosoft.split("\n") + rawYouTube.split("\n") + rawGoogle.split("\n")
+result = list()
+for rawresult in [rawMicrosoft, rawYouTube, rawGoogle]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
 
 with open("./amiProxy.conf", "w") as f:
     f.write("\n".join(result))

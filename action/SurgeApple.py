e45942cb99be41fb1920e5ec550d65affe07b978
ami@ <ami@futurkk.com>
2022-08-30 10:13:34 +0800

    Update SurgeApple.py

--- action/SurgeApple.py
+++ action/SurgeApple.py
@@ -2,7 +2,10 @@ import requests
 
 rawApple = requests.get("https://ruleset.skk.moe/List/non_ip/apple_services.conf").text
 
-result = rawApple.split("\n")
+result = list()
+for rawresult in [rawApple]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
 
 with open("./Apple.conf", "w") as f:
     f.write("\n".join(result))

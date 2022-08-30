e6d2ab74831f8a376779e771e0992d9c2e388b5c
ami@ <ami@futurkk.com>
2022-08-30 10:16:15 +0800

    Update SurgeEmail.py

--- action/SurgeEmail.py
+++ action/SurgeEmail.py
@@ -2,7 +2,10 @@ import requests
 
 rawEmail = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Mail/Mail.list").text
 
-result = rawEmail.split("\n")
+result = list()
+for rawresult in [rawEmail]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
 
 with open("./Email.conf", "w") as f:
     f.write("\n".join(result))

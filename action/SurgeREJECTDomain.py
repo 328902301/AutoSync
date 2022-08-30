0281e29a3657cb0929879de676faa869c2d2fc6a
ami@ <ami@futurkk.com>
2022-08-30 10:20:16 +0800

    Update SurgeREJECTDomain.py

--- action/SurgeREJECTDomain.py
+++ action/SurgeREJECTDomain.py
@@ -2,7 +2,10 @@ import requests
 
 rawREJECTDomain = requests.get("https://ruleset.skk.moe/List/domainset/reject.conf").text
 
-result = rawREJECTDomain.split("\n")
+result = list()
+for rawresult in [rawREJECTDomain]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
 
 with open("./REJECTDomain.conf", "w") as f:
     f.write("\n".join(result))

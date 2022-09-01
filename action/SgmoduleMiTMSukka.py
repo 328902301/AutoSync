5c0ca01ac6ea6ac6050ee48b35030fe0f15de3cc
ami@ <ami@futurkk.com>
2022-09-01 09:46:33 +0800

    Delete SgmoduleMiTMSukka.py

--- action/SgmoduleMiTMSukka.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawMiTM = requests.get("https://ruleset.skk.moe/Modules/sukka_mitm_hostnames.sgmodule").text
-
-result = rawMiTM.split("\n")
-
-with open("./sgmodule/MiTMSukka.sgmodule", "w") as f:
-    f.write("\n".join(result))

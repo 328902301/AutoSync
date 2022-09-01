93ae7094ea38ad8118bf2e9993059612111b8149
ami@ <ami@futurkk.com>
2022-09-01 11:23:10 +0800

    Update test.py

--- action/test.py
+++ action/test.py
@@ -38,12 +38,12 @@ config_combine = get_module_content(sgmodules)
 
 
 # write to file
-with open('combine.sgmodule', 'w') as f:
+with open('./sgmodule/ami.sgmodule', 'w') as f:
     f.writelines(['#!name=CombineModule\n', '#!desc=sgmodule combined\n'])
     for section_key in config_combine:
         f.write(section_key + '\n')
         if section_key == '[MITM]':
-            f.write('hostname = %APPEND% ' + ','.join(
+            f.write('hostname = %APPEND%' + ','.join(
                 [item.split('%APPEND%')[-1].rstrip() for item in config_combine[section_key]]) + '\n')
         else:
             f.writelines(config_combine[section_key])

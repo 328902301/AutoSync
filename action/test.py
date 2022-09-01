3bda587761e556cc0cb59ff0e25deeed336fc524
ami@ <ami@futurkk.com>
2022-09-01 09:52:45 +0800

    Update test.py

--- action/test.py
+++ action/test.py
@@ -28,7 +28,8 @@ def get_module_content(module_urls):
 
 sgmodules = ['https://ruleset.skk.moe/Modules/sukka_mitm_hostnames.sgmodule',
              'https://gist.githubusercontent.com/futurkk/144a94ddac70290f638f998148ccccf8/raw/ami.sgmodule',
-             'https://raw.githubusercontent.com/sub-store-org/Sub-Store/master/config/Surge.sgmodule']
+             'https://raw.githubusercontent.com/sub-store-org/Sub-Store/master/config/Surge.sgmodule'
+             'https://sgmodule-argument-proxy.vercel.app/api/temp?url=https%3A%2F%2Fraw.githubusercontent.com%2FVirgilClyne%2FiRingo%2Fmain%2Fsgmodule%2FWeather.sgmodule&arg=Switch%3Dtrue%26NextHour.Switch%3Dtrue%26NextHour.Source%3Dapi.caiyunapp.com%26AQI.Switch%3Dtrue%26AQI.Targets%3DHJ6332012%26AQI.Local.Switch%3Dtrue%26AQI.Local.Standard%3DWAQI_InstantCast%26AQI.Source%3Dapi.caiyunapp.com%26AQI.Comparison.Switch%3Dtrue%26AQI.Comparison.Source%3Dapi.caiyunapp.com%26Map.AQI%3Dfalse%26APIs.WeatherOL.HTTPHeaders%3D%7B%27Content-Type%27%3A%27application%2Fjson%27%7D%26APIs.ColorfulClouds.HTTPHeaders%3D%7B%27Content-Type%27%3A%27application%2Fjson%27%7D%26APIs.ColorfulClouds.Token%3D5Jo8Fr20vSRTaDYE%26APIs.ColorfulClouds.ForceCNForAQI%3Dtrue%26APIs.ColorfulClouds.ForceCNForComparison%3Dfalse%26APIs.WAQI.HTTPHeaders%3D%7B%27Content-Type%27%3A%27application%2Fjson%27%7D%26APIs.WAQI.Token%3D%26Log.Level%3Dinfo%26Log.Location%3Dfalse']
 
 config_combine = get_module_content(sgmodules)
 

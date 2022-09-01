70d7437d47675e53175e49bb283a2246fb5305e7
ami@ <ami@futurkk.com>
2022-09-01 09:19:57 +0800

    Update test.py

--- action/test.py
+++ action/test.py
@@ -1,22 +1,42 @@
 import requests
 
-rawMiTM = requests.get("https://ruleset.skk.moe/Modules/sukka_mitm_hostnames.sgmodule").text
-rawami = requests.get("https://gist.githubusercontent.com/futurkk/144a94ddac70290f638f998148ccccf8/raw/91b0287a3d43983b1407322a3a9b944174c73a41/ami.sgmodule").text
-rawsubstore = requests.get("https://raw.githubusercontent.com/sub-store-org/Sub-Store/master/config/Surge.sgmodule").text
 
-result = ['#!name=test\n#!desc=sgmodule test']
-for rawresult in [rawMiTM, rawami, rawsubstore]:
-    result.extend([item.rstrip() for item in rawresult.split('\n') if not (item.startswith('#'))])
-result_text = '\n'.join(result)
+def get_module_content(module_urls):
+    config_dict = dict()
+    for module_url in module_urls:
+        module_text = requests.get(module_url).text
+        section_key = ''
+        for raw_line in module_text.split('\n'):
+            if raw_line.startswith('#'):
+                pass
+            elif raw_line.startswith('['):
+                section_key = raw_line.rstrip()
+                if section_key not in config_dict:
+                    config_dict[section_key] = list()
+            else:
+                line_content = raw_line.rstrip()
+                if line_content:
+                    if section_key:
+                        if line_content not in config_dict[section_key]:
+                            config_dict[section_key].append(line_content + '\n')
+                        else:
+                            print(line_content + ' is duplicated')
+                    else:
+                        print(line_content + ' has no section_key')
+    return config_dict
 
-import configparser
-config = configparser.ConfigParser()
-config.read('result_text')
-sections_list = config.sections()
-for s in sections_list:
-    print(s)
-    for k in config[s]:
-        print(k,':',config[s][k])
 
-with open("./sgmodule/test.sgmodule", "w") as f:
-    f.write("\n".join(config))
+sgmodules = ['https://ruleset.skk.moe/Modules/sukka_mitm_hostnames.sgmodule',
+             'https://gist.githubusercontent.com/futurkk/144a94ddac70290f638f998148ccccf8/raw/' +
+             '91b0287a3d43983b1407322a3a9b944174c73a41/ami.sgmodule',
+             'https://raw.githubusercontent.com/sub-store-org/Sub-Store/master/config/Surge.sgmodule']
+
+config_combine = get_module_content(sgmodules)
+
+
+with open('result.sgmodule', 'w') as f:
+    f.writelines(['#!name=test\n', '#!desc=sgmodule test\n'])
+    for section_key in config_combine:
+        f.write(section_key + '\n')
+        f.writelines(config_combine[section_key])
+        f.write('\n')

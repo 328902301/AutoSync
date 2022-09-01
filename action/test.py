b4b5ebff4f27ecb4c5805e13efaee7c1d979f432
ami@ <ami@futurkk.com>
2022-09-01 11:20:33 +0800

    Update test.py

--- action/test.py
+++ action/test.py
@@ -1,6 +1,6 @@
 import requests
 
-
+# Method to get surge module content
 def get_module_content(module_urls):
     config_dict = dict()
     for module_url in module_urls:
@@ -34,9 +34,17 @@ sgmodules = ['https://raw.githubusercontent.com/sub-store-org/Sub-Store/master/c
 config_combine = get_module_content(sgmodules)
 
 
-with open('./sgmodule/ami.sgmodule', 'w') as f:
-    f.writelines(['#!name=ami@Surge\n', '#!desc=Some thing.\n', '\n'])
+config_combine = get_module_content(sgmodules)
+
+
+# write to file
+with open('combine.sgmodule', 'w') as f:
+    f.writelines(['#!name=CombineModule\n', '#!desc=sgmodule combined\n'])
     for section_key in config_combine:
         f.write(section_key + '\n')
-        f.writelines(config_combine[section_key])
+        if section_key == '[MITM]':
+            f.write('hostname = %APPEND% ' + ','.join(
+                [item.split('%APPEND%')[-1].rstrip() for item in config_combine[section_key]]) + '\n')
+        else:
+            f.writelines(config_combine[section_key])
         f.write('\n')

82189d1dba9c949789287d1eeb444f0c5fe4f3f2
ami@ <ami@futurkk.com>
2022-09-04 14:37:20 +0800

    Update SurgeWeChat.py

--- action/SurgeWeChat.py
+++ action/SurgeWeChat.py
@@ -1,9 +1,10 @@
 import requests
 
 rawWeChat = requests.get("https://raw.githubusercontent.com/NobyDa/Script/master/Surge/WeChat.list").text
+rawdd = requests.get("https://gist.githubusercontent.com/futurkk/8c63488b05ba2c6f567965531f826ca2/raw/direct.list").text
 
 result = list()
-for rawresult in [rawWeChat]:
+for rawresult in [rawWeChat, rawdd]:
     result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
 result_text = '\n'.join(result)
 

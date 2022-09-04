0c9428c226aa1992cd77ae19f46f69a13674fdb3
ami@ <ami@futurkk.com>
2022-09-04 14:41:31 +0800

    Update SurgeWeChat.py

--- action/SurgeWeChat.py
+++ action/SurgeWeChat.py
@@ -1,6 +1,6 @@
 import requests
 
-rawWeChat = requests.get("https://raw.githubusercontent.com/NobyDa/Script/master/Surge/WeChat.list").text
+rawWeChat = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/WeChat/WeChat.list").text
 rawdd = requests.get("https://gist.githubusercontent.com/futurkk/8c63488b05ba2c6f567965531f826ca2/raw/direct.list").text
 
 result = list()

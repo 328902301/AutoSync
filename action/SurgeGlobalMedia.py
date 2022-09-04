f3bfbde01d77aeb7dc162d875e4410c4c6eb6148
ami@ <ami@futurkk.com>
2022-09-04 14:30:01 +0800

    Update SurgeGlobalMedia.py

--- action/SurgeGlobalMedia.py
+++ action/SurgeGlobalMedia.py
@@ -3,9 +3,10 @@ import requests
 rawTelegram = requests.get("https://raw.githubusercontent.com/VirgilClyne/VirgilClyne/main/modules/ASN/ASN.Telegram.list").text
 rawNetflix = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Netflix/Netflix.list").text
 rawDisney = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Disney/Disney.list").text
+rawpv = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/AmazonPrimeVideo/AmazonPrimeVideo.list").text
 
 result = list()
-for rawresult in [rawTelegram, rawNetflix, rawDisney]:
+for rawresult in [rawTelegram, rawNetflix, rawDisney,rawpv]:
     result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
 result_text = '\n'.join(result)
 

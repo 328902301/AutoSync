58389f9e93025ad058c045d86aef8917fa3f626b
ami@ <ami@futurkk.com>
2022-09-04 14:28:14 +0800

    Update SurgeGlobal.py

--- action/SurgeGlobal.py
+++ action/SurgeGlobal.py
@@ -1,12 +1,9 @@
 import requests
 
 rawGlobal = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Global/Global.list").text
-rawMicrosoft = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Microsoft/Microsoft.list").text
-rawYouTube = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/YouTube/YouTube.list").text
-rawGoogle = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Google/Google.list").text
 
 result = list()
-for rawresult in [rawMicrosoft, rawYouTube, rawGoogle, rawGlobal]:
+for rawresult in [rawGlobal]:
     result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
 result_text = '\n'.join(result)
 

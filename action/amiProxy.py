d4180600e74dbdcd13798f57bfc6d49386c215b9
ami@ <ami@futurkk.com>
2022-09-04 14:27:25 +0800

    Create amiProxy.py

---
+++ action/amiProxy.py
@@ -0,0 +1,13 @@
+import requests
+
+rawMicrosoft = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Microsoft/Microsoft.list").text
+rawYouTube = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/YouTube/YouTube.list").text
+rawGoogle = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Google/Google.list").text
+
+result = list()
+for rawresult in [rawMicrosoft, rawYouTube, rawGoogle, rawGlobal]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
+
+with open("./amiProxy.conf", "w") as f:
+    f.write("\n".join(result))

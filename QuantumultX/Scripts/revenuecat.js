8ebf84550576b85b98a3f10eb0e08f60c3662235
nobady <44594562+YAOJIO@users.noreply.github.com>
2022-09-08 20:54:50 +0800

    Update revenuecat.js

--- QuantumultX/Scripts/revenuecat.js
+++ QuantumultX/Scripts/revenuecat.js
@@ -42,7 +42,13 @@ if (ua.indexOf('Planny/6828 CFNetwork') != -1) {
 if (ua.indexOf('%E8%B0%9C%E5%BA%95%E6%97%B6%E9%92%9F') != -1) {
     Body = {"request_date":"2022-09-07T10:39:48Z","request_date_ms":1662547188167,"subscriber":{"entitlements":{"Entitlement.Pro":{"expires_date":null,"grace_period_expires_date":null,"product_identifier":"tech.miidii.MDClock.pro","purchase_date":"2021-07-23T04:40:29Z"}},"first_seen":"2021-11-04T01:41:55Z","last_seen":"2022-09-07T10:39:40Z","management_url":null,"non_subscriptions":{"tech.miidii.MDClock.pro":[{"id":"9d8637e31a","is_sandbox":false,"original_purchase_date":"2021-07-23T04:40:29Z","purchase_date":"2021-07-23T04:40:29Z","store":"app_store"}]},"original_app_user_id":"$RCAnonymousID:a2b9499d9cf64aca9130c175a4e51b04","original_application_version":"364","original_purchase_date":"2021-07-23T04:39:03Z","other_purchases":{"tech.miidii.MDClock.pro":{"purchase_date":"2021-07-23T04:40:29Z"}},"subscriptions":{}}};
 }
-    
+
+//08 Darkroom
+//https://apps.apple.com/cn/app/id953286746
+if (ua.indexOf('Darkroom') != -1) {
+    Body = {"request_date":"2022-09-08T12:38:57Z","request_date_ms":1662640737186,"subscriber":{"entitlements":{"co.bergen.Darkroom.entitlement.allToolsAndFilters":{"expires_date":"2052-09-15T12:38:24Z","product_identifier":"co.bergen.Darkroom.product.year.everything","purchase_date":"2022-09-08T12:38:24Z"},"co.bergen.Darkroom.entitlement.selectiveAdjustments":{"expires_date":"2052-09-15T12:38:24Z","product_identifier":"co.bergen.Darkroom.product.year.everything","purchase_date":"2022-09-08T12:38:24Z"}},"first_seen":"2022-09-08T12:37:51Z","last_seen":"2022-09-08T12:37:51Z","non_subscriptions":{},"original_app_user_id":"$RCAnonymousID:2f245117497f4200b4683f102da726bb","original_application_version":"60600","original_purchase_date":"2022-07-30T04:21:40Z","other_purchases":{},"subscriptions":{"co.bergen.Darkroom.product.year.everything":{"billing_issues_detected_at":null,"expires_date":"2052-09-15T12:38:24Z","is_sandbox":false,"original_purchase_date":"2022-09-08T12:38:25Z","ownership_type":"PURCHASED","period_type":"trial","purchase_date":"2022-09-08T12:38:24Z","store":"app_store","unsubscribe_detected_at":null}}}};
+}
+
 Status = 'HTTP/1.1 200 OK';
 Headers = {"Content-Type": "application/json"};
 

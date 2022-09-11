bc048acb87ba114c3570d252dd27bd7bfb3f4215
ami@ <ami@futurkk.com>
2022-09-11 23:55:26 +0800

    Update qxWeChat.py

--- action/qxWeChat.py
+++ action/qxWeChat.py
@@ -1,8 +1,12 @@
 import requests
 
-rawWeChat = requests.get("https://raw.githubusercontent.com/NobyDa/Script/master/Surge/WeChat.list").text
+rawWeChat = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/WeChat/WeChat.list").text
+rawdd = requests.get("https://gist.githubusercontent.com/futurkk/8c63488b05ba2c6f567965531f826ca2/raw/direct.list").text
 
-result = rawWeChat.split("\n")
+result = list()
+for rawresult in [rawWeChat, rawdd]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
 
-with open("./quanx/amiDirect.list", "w") as f:
+with open("./quanx/WeChat.list", "w") as f:
     f.write("\n".join(result))

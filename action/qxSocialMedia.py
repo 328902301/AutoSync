1800886f0f32221efa9d9fff55a0ebe2c5610e60
ami@ <ami@futurkk.com>
2022-09-11 23:57:52 +0800

    Update qxSocialMedia.py

--- action/qxSocialMedia.py
+++ action/qxSocialMedia.py
@@ -4,8 +4,7 @@ rawBiliBili = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_r
 rawBiliBilintl = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/BiliBiliIntl/BiliBiliIntl.list").text
 rawSpotify = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Spotify/Spotify.list").text
 rawTwitch = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Twitch/Twitch.list").text
-rawpv = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/PrimeVideo/PrimeVideo.list").text
-
+rawpv = requests.get("https://gist.githubusercontent.com/futurkk/9246989de0da961983fc17f4fc3d123a/raw/media.list").text
 
 result = rawBiliBili.split("\n") + rawBiliBilintl.split("\n") + rawSpotify.split("\n") + rawTwitch.split("\n") + rawpv.split("\n")
 

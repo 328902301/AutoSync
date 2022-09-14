25bb12cb2b5ff140035d7ec33d808759728c8a4e
nobady <44594562+YAOJIO@users.noreply.github.com>
2022-09-14 22:17:48 +0800

    Update RevenueCat.js

--- QuantumultX/Scripts/RevenueCat.js
+++ QuantumultX/Scripts/RevenueCat.js
@@ -33,7 +33,7 @@ if (ua.indexOf('VSCO') != -1) {
 
 //06 Planny 6
 //https://apps.apple.com/cn/app/id1515324201
-if (ua.indexOf('Planny/6828 CFNetwork') != -1) {
+if (ua.indexOf('Planny') != -1) {
     Body = {"request_date":"2022-07-31T12:39:35Z","request_date_ms":1659271175185,"subscriber":{"entitlements":{"premium":{"expires_date":null,"grace_period_expires_date":null,"product_identifier":"com.kevinreutter.Sagittarius.PremiumLifetime","purchase_date":"2022-07-31T12:38:21Z"}},"first_seen":"2022-07-31T12:37:44Z","last_seen":"2022-07-31T12:37:44Z","management_url":null,"non_subscriptions":{"com.kevinreutter.Sagittarius.PremiumLifetime":[{"id":"e69f79ea37","is_sandbox":false,"original_purchase_date":"2022-07-31T12:38:21Z","purchase_date":"2022-07-31T12:38:21Z","store":"app_store"}]},"original_app_user_id":"$RCAnonymousID:7d194fd555aa4d399b98609fa633de22","original_application_version":"1.0","original_purchase_date":"2013-08-01T07:00:00Z","other_purchases":{"com.kevinreutter.Sagittarius.PremiumLifetime":{"purchase_date":"2022-07-31T12:38:21Z"}},"subscriptions":{}}};
 }
 

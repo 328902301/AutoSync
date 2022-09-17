fda387cdb10e9c9ce6a28eef9095f15e798f908e
king <77053523+nameking77@users.noreply.github.com>
2022-09-17 17:48:01 +0800

    Create ylgy.js

---
+++ rewrite/ylgy.js
@@ -0,0 +1,17 @@
+
+
+
+/******************************
+
+[rewrite_local]
+
+^https?:\/\/cat\-match\.easygame2021\.com\/sheep\/v1\/game\/(map|personal)\_info?(?!map_id=80001) url script-response-body ylgy.js
+
+[mitm] 
+
+hostname = cat-match-static.easygame2021.com
+cb6bb31d-ff5f-4cb9-b0a5-64018e70a0ca.c0636.json
+
+
+
+*******************************/

85a2edc761fae9c1787a4b41c7e246d4da405935
king <77053523+nameking77@users.noreply.github.com>
2022-09-17 17:49:10 +0800

    Update ylgy.js

--- rewrite/ylgy.js
+++ rewrite/ylgy.js
@@ -5,13 +5,10 @@
 
 [rewrite_local]
 
-^https?:\/\/cat\-match\.easygame2021\.com\/sheep\/v1\/game\/(map|personal)\_info?(?!map_id=80001) url script-response-body ylgy.js
+^https?:\/\/cat\-match\.easygame2021\.com\/sheep\/v1\/game\/(map|personal)\_info?(?!map_id=80001) url script-response-body https://raw.githubusercontent.com/nameking77/Qx/main/rewrite/ylgy.js
 
 [mitm] 
 
 hostname = cat-match-static.easygame2021.com
-cb6bb31d-ff5f-4cb9-b0a5-64018e70a0ca.c0636.json
-
-
 
 *******************************/

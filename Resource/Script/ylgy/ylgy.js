139135f759ade153f8eb580548a5f2a5859c0058
Yi Ke <11892349-lodepuly@users.noreply.gitlab.com>
2022-09-18 12:24:05 +0000

    Update ylgy.js

--- Resource/Script/ylgy/ylgy.js
+++ Resource/Script/ylgy/ylgy.js
@@ -1,10 +1,15 @@
-var body = $response.body
-var url = $request.url
-
-if (body) {
-  var obj = JSON.parse($response.body)
-  obj.data ='046ef1bab26e5b9bfe2473ded237b572'
-  $done({ body: JSON.stringify(obj) })
-} else {
-  $done({})
-}
+var Body = JSON.parse($response.body);
+switch ($request.url.match(/map|personal|json/)[0]){
+ case "map":
+ Body.data = '046ef1bab26e5b9bfe2473ded237b572';
+ break;
+ case "personal":
+ var objk = Body.data;
+objk["daily_count"] = "1e+308";
+objk["nick_name"] = "站在我下面的都是辣鸡";
+  break;
+ default:
+$done({ body: $response.body
+.replace(/false/g,'true') });
+ }
+$done({body: JSON.stringify(Body)});

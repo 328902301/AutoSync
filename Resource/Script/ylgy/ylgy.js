f69eb173200ac91e3d682182065f8147cfa72c9b
Yi Ke <11892349-lodepuly@users.noreply.gitlab.com>
2022-09-20 14:31:33 +0000

    Update ylgy.js

--- Resource/Script/ylgy/ylgy.js
+++ Resource/Script/ylgy/ylgy.js
@@ -1,19 +1,14 @@
 /*
-脚本来自Telegram的Surge Pro群组热心网友 @小白脸，由于暂无Telegram用户名，所以这里仅署名昵称。
+脚本来自https://github.com/yangdaozhao/ylgy
 */
 
-var Body = JSON.parse($response.body);
-switch ($request.url.match(/map|personal|json/)[0]){
- case "map":
- Body.data = '046ef1bab26e5b9bfe2473ded237b572';
- break;
- case "personal":
- var objk = Body.data;
-objk["daily_count"] = "1e+308";
-objk["nick_name"] = "站在我下面的都是辣鸡";
-  break;
- default:
-$done({ body: $response.body
-.replace(/false/g,'true') });
- }
-$done({body: JSON.stringify(Body)});
+var body = $response.body
+var url = $request.url
+
+if (body) {
+  var obj = JSON.parse($response.body)
+  obj.blockTypeData = {}
+  $done({ body: JSON.stringify(obj) })
+} else {
+  $done({})
+}

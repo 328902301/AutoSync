2d2645a362acaa5615a5d6f0d3415d4680bde0e4
ami@ <ami@futurkk.com>
2022-10-04 14:11:20 +0800

    Delete qxApple.py

--- action/qxApple.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawApple = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Apple/Apple.list").text
-
-result = rawApple.split("\n")
-
-with open("./quanx/Apple.list", "w") as f:
-    f.write("\n".join(result))

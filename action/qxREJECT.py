984c825651b8c583704a0cb3a38b782dcdf9d2f2
ami@ <ami@futurkk.com>
2022-10-04 14:11:56 +0800

    Delete qxREJECT.py

--- action/qxREJECT.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawREJECT = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Advertising/Advertising.list").text
-
-result = rawREJECT.split("\n")
-
-with open("./quanx/REJECT.list", "w") as f:
-    f.write("\n".join(result))

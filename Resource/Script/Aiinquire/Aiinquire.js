d009d535dafc9b47d7ef586705511f1f225f5cd9
Yi Ke <11892349-lodepuly@users.noreply.gitlab.com>
2022-10-04 17:40:05 +0000

    Update Aiinquire.js

--- Resource/Script/Aiinquire/Aiinquire.js
+++ Resource/Script/Aiinquire/Aiinquire.js
@@ -4,10 +4,6 @@
 引用地址：https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/aiqicha.js
 */
 
-^https:\//aiqicha.baidu.com\/usercenter\/getvipinfoajax url script-response-body https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/aiqicha.js
-mitm= aiqicha.baidu.com
-*/
-
 let obj = JSON.parse($response.body);
     obj.data = {
     "vip": 1,

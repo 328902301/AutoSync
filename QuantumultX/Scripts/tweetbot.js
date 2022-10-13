1ffa9f264882be5ece16237405454111c44b63d7
nobady <44594562+YAOJIO@users.noreply.github.com>
2022-10-13 11:45:51 +0800

    Add files via upload

---
+++ QuantumultX/Scripts/tweetbot.js
@@ -0,0 +1,14 @@
+/*
+https://push.tapbots.com/tweetbot/5/verify_subscription url script-response-body tweetbot.js
+
+https://github.com/Ssiswent/QuanX/blob/main/tweetbot.js
+
+hostname=*.tapbots.com
+*/
+
+let obj = JSON.parse($response.body);
+
+obj["expires_at"]= 3476097520,
+obj["can_trial"]= true,
+
+$done({body: JSON.stringify(obj)});

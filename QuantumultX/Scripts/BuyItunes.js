3308d60a80b822d9839420106963e56077d556e9
nobady <44594562+YAOJIO@users.noreply.github.com>
2022-10-20 14:30:32 +0800

    Update BuyItunes.js

--- QuantumultX/Scripts/BuyItunes.js
+++ QuantumultX/Scripts/BuyItunes.js
@@ -181,15 +181,6 @@ if (ua.indexOf('ProKnockOut') != -1) {
     $done({body:JSON.stringify(obj)});
 }
 
-//21 Panda Widget: 桌面小组件
-//https://apps.apple.com/cn/app/id1569291816
-if (ua.indexOf('SuperWidget') != -1) {
-	url = $request.url;
-    let obj = JSON.parse($response.body);
-    obj = {"receipt":{"receipt_type":"Production","app_item_id":1569291816,"receipt_creation_date":"2022-01-26 02:55:19 Etc/GMT","bundle_id":"com.heliang.super.widget","original_purchase_date":"2021-12-01 19:01:34 Etc/GMT","in_app":[{"quantity":"1","purchase_date_ms":"1637860065000","expires_date":"2099-09-09 17:07:45 Etc/GMT","expires_date_pst":"2099-09-09 09:07:45 America/Los_Angeles","is_in_intro_offer_period":"false","transaction_id":"730000766600650","is_trial_period":"false","original_transaction_id":"730000766600650","purchase_date":"2021-11-25 17:07:45 Etc/GMT","product_id":"com.focoslive","original_purchase_date_pst":"2021-11-25 09:07:48 America/Los_Angeles","in_app_ownership_type":"PURCHASED","original_purchase_date_ms":"1637860068000","web_order_line_item_id":"730000330755327","expires_date_ms":"4092647115000","purchase_date_pst":"2021-11-25 09:07:45 America/Los_Angeles","original_purchase_date":"2021-11-25 17:07:48 Etc/GMT"}],"adam_id":1569291816,"receipt_creation_date_pst":"2022-01-25 18:55:19 America/Los_Angeles","request_date":"2022-02-05 17:13:48 Etc/GMT","request_date_pst":"2022-02-05 09:13:48 America/Los_Angeles","version_external_identifier":846821007,"request_date_ms":"1644081228639","original_purchase_date_pst":"2021-12-01 11:01:34 America/Los_Angeles","application_version":"40","original_purchase_date_ms":"1638385294000","receipt_creation_date_ms":"1643165719000","original_application_version":"22","download_id":500935934584949200},"pending_renewal_info":[{"product_id":"com.focoslive","original_transaction_id":"730000766600650","auto_renew_product_id":"com.focoslive","auto_renew_status":"1"}],"status":0,"latest_receipt_info":[{"quantity":"1","purchase_date_ms":"1637860065000","expires_date":"2099-09-09 17:07:45 Etc/GMT","expires_date_pst":"2099-09-09 09:07:45 America/Los_Angeles","is_in_intro_offer_period":"false","transaction_id":"730000766600650","is_trial_period":"false","original_transaction_id":"730000766600650","purchase_date":"2021-11-25 17:07:45 Etc/GMT","product_id":"com.focoslive","original_purchase_date_pst":"2021-11-25 09:07:48 America/Los_Angeles","in_app_ownership_type":"PURCHASED","subscription_group_identifier":"20902245","original_purchase_date_ms":"1637860068000","web_order_line_item_id":"730000330755327","expires_date_ms":"4092647115000","purchase_date_pst":"2021-11-25 09:07:45 America/Los_Angeles","original_purchase_date":"2021-11-25 17:07:48 Etc/GMT"}],"latest_receipt":""};
-    $done({body:JSON.stringify(obj)});
-}
-
 //22 Bazaart百色特 - 照片与视频编辑器
 //https://apps.apple.com/cn/app/id515094775
 if (ua.indexOf('bazaart') != -1) {

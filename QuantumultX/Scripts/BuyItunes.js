8132623fcc9df779057e1d7080a4a0d3c6eaa3bc
nobady <44594562+YAOJIO@users.noreply.github.com>
2022-10-21 21:44:10 +0800

    Update BuyItunes.js

--- QuantumultX/Scripts/BuyItunes.js
+++ QuantumultX/Scripts/BuyItunes.js
@@ -325,16 +325,7 @@ if (ua.indexOf('SpeedTest') != -1) {
 	$done({body:JSON.stringify(obj)});
 }
 
-//37 Battery Widget & Color Widgets
-//https://apps.apple.com/cn/app/id1532637143
-if (ua.indexOf('SystemWidget/3.2 (co.vulcanlabs.batterywidget; build:3; iOS 15.7.0) Alamofire/5.2.2') != -1) {
-	url = $request.url;
-	let obj = JSON.parse($response.body);
-	obj = {"environment":"Production","receipt":{"receipt_type":"Production","adam_id":1532637143,"app_item_id":1532637143,"bundle_id":"co.vulcanlabs.batterywidget","application_version":"3","download_id":501348947990994300,"version_external_identifier":840820766,"receipt_creation_date":"2022-04-26 17:03:59 Etc/GMT","receipt_creation_date_ms":"1650992639000","receipt_creation_date_pst":"2022-04-26 10:03:59 America/Los_Angeles","request_date":"2022-04-26 17:04:18 Etc/GMT","request_date_ms":"1650992658490","request_date_pst":"2022-04-26 10:04:18 America/Los_Angeles","original_purchase_date":"2022-04-26 16:11:03 Etc/GMT","original_purchase_date_ms":"1650989463000","original_purchase_date_pst":"2022-04-26 09:11:03 America/Los_Angeles","original_application_version":"3","in_app":[{"quantity":"1","product_id":"co.vulcanlabs.batterywidget.lifetime","transaction_id":"190001276009392","original_transaction_id":"190001276009392","purchase_date":"2022-04-26 17:03:58 Etc/GMT","purchase_date_ms":"1650992638000","purchase_date_pst":"2022-04-26 10:03:58 America/Los_Angeles","original_purchase_date":"2022-04-26 17:03:59 Etc/GMT","original_purchase_date_ms":"1650992639000","original_purchase_date_pst":"2022-04-26 10:03:59 America/Los_Angeles","expires_date":"2029-04-26 17:54:33 Etc/GMT","expires_date_ms":"1871891673000","expires_date_pst":"2029-04-26 10:54:33 America/Los_Angeles","web_order_line_item_id":"190000553653902","is_trial_period":"true","is_in_intro_offer_period":"false","in_app_ownership_type":"PURCHASED"}]},"latest_receipt_info":[{"quantity":"1","product_id":"co.vulcanlabs.batterywidget.lifetime","transaction_id":"190001276009392","original_transaction_id":"190001276009392","purchase_date":"2022-04-26 17:03:58 Etc/GMT","purchase_date_ms":"1650992638000","purchase_date_pst":"2022-04-26 10:03:58 America/Los_Angeles","original_purchase_date":"2022-04-26 17:03:59 Etc/GMT","original_purchase_date_ms":"1650992639000","original_purchase_date_pst":"2022-04-26 10:03:59 America/Los_Angeles","expires_date":"2029-04-26 17:54:33 Etc/GMT","expires_date_ms":"1871891673000","expires_date_pst":"2029-04-26 10:54:33 America/Los_Angeles","web_order_line_item_id":"190000553653902","is_trial_period":"true","is_in_intro_offer_period":"false","in_app_ownership_type":"PURCHASED","subscription_group_identifier":"20689215"}],"latest_receipt":"","pending_renewal_info":[{"auto_renew_product_id":"co.vulcanlabs.batterywidget.lifetime","product_id":"co.vulcanlabs.batterywidget.lifetime","original_transaction_id":"190001276009392","auto_renew_status":"1"}],"status":0};
-	$done({body:JSON.stringify(obj)});
-}
-
-//38 VPN-Clean super proxy master
+//37 VPN-Clean super proxy master
 //https://apps.apple.com/us/app/id1446749209
 if (ua.indexOf('VPN%20Clean') != -1) {
 	url = $request.url;
@@ -343,7 +334,7 @@ if (ua.indexOf('VPN%20Clean') != -1) {
 	$done({body:JSON.stringify(obj)});
 }
 
-//39 TOR Browser: OrNET Onion + VPN
+//38 TOR Browser: OrNET Onion + VPN
 //https://apps.apple.com/us/app/id1177964608
 if (ua.indexOf('TorBrowser') != -1) {
 	url = $request.url;
@@ -352,7 +343,7 @@ if (ua.indexOf('TorBrowser') != -1) {
 	$done({body:JSON.stringify(obj)});
 }
 
-//40 Voloco：人声录音室
+//39 Voloco：人声录音室
 //https://apps.apple.com/cn/app/id1052970183
 if (ua.indexOf('Voloco') != -1) {
 	url = $request.url;
@@ -361,7 +352,7 @@ if (ua.indexOf('Voloco') != -1) {
 	$done({body:JSON.stringify(obj)});
 }
 
-//41 水提醒-Water Reminder
+//40 水提醒-Water Reminder
 //https://apps.apple.com/cn/app/id1221965482
 if (ua.indexOf('Water%20Reminder') != -1) {
 	url = $request.url;
@@ -370,7 +361,7 @@ if (ua.indexOf('Water%20Reminder') != -1) {
 	$done({body:JSON.stringify(obj)});
 }
 
-//42 WaterMinder®∙Water Tracker
+//41 WaterMinder®∙Water Tracker
 //https://apps.apple.com/cn/app/id653031147
 if (ua.indexOf('WaterMinder') != -1) {
 	url = $request.url;
@@ -379,7 +370,7 @@ if (ua.indexOf('WaterMinder') != -1) {
 	$done({body:JSON.stringify(obj)});
 }
 
-//43 Workout for Women: Fitness App
+//42 Workout for Women: Fitness App
 //https://apps.apple.com/cn/app/id839285684
 if (ua.indexOf('OOG104') != -1) {
 	url = $request.url;

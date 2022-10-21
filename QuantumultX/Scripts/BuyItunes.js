57c2dfa96bc7545d618970a509626371fffa5dcf
nobady <44594562+YAOJIO@users.noreply.github.com>
2022-10-21 10:35:03 +0800

    Update BuyItunes.js

--- QuantumultX/Scripts/BuyItunes.js
+++ QuantumultX/Scripts/BuyItunes.js
@@ -129,14 +129,14 @@ if (ua.indexOf('vpn') != -1) {
     $done({body:JSON.stringify(obj)});
 }
 
-//14 万能小组件-Top Widgets手机桌面主题美化
-//https://apps.apple.com/us/app/id1527221228
-//if (ua.indexOf('Widget') != -1) {
-//	url = $request.url;
-//    let obj = JSON.parse($response.body);
-//    obj = {"receipt":{"receipt_type":"Production","adam_id":1208145167,"app_item_id":1208145167,"bundle_id":"com.sugarmo.ScrollClip","application_version":"3082","download_id":9999,"version_external_identifier":837747342,"receipt_creation_date":"2020-09-13 06:58:34 Etc/GMT","receipt_creation_date_ms":"1599980314000","receipt_creation_date_pst":"2020-09-12 23:58:34 America/Los_Angeles","request_date":"2020-09-13 06:59:15 Etc/GMT","request_date_ms":"1599980355799","request_date_pst":"2020-09-12 23:59:15 America/Los_Angeles","original_purchase_date":"2020-09-13 06:50:28 Etc/GMT","original_purchase_date_ms":"1599979828000","original_purchase_date_pst":"2020-09-12 23:50:28 America/Los_Angeles","original_application_version":"3082","in_app":[{"quantity":"1","product_id":"com.sugarmo.ScrollClip.pro","transaction_id":"1000000000000000","original_transaction_id":"1000000000000000","purchase_date":"2020-01-01 00:00:00 Etc/GMT","purchase_date_ms":"1587700000000","purchase_date_pst":"2020-01-21 00:00:00 America/Los_Angeles","original_purchase_date":"2020-01-01 00:00:00 Etc/GMT","original_purchase_date_ms":"1587700000000","original_purchase_date_pst":"2020-01-01 00:00:00 America/Los_Angeles","is_trial_period":"false"}]},"status":0,"environment":"Production"};
-//    $done({body:JSON.stringify(obj)});
-//}
+//14 万能小组件-Top Widgets手机桌面主题美化(配合ScriptsCrack153解锁新老版本)
+//https://apps.apple.com/cn/app/id1527221228
+if (ua.indexOf('Widget') != -1) {
+	url = $request.url;
+    let obj = JSON.parse($response.body);
+    obj = {"receipt":{"receipt_type":"Production","adam_id":1208145167,"app_item_id":1208145167,"bundle_id":"com.sugarmo.ScrollClip","application_version":"3082","download_id":9999,"version_external_identifier":837747342,"receipt_creation_date":"2020-09-13 06:58:34 Etc/GMT","receipt_creation_date_ms":"1599980314000","receipt_creation_date_pst":"2020-09-12 23:58:34 America/Los_Angeles","request_date":"2020-09-13 06:59:15 Etc/GMT","request_date_ms":"1599980355799","request_date_pst":"2020-09-12 23:59:15 America/Los_Angeles","original_purchase_date":"2020-09-13 06:50:28 Etc/GMT","original_purchase_date_ms":"1599979828000","original_purchase_date_pst":"2020-09-12 23:50:28 America/Los_Angeles","original_application_version":"3082","in_app":[{"quantity":"1","product_id":"com.sugarmo.ScrollClip.pro","transaction_id":"1000000000000000","original_transaction_id":"1000000000000000","purchase_date":"2020-01-01 00:00:00 Etc/GMT","purchase_date_ms":"1587700000000","purchase_date_pst":"2020-01-21 00:00:00 America/Los_Angeles","original_purchase_date":"2020-01-01 00:00:00 Etc/GMT","original_purchase_date_ms":"1587700000000","original_purchase_date_pst":"2020-01-01 00:00:00 America/Los_Angeles","is_trial_period":"false"}]},"status":0,"environment":"Production"};
+    $done({body:JSON.stringify(obj)});
+}
 
 //15 Fabulous: 每日动力泉源
 //https://apps.apple.com/cn/app/id1203637303

f7bcc9543ef11778129e9bb076fc2ad8298d7784
nobady <44594562+YAOJIO@users.noreply.github.com>
2022-10-22 10:32:47 +0800

    Update Subs.js

--- QuantumultX/Scripts/Subs.js
+++ QuantumultX/Scripts/Subs.js
@@ -13,12 +13,6 @@ if (ua.indexOf('Anybox') != -1) {
     Body = {"request_date":"2022-10-14T07:50:01Z","request_date_ms":1665733801390,"subscriber":{"entitlements":{"pro":{"expires_date":"2029-10-14T07:49:44Z","grace_period_expires_date":null,"product_identifier":"cc.anybox.Anybox.annual","purchase_date":"2022-10-14T07:49:44Z"}},"first_seen":"2022-10-14T07:45:36Z","last_seen":"2022-10-14T07:45:36Z","management_url":"https://apps.apple.com/account/subscriptions","non_subscriptions":{},"original_app_user_id":"$RCAnonymousID:Eric","original_application_version":"47","original_purchase_date":"2022-10-14T07:26:13Z","other_purchases":{},"subscriptions":{"cc.anybox.Anybox.annual":{"billing_issues_detected_at":null,"expires_date":"2029-10-14T07:49:44Z","grace_period_expires_date":null,"is_sandbox":false,"original_purchase_date":"2022-10-14T07:49:45Z","ownership_type":"PURCHASED","period_type":"annual","purchase_date":"2022-10-14T07:49:44Z","store":"app_store","unsubscribe_detected_at":null}}}};
 }
 
-//03 Rapchat：制作热门歌曲
-//https://apps.apple.com/cn/app/id804913240
-if (ua.indexOf('Rapchat') != -1) {
-    Body = {"request_date":"2022-10-21T22:29:14Z","request_date_ms":1666391354912,"subscriber":{"entitlements":{"Gold":{"expires_date":"2029-05-26T05:05:04Z","product_identifier":"rc_4999_1y_7d_antares","purchase_date":"2022-04-09T05:05:04Z"}},"first_seen":"2022-10-21T22:28:22Z","last_seen":"2022-10-21T22:28:34Z","management_url":null,"non_subscriptions":{},"original_app_user_id":"$RCAnonymousID:1e9e762f4c8f4f53a6e7339db8af267a","original_application_version":"10522","original_purchase_date":"2022-10-21T22:03:00Z","other_purchases":{},"subscriptions":{"rc_4999_1y_7d_antares":{"billing_issues_detected_at":null,"expires_date":"2029-05-26T05:05:04Z","is_sandbox":false,"original_purchase_date":"2022-04-09T05:05:04Z","ownership_type":"PURCHASED","period_type":"normal","purchase_date":"2022-04-09T05:05:04Z","store":"app_store","unsubscribe_detected_at":null}}}};
-}
-
 Status = 'HTTP/1.1 200 OK';
 Headers = {"Content-Type": "application/json"};
 

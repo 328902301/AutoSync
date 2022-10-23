2a31df037c530d460ae5293ed141e0e401df75e5
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2022-10-23 13:56:33 +0800

    Update FitnessView.js

--- FitnessView.js
+++ FitnessView.js
@@ -10,7 +10,7 @@ TG频道群：https://t.me/gjwj6
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body 
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/gjwj666/jichang/main/FitnessView.js
 
 [mitm] 
 

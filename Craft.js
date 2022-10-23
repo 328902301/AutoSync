34cc104a9e3ef853210730332be0598af1011840
gjwj666 <102322377+gjwj666@users.noreply.github.com>
2022-10-23 14:57:40 +0800

    Update Craft.js

--- Craft.js
+++ Craft.js
@@ -11,7 +11,7 @@ TG频道群：https://t.me/gjwj6
 
 [rewrite_local]
 
-^https:\/\/api\.craft\.do\/auth\/v2\/profile url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Craft.js
+^https:\/\/api\.craft\.do\/auth\/v2\/profile url script-response-body https://raw.githubusercontent.com/gjwj666/jichang/main/Craft.js
 
 [mitm] 
 

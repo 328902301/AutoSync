f459e2194d385d9f27abe274cd749aaea2c0db5c
king <77053523+nameking77@users.noreply.github.com>
2022-10-27 23:05:56 +0800

    Create xndw.js

---
+++ rewrite/xndw.js
@@ -0,0 +1,8 @@
+/*******************************
+
+[rewrite_local]
+^http[s]?:\/\/app.aa-ab.com\/home\/versionControl.+$ url script-response-body xndw.js
+[mitm] 
+hostname = app.aa-ab.com
+
+*******************************/

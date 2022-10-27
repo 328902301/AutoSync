5e1f30e2ab9d4b270da371d16721d8392172d802
king <77053523+nameking77@users.noreply.github.com>
2022-10-27 23:07:30 +0800

    Update xndw.js

--- rewrite/xndw.js
+++ rewrite/xndw.js
@@ -1,7 +1,7 @@
 /*******************************
 
 [rewrite_local]
-^http[s]?:\/\/app.aa-ab.com\/home\/versionControl.+$ url script-response-body xndw.js
+^http[s]?:\/\/app.aa-ab.com\/home\/versionControl.+$ url script-response-body https://raw.githubusercontent.com/nameking77/Qx/main/rewrite/xndw.js
 [mitm] 
 hostname = app.aa-ab.com
 

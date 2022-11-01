7b569b0051e246d07dbb9b9b8a3a071de8ff516b
ami@ <ami@futurkk.com>
2022-11-01 11:26:57 +0800

    Update SurgeSocialMedia.py

--- action/SurgeSocialMedia.py
+++ action/SurgeSocialMedia.py
@@ -6,10 +6,10 @@ rawBiliBilintl = requests.get("https://raw.githubusercontent.com/blackmatrix7/io
 rawSpotify = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Spotify/Spotify.list").text
 rawTwitch = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Twitch/Twitch.list").text
 raweb = requests.get("https://gist.githubusercontent.com/futurkk/9246989de0da961983fc17f4fc3d123a/raw/media.list").text
-
+rawxiaohongshu = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/XiaoHongShu/XiaoHongShu.list").text
 
 result = list()
-for rawresult in [rawYouTube, rawBiliBili, rawBiliBilintl, rawSpotify, rawTwitch, raweb]:
+for rawresult in [rawYouTube, rawBiliBili, rawBiliBilintl, rawSpotify, rawTwitch, raweb, rawxiaohongshu]:
     result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
 result_text = '\n'.join(result)
 

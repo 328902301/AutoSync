745da5aa0ae146707e0a13e263d50580437c1b87
General℡ <76241047+General74110@users.noreply.github.com>
2022-11-02 18:22:03 +0800

    Create DouYin.js

---
+++ Rule/DouYin.js
@@ -0,0 +1,12 @@
+# : DouYin
+# 原作者: blackmatrix7
+# 原作仓库: https://github.com/blackmatrix7/ios_rule_script
+# 更新时间: 2022-10-18 21:05:10
+# HOST-SUFFIX: 6
+# TOTAL: 6
+HOST-SUFFIX,amemv.com,DouYin
+HOST-SUFFIX,ixigua.com,DouYin
+HOST-SUFFIX,ixiguavideo.com,DouYin
+HOST-SUFFIX,pstatp.com,DouYin
+HOST-SUFFIX,snssdk.com,DouYin
+HOST-SUFFIX,toutiao.com,DouYin

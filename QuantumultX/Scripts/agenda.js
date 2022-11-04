d3b4612730d0c8de0b70fab53dbdcf7a72596de0
nobady <44594562+YAOJIO@users.noreply.github.com>
2022-11-04 09:59:59 +0800

    Add files via upload

---
+++ QuantumultX/Scripts/agenda.js
@@ -0,0 +1,14 @@
+let obj = JSON.parse($response.body);
+
+obj = {
+  "error": 0,
+  "universalPurchaseAvailable": true,
+  "informUserOfPendingBonus": true,
+  "licenseStatus": 1,
+  "isSubscribed": true,
+  "unlockExpiry": 63114076800,
+  "isSubscribedMobile": true,
+  "licenseStatusMobile": 1,
+  "unlockExpiryMobile": 63114076800
+}
+$done({body: JSON.stringify(obj)});

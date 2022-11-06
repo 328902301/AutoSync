5a795877429f044fd26b626234b11a8e59f3ed8c
ami@ <ami@futurkk.com>
2022-11-06 22:41:40 +0800

    Update SurgeamiProxy.py

--- action/SurgeamiProxy.py
+++ action/SurgeamiProxy.py
@@ -2,13 +2,16 @@ import requests
 
 rawBiliBili = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/BiliBili/BiliBili.list").text
 rawBiliBilintl = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/BiliBiliIntl/BiliBiliIntl.list").text
-rawxiaohongshu = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/XiaoHongShu/XiaoHongShu.list").text
 raweb = requests.get("https://gist.githubusercontent.com/futurkk/9246989de0da961983fc17f4fc3d123a/raw/media.list").text
+rawTelegram = requests.get("https://raw.githubusercontent.com/VirgilClyne/GetSomeFries/main/ruleset/ASN.Telegram.list").text
+rawNetflix = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Netflix/Netflix.list").text
+rawDisney = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Disney/Disney.list").text
+rawpv = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/AmazonPrimeVideo/AmazonPrimeVideo.list").text
 
 result = list()
-for rawresult in [rawBiliBili, rawBiliBilintl, rawxiaohongshu, raweb]:
+for rawresult in [rawBiliBili, rawBiliBilintl, raweb, rawTelegram, rawNetflix, rawDisney, rawpv]:
     result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
 result_text = '\n'.join(result)
 
-with open("./amiProxy.conf", "w") as f:
+with open("./Media.conf", "w") as f:
     f.write("\n".join(result))

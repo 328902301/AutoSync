903872b8ad8580bdc263bda35bcdfe0828f8d1ed
ddgksf2013 <jinjjseu@gmail.com>
2022-11-07 11:38:25 +0000

    Update Script/startup.js

--- Script/startup.js
+++ Script/startup.js
@@ -7,5 +7,5 @@ if (/^https?:\/\/api\.m\.jd\.com\/api\?functionId=delivery_show/.test($request.u
     let obj=JSON.parse($response.body);
     obj.data.materialList.startTime=3667476800000;
     obj.data.materialList.endTime  =3667908800000;
-    $done({body:JSON.stringify(obj)};
+    $done({body:JSON.stringify(obj)});
 }

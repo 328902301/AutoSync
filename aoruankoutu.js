839f1df58f5a7d07534e246b31978700ee2ccb7e
gujiaming2022 <59131675+gujiaming2022@users.noreply.github.com>
2022-11-11 21:11:39 +0800

    Create aoruankoutu.js

---
+++ aoruankoutu.js
@@ -0,0 +1,24 @@
+var cuttlefish ={"warning":"本腳本僅供學習交流使用，禁止轉載售賣","tgchannel":"https://t.me/ddgksf2021","feedback":"https://t.me/ddgksf2013_bot"};
+var ddgksf2013 = {
+  "status" : 200,
+  "message" : "success",
+  "data" : {
+    "expired_at" : 4045798296,
+    "is_activated" : 1,
+    "is_lifetime" : 1,
+    "expire_time" : "2099-01-01 00:00:00",
+    "device_id" : 600150864,
+    "period_type" : "active",
+    "remain_days" : 99999,
+    "product_id" : 369,
+    "has_present" : 0,
+    "allowed_device_count" : 1,
+    "has_buy_extend" : 0,
+    "will_expire" : 0,
+    "license_type" : "premium",
+    "begin_activated_time" : 1645798296,
+    "durations" : 0,
+    "vip_special" : 1
+  }
+};
+$done({body: JSON.stringify(ddgksf2013)});

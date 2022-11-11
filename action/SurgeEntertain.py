78e0671455b4d46fb7336f8a0415d26455f5114d
ami@ <ami@futurkk.com>
2022-11-11 19:06:03 +0800

    Update SurgeEntertain.py

--- action/SurgeEntertain.py
+++ action/SurgeEntertain.py
@@ -1,13 +1,11 @@
 import requests
 
 rawYouTube = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/YouTube/YouTube.list").text
-rawSpotify = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Spotify/Spotify.list").text
-rawTwitch = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Twitch/Twitch.list").text
 rawMicrosoft = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Microsoft/Microsoft.list").text
 rawGoogle = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Google/Google.list").text
 
 result = list()
-for rawresult in [rawYouTube, rawSpotify, rawTwitch, rawMicrosoft, rawGoogle]:
+for rawresult in [rawYouTube, rawMicrosoft, rawGoogle]:
     result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
 result_text = '\n'.join(result)
 

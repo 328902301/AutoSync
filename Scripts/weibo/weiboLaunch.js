7f26a8647ab03656c8d3f1c54e95f7de9273bc36
Cube <49463634+RuCu6@users.noreply.github.com>
2022-11-18 12:16:21 +0800

    Update weiboLaunch.js

--- Scripts/weibo/weiboLaunch.js
+++ Scripts/weibo/weiboLaunch.js
@@ -1,22 +1,25 @@
 const path1 = "/interface/sdk/sdkad.php";
 const path2 = "/wbapplua/wbpullad.lua";
-var obj = JSON.parse($response.body)
+var url = $request.url;
+var body = $response.body;
 
-if ($request.url.indexOf(path1) != -1) {
+if (url.indexOf(path1) != -1) {
+  if (body.match(/({.*})OK/) && body.match(/({.*})OK/)[1]) {
+  var obj = JSON.parse(body.match(/({.*})OK/)[1]);
+  if (obj.background_delay_display_time) obj.background_delay_display_time = 60*60*24*366;
   for (let item of obj['ads']) {
     item['displaytime'] = 0;
     item['begintime'] = '2040-12-27 00:00:01';
     item['endtime'] = '2040-12-27 23:59:59';
   }
-  if (obj.background_delay_display_time) obj.background_delay_display_time = 60*60*24*365;
-  if (obj.show_push_splash_ad) obj.show_push_splash_ad = false;
-  if (obj.ads) obj.ads = [];
-  body = JSON.stringify(obj) + 'OK';
-}
-
-if ($request.url.indexOf(path2) != -1) {
-  if (obj.cached_ad && obj.cached_ad.ads) obj.cached_ad.ads = [];
+  $done({body: `${JSON.stringify(obj)}OK`});
+} else if (url.indexOf(path2) != -1) {
+  var obj = JSON.parse(body);
+  for (let item of obj['cached_ad']) {
+    item['ads']['start_date'] = 2239372800;
+    item['ads']['duration'] = 0
+    item['ads']['end_date'] = 2239459199;
+  }
   body = JSON.stringify(obj);
-}
-
-$done({body});
+} else {
+$done({});

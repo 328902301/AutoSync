4f48ea6b06d5948f2b1fa6d80fe13ecb1313d7e6
Cube <49463634+RuCu6@users.noreply.github.com>
2022-11-18 12:17:50 +0800

    Update weiboLaunch.js

--- Scripts/weibo/weiboLaunch.js
+++ Scripts/weibo/weiboLaunch.js
@@ -5,13 +5,15 @@ var body = $response.body;
 
 if (url.indexOf(path1) != -1) {
   if (body.match(/({.*})OK/) && body.match(/({.*})OK/)[1]) {
-  var obj = JSON.parse(body.match(/({.*})OK/)[1]);
-  if (obj.background_delay_display_time) obj.background_delay_display_time = 60*60*24*366;
-  for (let item of obj['ads']) {
-    item['displaytime'] = 0;
-    item['begintime'] = '2040-12-27 00:00:01';
-    item['endtime'] = '2040-12-27 23:59:59';
-  }
+    var obj = JSON.parse(body.match(/({.*})OK/)[1]);
+    if (obj.background_delay_display_time) {
+      obj.background_delay_display_time = 60*60*24*366;
+    }
+    for (let item of obj['ads']) {
+      item['displaytime'] = 0;
+      item['begintime'] = '2040-12-27 00:00:01';
+      item['endtime'] = '2040-12-27 23:59:59';
+    }
   $done({body: `${JSON.stringify(obj)}OK`});
 } else if (url.indexOf(path2) != -1) {
   var obj = JSON.parse(body);

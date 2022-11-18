5b349ba914d00083f3c6abba464b3006b5b0c5d2
Cube <49463634+RuCu6@users.noreply.github.com>
2022-11-18 11:11:44 +0800

    Update weiboLaunch.js

--- Scripts/weibo/weiboLaunch.js
+++ Scripts/weibo/weiboLaunch.js
@@ -1,18 +1,22 @@
-// https://github.com/yjqiang/surge_scripts/tree/main/scripts/weibo/weibo_sdkad.js
+const path1 = "/interface/sdk/sdkad.php";
+const path2 = "/wbapplua/wbpullad.lua";
+var obj = JSON.parse($response.body)
 
-if ($response.body.match(/({.*})OK/) && $response.body.match(/({.*})OK/)[1]) {
-  let obj = JSON.parse($response.body.match(/({.*})OK/)[1]);  // $response.body： {json..,}OK
-  if (obj.background_delay_display_time) {
-    obj.background_delay_display_time = 60*60*24*365;
-  }
+if ($request.url.indexOf(path1) != -1) {
   for (let item of obj['ads']) {
-    // console.log(`${item['begintime']} -- ${item['endtime']}`);
-    item['displaytime'] = 0;  // 显示时间
-    // 2040 年
+    item['displaytime'] = 0;
     item['begintime'] = '2040-12-27 00:00:01';
     item['endtime'] = '2040-12-27 23:59:59';
   }
-  $done({body: `${JSON.stringify(obj)}OK`});
-} else {
-  $done({});
+  if (obj.background_delay_display_time) obj.background_delay_display_time = 60*60*24*365;
+  if (obj.show_push_splash_ad) obj.show_push_splash_ad = false;
+  if (obj.ads) obj.ads = [];
+  body = JSON.stringify(obj) + 'OK';
 }
+
+if ($request.url.indexOf(path2) != -1) {
+  if (obj.cached_ad && obj.cached_ad.ads) obj.cached_ad.ads = [];
+  body = JSON.stringify(obj);
+}
+
+$done({body});

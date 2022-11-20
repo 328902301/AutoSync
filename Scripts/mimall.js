975c8cc8244be57983a2321e105adeac1ccbadae
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2022-11-20 06:36:47 +0000

    Update mimall.js

--- Scripts/mimall.js
+++ Scripts/mimall.js
@@ -2,7 +2,7 @@
 引用地址 https://github.com/githubacct001/QuantumultX/blob/secret/Rewrite/XiaomiMall/XiaomiMall_Open.js
 
 [rewrite_local]
-^https:\/\/api\.m\.mi\.com\/v1\/app\/start url script-response-body https://gitlab.com/RuCu6/QuanX/-/raw/main/Scripts/mimall.js
+^https:\/\/api\.m\.mi\.com\/v1\/app\/start$ url script-response-body https://gitlab.com/RuCu6/QuanX/-/raw/main/Scripts/mimall.js
 
 [mitm]
 hostname = api.m.mi.com

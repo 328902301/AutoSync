62955abb16be351f522e243a8ebcbf6925ab8e94
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2022-11-20 07:42:58 +0000

    Update mimall.js

--- Scripts/mimall.js
+++ Scripts/mimall.js
@@ -1,47 +1,13 @@
 // 鸣谢 @githubacct001
 
 let obj = JSON.parse($response.body);
-obj = {
-  'code': 0,
-  'data': {
-    'skip_splash': true,
-    'preload_plugins': '249,101,102,109',
-    'clear_apis': [
-      'http:\/\/api.m.mi.com\/v1\/home\/appInfov2',
-      'http:\/\/api.m.mi.com\/v1\/home\/tab_page',
-      'http:\/\/api.m.mi.com\/v1\/home\/appInfo',
-      'http:\/\/api.m.mi.com\/v1\/home\/recommendChannel',
-      'http:\/\/api.m.mi.com\/v1\/product\/productView2⁣'
-    ],
-    'appMode': {
-      'appModes': [
-        {
-          'pluginMapInfos': [
-            {
-              'srcPid': '100',
-              'dstPid': '100001'
-            },
-            {
-              'srcPid': '101',
-              'dstPid': '100002'
-            }
-          ],
-          'modeName': 'mijia'
-        }
-      ]
-    },
-    'api_version': 1,
-    'api_timeoutms': 5000,
-    'Motto': '',
-    'page_guide': {
-      'img_url': null,
-      'show_skip_btn': false,
-      'skip_btn': ''
-    },
-    'agreement_change': {
-    }
-  },
-  'info': 'ok',
-  'desc': '成功'
-}
+
+obj.code = 0;
+if (obj.data.skip_splash && obj.data.splash) {
+  obj.data.skip_splash = true,
+  obj.data.splash = ''
+};
+obj.info = 'ok';
+obj.desc = '成功';
+
 $done({body: JSON.stringify(obj)});

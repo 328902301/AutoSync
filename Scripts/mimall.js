b15b0dd000ee70ec9b4a18297bddb6aac20b7757
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2022-11-20 07:02:20 +0000

    Replace mimall.js

--- Scripts/mimall.js
+++ Scripts/mimall.js
@@ -1,19 +1,48 @@
-/*
-引用地址 https://github.com/githubacct001/QuantumultX/blob/secret/Rewrite/XiaomiMall/XiaomiMall_Open.js
-
-[rewrite_local]
-^https:\/\/api\.m\.mi\.com\/v1\/app\/start$ url script-response-body https://gitlab.com/RuCu6/QuanX/-/raw/main/Scripts/mimall.js
-
-[mitm]
-hostname = api.m.mi.com
-*/
+// 鸣谢 @githubacct001
 
 let obj = JSON.parse($response.body);
 obj = {
   'code': 0,
   'data': {
     'skip_splash': true,
-    'splash': {},
-  }
+    'splash': {}
+    'preload_plugins': '249,101,102,109',
+    'clear_apis': [
+      'http:\/\/api.m.mi.com\/v1\/home\/appInfov2',
+      'http:\/\/api.m.mi.com\/v1\/home\/tab_page',
+      'http:\/\/api.m.mi.com\/v1\/home\/appInfo',
+      'http:\/\/api.m.mi.com\/v1\/home\/recommendChannel',
+      'http:\/\/api.m.mi.com\/v1\/product\/productView2⁣'
+    ],
+    'appMode': {
+      'appModes': [
+        {
+          'pluginMapInfos': [
+            {
+              'srcPid': '100',
+              'dstPid': '100001'
+            },
+            {
+              'srcPid': '101',
+              'dstPid': '100002'
+            }
+          ],
+          'modeName': 'mijia'
+        }
+      ]
+    },
+    'api_version': 1,
+    'api_timeoutms': 5000,
+    'Motto': '',
+    'page_guide': {
+      'img_url': null,
+      'show_skip_btn': false,
+      'skip_btn': ''
+    },
+    'agreement_change': {
+    }
+  },
+  'info': 'ok',
+  'desc': '成功'
 }
 $done({body: JSON.stringify(obj)});

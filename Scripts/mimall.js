aa7a65f2453b5c6f001f8986591478073796e76f
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2022-11-20 09:36:51 +0000

    Update mimall.js

--- Scripts/mimall.js
+++ Scripts/mimall.js
@@ -2,8 +2,8 @@ let obj = JSON.parse($response.body);
 
 obj.code = 0;
 if (obj.data.skip_splash && obj.data.splash) {
-  obj.data.skip_splash = true,
-  obj.data.splash = ''
+  obj.data.skip_splash = true;
+  obj.data.splash = [];
 };
 obj.info = 'ok';
 obj.desc = '成功';

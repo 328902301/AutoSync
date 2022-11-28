686c8538ad930121c02e5151817057ec6a937170
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2022-11-28 01:26:30 +0000

    Update splashAds.js

--- Scripts/splashAds.js
+++ Scripts/splashAds.js
@@ -10,15 +10,16 @@ if (!body) {
 // 12306
 if (url.includes('ad.12306.cn/ad/ser/getAdList')) {
   let obj = JSON.parse(body);
-  if (obj.materialsList)
+  if (obj.materialsList) {
     if (obj.materialsList.length == 1) {
       obj.materialsList[0].filePath = '';
       obj.advertParam.skipTime = 1000;
       obj.advertParam.skipTimeAgain = 5;
       obj.advertParam.showSkipBtn = 0;
-    } else if (objobj.materialsList.length > 1) {
+    } else if (obj.materialsList.length > 1) {
       obj.materialsList = [];
     }
+  }
   body = JSON.stringify(obj);
 }
 

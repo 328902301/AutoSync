d374a3aa59ecb23b6c6177961130663242e0a458
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2022-11-29 04:47:15 +0000

    Replace coolapk.js

--- Scripts/coolapk.js
+++ Scripts/coolapk.js
@@ -1,15 +1,4 @@
-/*
-version     v0.0.1
-updatetime  2022-11-04
-function    酷安去首页广告、信息流广告、评论广告
-author      kk pp
-
-[rewrite_local]
-^https?:\/\/api.coolapk.com\/v6\/(feed\/(replyList|detail)|main\/indexV8|dataList) url script-response-body https://github.com/ddgksf2013/Cuttlefish/raw/master/Script/coolapk.js
-
-[mitm]
-hostname = api.coolapk.com
-*/
+// 2022-11-29 10:30
 
 var url = $request.url;
 var body = $response.body;
@@ -18,30 +7,60 @@ if (!body) {
   $done({});
 }
 
-if (url.includes('replyList')) {
-  let obj= JSON.parse(body);
-  obj.data = Object.values(obj.data).filter(item => item.id);
-  body = JSON.stringify(obj);
+function isresponse() {
+  if (!body) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
-if ( url.includes('indexV8') ) {
-  let obj= JSON.parse(body);
-  obj.data = Object.values(obj.data).filter(item => !(item['entityTemplate']=='sponsorCard' || item.entityId==8639 || item.entityId==33066 || item.entityId==32557 || item.title.indexOf('值得买') != -1));
-  body = JSON.stringify(obj);
+switch (isresponse) {
+  // 酷安 replyList
+  case /^https?:\/\/api.coolapk.com\/v6\/feed\/replyList/.test(url):
+    try {
+      let obj = JSON.parse(body);
+      obj.data = Object.values(obj.data).filter((item) => item.id);
+      body = JSON.stringify(obj);
+    } catch (err) {
+      console.log("酷安 replyList 异常");
+    }
+    break;
+  // 酷安 index
+  case /^https?:\/\/api.coolapk.com\/v6\/main\/indexV8/.test(url):
+    try {
+      let obj = JSON.parse(body);
+      obj.data = Object.values(obj.data).filter((item) => !(
+        item["entityTemplate"] == "sponsorCard" || item.entityId == 8639 || item.entityId == 33066 || item.entityId == 32557 || item.title.indexOf("值得买") != -1
+      ));
+      body = JSON.stringify(obj);
+    } catch (err) {
+      console.log("酷安 index 异常");
+    }
+    break;
+  // 酷安 dataList
+  case /^https?:\/\/api.coolapk.com\/v6\/main\/dataList/.test(url):
+    try {
+      let obj = JSON.parse(body);
+      obj.data = Object.values(obj.data).filter((item) => !(item["entityTemplate"] == "sponsorCard" || item.title == "精选配件"));
+      body = JSON.stringify(obj);
+    } catch (err) {
+      console.log("酷安 dataList 异常");
+    }
+    break;
+  // 酷安 detail
+  case /^https?:\/\/api.coolapk.com\/v6\/feed\/detail/.test(url):
+    try {
+      let obj = JSON.parse(body);
+      obj.data.hotReplyRows = Object.values(obj.data.hotReplyRows).filter((item) => item["id"]);
+      obj.data.include_goods_ids = [];
+      obj.data.include_goods = [];
+      body = JSON.stringify(obj);
+    } catch (err) {
+      console.log("酷安 detail 异常");
+    }
+    break;
+  default:
 }
 
-if (url.includes('dataList')) {
-  let obj= JSON.parse(body);
-  obj.data = Object.values(obj.data).filter(item => !(item['entityTemplate']=='sponsorCard' || item.title=='精选配件'));
-  body = JSON.stringify(obj);
-}
-
-if (url.includes('detail')) {
-  let obj= JSON.parse(body);
-  obj.data.hotReplyRows = Object.values(obj.data.hotReplyRows).filter(item => item['id']);
-  obj.data.include_goods_ids = [];
-  obj.data.include_goods = [];
-  body = JSON.stringify(obj);
-}
-
-$done({body});
+$done({ body });

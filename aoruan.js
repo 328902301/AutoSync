10dc873872e65315e4f173652b15ae8d9f794006
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-11-30 18:47:50 +0800

    Update aoruan.js

--- aoruan.js
+++ aoruan.js
@@ -1,35 +1,33 @@
 /****************************************
 
-项目功能：傲软全家桶
-
-项目名称：傲软抠图 解锁Vip
+项目名称：傲软抠图
 下载地址：https://t.cn/A6xBOE5d
 
-项目名称：傲软扫描 解锁Vip
+项目名称：傲软扫描
 下载地址：https://t.cn/A6o1jHWR
 
-项目名称：傲软PDF转换 解锁Vip
+项目名称：傲软PDF转换
 下载地址：https://t.cn/A6o1j588
 
-项目名称：傲软PDF编辑 解锁Vip
+项目名称：傲软PDF编辑
 下载地址：https://t.cn/A6o1jCGU
 
-项目名称：傲软投屏 解锁Vip
+项目名称：傲软投屏
 下载地址：https://t.cn/A65nw9gx
 
-项目名称：咖映 解锁Vip
+项目名称：咖映
 下载地址：https://t.cn/A6o1lsFL
 
-项目名称：轻闪PDF 解锁Vip
+项目名称：轻闪PDF
 下载地址：https://t.cn/A6o1iiI2
 
-项目名称：乃糖小组件 解锁Vip
+项目名称：乃糖小组件
 下载地址：https://t.cn/A6o1iMdP
 
-项目名称：佐糖 解锁Vip
+项目名称：佐糖
 下载地址：https://t.cn/A6o1iVTI
 
-项目名称：佐糖照片修复 解锁Vip
+项目名称：佐糖照片修复
 下载地址：https://t.cn/A6o1itzG
 
 使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
@@ -38,7 +36,7 @@
 
 [rewrite_local]
 
-^https?:\/\/.*\.aoscdn\.com\/base\/vip\/client\/authorizations url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/Rewrite/aoruan.js
+^https?:\/\/.*\.aoscdn\.com\/base\/vip\/client\/authorizations url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/aoruan.js
 
 [mitm] 
 

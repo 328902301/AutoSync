a0da7d698fd0304a86e57f34eac37bed39fbf3ec
lodepuly <admin@qq.com>
2022-12-08 14:11:45 +0800

    no message

--- Resource/Script/TestFlight/TF_keys.js
+++ Resource/Script/TestFlight/TF_keys.js
@@ -19,8 +19,8 @@ $persistentStore.write(session_id, 'session_id')
 $persistentStore.write(session_digest, 'session_digest')
 $persistentStore.write(request_id, 'request_id')
 if ($persistentStore.read('request_id') != null) {
-  $notification.post('请关闭本脚本', '信息获取成功','')
+  $notification.post('TF信息获取', '信息获取成功，请关闭脚本！','')
 } else {
-  $notification.post('信息获取失败','请打开Mitm over HTTP2开关，并重启VPN和TestFlight App','')
+  $notification.post('TF信息获取','信息获取失败，请打开Mitm over HTTP2开关，并重启VPN和TestFlight App！','')
 }
 $done({})

fa8d242b36403c6c3023372cefc3f2f2c43317c3
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2022-12-09 13:14:51 +0000

    fix 顶部直播

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -1,5 +1,5 @@
 // https://github.com/zmqcherish/proxy-script/blob/main/weibo_main.js
-// 2022-12-09 21:10
+// 2022-12-09 21:15
 
 // 主要的选项配置
 const mainConfig = {
@@ -450,7 +450,9 @@ function removeMediaHomelist(data) {
   if (mainConfig.removeLiveMedia) {
     log("remove 首页直播");
     if (data.data && data.data.is_top) {
-      data.data.is_top = 0;
+      for (let i = 0; i < data.data.length; i++) {
+        data.data.is_top[i] = 0;
+      }
     }
   }
 }

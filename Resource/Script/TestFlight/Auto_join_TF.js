d62700b1712fd2d3ac311ae3b2e7fc03c036a147
lodepuly <admin@qq.com>
2022-12-11 02:23:04 +0800

    no message

--- Resource/Script/TestFlight/Auto_join_TF.js
+++ Resource/Script/TestFlight/Auto_join_TF.js
@@ -4,21 +4,14 @@
 感谢某位大佬将改写为Loon版脚本！
 */
 !(async () => {
-var ids = $persistentStore.read('APP_ID')
-if (ids == null) {
-    $notification.post('请填写要加入的TestFlight App ID','','')
-    $done()
-    return
-}
-
+ids = $persistentStore.read('APP_ID')
 if (ids == '') {
-	$notification.post('所有TF已加入完毕','请禁用插件','')
-	$done()
+  $notification.post('所有TF已加入完毕','请手动禁用该模块','')
 } else {
-	ids = ids.split(',')
-	for await (const ID of ids) {
-		await autoPost(ID)
-	}
+  ids = ids.split(',')
+  for await (const ID of ids) {
+    await autoPost(ID)
+  }
 }
 $done()
 })();
@@ -30,29 +23,37 @@ function autoPost(ID) {
     'X-Session-Id': `${$persistentStore.read('session_id')}`,
     'X-Session-Digest': `${$persistentStore.read('session_digest')}`,
     'X-Request-Id': `${$persistentStore.read('request_id')}`,
-    'User-Agent': `iOS Loon`
+    'User-Agent': `${$persistentStore.read('tf_ua')}`,
   }
-  console.log('testurl' + testurl)
   return new Promise(function(resolve) {
     $httpClient.get({url: testurl + ID,headers: header}, function(error, resp, data) {
       if (error === null) {
-        let jsonData = JSON.parse(data)
-        if (jsonData.data == null) {
-          console.log(ID + ' ' + jsonData.messages[0].message)
-          resolve();
-        } else if (jsonData.data.status == 'FULL') {
-          console.log(ID + ' ' + jsonData.data.message)
-          resolve();
+        if (resp.status == 404) {
+          ids = $persistentStore.read('APP_ID').split(',')
+          ids = ids.filter(ids => ids !== ID)
+          $persistentStore.write(ids.toString(),'APP_ID')
+          console.log(ID + ' ' + '不存在该TF，已自动删除该APP_ID')
+          $notification.post(ID, '不存在该TF', '已自动删除该APP_ID')
+          resolve()
         } else {
-          $httpClient.post({url: testurl + ID + '/accept',headers: header}, function(error, resp, body) {
-            let jsonBody = JSON.parse(body)
-            $notification.post(jsonBody.data.name, 'TestFlight加入成功', '')
-						console.log(jsonBody.data.name + ' TestFlight加入成功')
-						ids = $persistentStore.read('APP_ID').split(',')
-						ids = ids.filter(ids => ids !== ID)
-						$persistentStore.write(ids.toString(),'APP_ID')
-						resolve()
-          });
+          let jsonData = JSON.parse(data)
+          if (jsonData.data == null) {
+            console.log(ID + ' ' + jsonData.messages[0].message)
+            resolve();
+          } else if (jsonData.data.status == 'FULL') {
+            console.log(jsonData.data.app.name + ' ' + ID + ' '+ jsonData.data.message)
+            resolve();
+          } else {
+            $httpClient.post({url: testurl + ID + '/accept',headers: header}, function(error, resp, body) {
+              let jsonBody = JSON.parse(body)
+              $notification.post(jsonBody.data.name, 'TestFlight加入成功', '')
+              console.log(jsonBody.data.name + ' TestFlight加入成功')
+              ids = $persistentStore.read('APP_ID').split(',')
+              ids = ids.filter(ids => ids !== ID)
+              $persistentStore.write(ids.toString(),'APP_ID')
+              resolve()
+            });
+          }
         }
       } else {
         if (error =='The request timed out.') {

92c8639c8847a117b72d85a5e40860d8a261b80e
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2022-12-12 03:20:51 +0000

    Update weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -125,40 +125,38 @@ function isAd(data) {
 function removeSplashAds(data) {
   if (!data.ads || !data.cached_ad) return data;
   if (url.includes("sdkapp.uve.weibo.com/interface/sdk/sdkad.php")) {
-    let obj = JSON.parse(data);
-    if (obj.needlocation) obj.needlocation = false;
-    if (obj.show_push_splash_ad) obj.show_push_splash_ad = false;
-    if (obj.code) obj.code = 200;
-    if (obj.background_delay_display_time) {
-      obj.background_delay_display_time = 31536000; // 60 * 60 * 24 * 365 = 31536000
+    if (data.needlocation) obj.needlocation = false;
+    if (data.show_push_splash_ad) data.show_push_splash_ad = false;
+    if (data.code) data.code = 200;
+    if (data.background_delay_display_time) {
+      data.background_delay_display_time = 31536000; // 60 * 60 * 24 * 365 = 31536000
     }
-    if (obj.lastAdShow_delay_display_time) {
-      obj.lastAdShow_delay_display_time = 31536000;
+    if (data.lastAdShow_delay_display_time) {
+      data.lastAdShow_delay_display_time = 31536000;
     }
-    if (obj.realtime_ad_video_stall_time) {
-      obj.realtime_ad_video_stall_time = 31536000;
+    if (data.realtime_ad_video_stall_time) {
+      data.realtime_ad_video_stall_time = 31536000;
     }
-    if (obj.realtime_ad_timeout_duration) {
-      obj.realtime_ad_timeout_duration = 31536000;
+    if (data.realtime_ad_timeout_duration) {
+      data.realtime_ad_timeout_duration = 31536000;
     }
-    for (let item of obj["ads"]) {
+    for (let item of data["ads"]) {
       item["displaytime"] = 0;
       item["displayintervel"] = 31536000;
       item["allowdaydisplaynum"] = 0;
       item["begintime"] = "2040-01-01 00:00:00";
       item["endtime"] = "2040-01-01 23:59:59";
     }
-    return JSON.stringify(obj) + "OK";
+    return data + "OK";
   }
   if (url.includes("wbapp.uve.weibo.com/wbapplua/wbpullad.lua")) {
-    let obj = JSON.parse(data);
-    for (let item of obj["cached_ad"]["ads"]) {
+    for (let item of data["cached_ad"]["ads"]) {
       item["start_date"] = 2208960000; // Unix 时间戳 2040-01-01 00:00:00
       item["show_count"] = 0;
       item["duration"] = 31536000; // 60 * 60 * 24 * 365 = 31536000
       item["end_date"] = 2209046399; // Unix 时间戳 2040-01-01 23:59:59
     }
-    return JSON.stringify(obj);
+    return data;
   }
   return data;
 }

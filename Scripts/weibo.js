9429a3364a3d2be1399ad08acd0af6a5739262ea
Cube <49463634+RuCu6@users.noreply.github.com>
2022-12-12 11:08:49 +0800

    Update weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -124,9 +124,42 @@ function isAd(data) {
 // 开屏广告
 function removeSplashAds(data) {
   if (!data.match(/\{.*\}/) || !data.cached_ad) return data;
-  if(url.includes("sdkapp.uve.weibo.com/interface/sdk/sdkad.php")) {
+  if (url.includes("sdkapp.uve.weibo.com/interface/sdk/sdkad.php")) {
+    data = data.match(/\{.*\}/);
+    let obj = JSON.parse(data);
+    if (obj.needlocation) obj.needlocation = false;
+    if (obj.show_push_splash_ad) obj.show_push_splash_ad = false;
+    if (obj.code) obj.code = 200;
+    if (obj.background_delay_display_time) {
+      obj.background_delay_display_time = 31536000; // 60 * 60 * 24 * 365 = 31536000
+    }
+    if (obj.lastAdShow_delay_display_time) {
+      obj.lastAdShow_delay_display_time = 31536000;
+    }
+    if (obj.realtime_ad_video_stall_time) {
+      obj.realtime_ad_video_stall_time = 31536000;
+    }
+    if (obj.realtime_ad_timeout_duration) {
+      obj.realtime_ad_timeout_duration = 31536000;
+    }
+    for (let item of obj["ads"]) {
+      item["displaytime"] = 0;
+      item["displayintervel"] = 31536000;
+      item["allowdaydisplaynum"] = 0;
+      item["begintime"] = "2040-01-01 00:00:00";
+      item["endtime"] = "2040-01-01 23:59:59";
+    }
+    return JSON.stringify(obj) + "OK";
   }
-  if(url.includes("wbapp.uve.weibo.com/wbapplua/wbpullad.lua")) {
+  if (url.includes("wbapp.uve.weibo.com/wbapplua/wbpullad.lua")) {
+    let obj = JSON.parse(data);
+    for (let item of obj["cached_ad"]["ads"]) {
+      item["start_date"] = 2208960000; // Unix 时间戳 2040-01-01 00:00:00
+      item["show_count"] = 0;
+      item["duration"] = 31536000; // 60 * 60 * 24 * 365 = 31536000
+      item["end_date"] = 2209046399; // Unix 时间戳 2040-01-01 23:59:59
+    }
+    return JSON.stringify(obj);
   }
   return data;
 }

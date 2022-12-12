5d0cc988939ab63dad8378ef3fcefc9c2f146e2e
Cube <49463634+RuCu6@users.noreply.github.com>
2022-12-12 17:49:36 +0800

    Update myBlockAds.js

--- Scripts/myBlockAds.js
+++ Scripts/myBlockAds.js
@@ -36,8 +36,6 @@ function adAppName(adUrls) {
   if (/^https?:\/\/s-api\.smzdm\.com\/sou\/filter\/tags\/hot_tags/.test(adUrls)) return "什么值得买-搜索标签广告";
   if (/^https?:\/\/s-api\.smzdm\.com\/sou\/list_v10/.test(adUrls)) return "什么值得买-搜索结果广告";
   if (/^https?:\/\/zhiyou\.m\.smzdm\.com\/user\/vip\/ajax_get_banner/.test(adUrls)) return "什么值得买-会员权益中心banner广告";
-  // if (/^https?:\/\/sdkapp\.uve\.weibo\.com\/interface\/sdk\/sdkad\.php/.test(adUrls)) return "微博-开屏广告-sdkad";
-  // if (/^https?:\/\/wbapp\.uve\.weibo\.com\/wbapplua\/wbpullad\.lua/.test(adUrls)) return "微博-开屏广告-wbpullad";
   if (/^https?:\/\/hd\.mina\.mi\.com\/splashscreen\/alert/.test(adUrls)) return "小爱音箱-开屏广告";
   if (/^https?:\/\/edith\.xiaohongshu\.com\/api\/sns\/v1\/system_service\/config\?/.test(adUrls)) return "小红书-开屏广告-config";
   if (/^https?:\/\/edith\.xiaohongshu\.com\/api\/sns\/v2\/system_service\/splash_config$/.test(adUrls)) return "小红书-开屏广告-splash_config"
@@ -581,51 +579,6 @@ switch (adAppName(url)) {
       console.log(`什么值得买-会员权益中心banner广告, 出现异常`);
     }
     break;
-  // case "微博-开屏广告-sdkad":
-  //   try {
-  //     body = body.match(/\{.*\}/);
-  //     let obj = JSON.parse(body);
-  //     if (obj.needlocation) obj.needlocation = false;
-  //     if (obj.show_push_splash_ad) obj.show_push_splash_ad = false;
-  //     if (obj.code) obj.code = 200;
-  //     if (obj.background_delay_display_time) {
-  //       obj.background_delay_display_time = 31536000; // 60 * 60 * 24 * 365 = 31536000
-  //     }
-  //     if (obj.lastAdShow_delay_display_time) {
-  //       obj.lastAdShow_delay_display_time = 31536000;
-  //     }
-  //     if (obj.realtime_ad_video_stall_time) {
-  //       obj.realtime_ad_video_stall_time = 31536000;
-  //     }
-  //     if (obj.realtime_ad_timeout_duration) {
-  //       obj.realtime_ad_timeout_duration = 31536000;
-  //     }
-  //     for (let item of obj["ads"]) {
-  //       item["displaytime"] = 0;
-  //       item["displayintervel"] = 31536000;
-  //       item["allowdaydisplaynum"] = 0;
-  //       item["begintime"] = "2040-01-01 00:00:00";
-  //       item["endtime"] = "2040-01-01 23:59:59";
-  //     }
-  //     body = JSON.stringify(obj) + "OK";
-  //   } catch (error) {
-  //     console.log(`微博-开屏广告-sdkad, 出现异常`);        
-  //   }
-  //   break;
-  // case "微博-开屏广告-wbpullad":
-  //   try {
-  //     let obj = JSON.parse(body);
-  //     for (let item of obj["cached_ad"]["ads"]) {
-  //       item["start_date"] = 2208960000; // Unix 时间戳 2040-01-01 00:00:00
-  //       item["show_count"] = 0;
-  //       item["duration"] = 31536000; // 60 * 60 * 24 * 365 = 31536000
-  //       item["end_date"] = 2209046399; // Unix 时间戳 2040-01-01 23:59:59
-  //     }
-  //     body = JSON.stringify(obj);
-  //   } catch (error) {
-  //     console.log(`微博-开屏广告-wbpullad, 出现异常`);
-  //   }
-  //   break;
   case "小爱音箱-开屏广告":
     try {
       let obj = JSON.parse(body);

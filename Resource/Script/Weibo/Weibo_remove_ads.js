a693d0d52b413d1fb01159dc5703aab7df355884
lodepuly <admin@qq.com>
2022-12-16 15:55:52 +0800

    no message

--- Resource/Script/Weibo/Weibo_remove_ads.js
+++ Resource/Script/Weibo/Weibo_remove_ads.js
@@ -47,8 +47,8 @@ const itemMenusConfig = {
 	mblog_menus_custom:false,				//寄微博
 	mblog_menus_video_later:true,			//可能是稍后再看？没出现过
 	mblog_menus_comment_manager:true,		//评论管理
-	mblog_menus_avatar_widget:true,		    //头像挂件
-	mblog_menus_card_bg: true,			    //卡片背景
+	mblog_menus_avatar_widget:false,		    //头像挂件
+	mblog_menus_card_bg: false,			    //卡片背景
 	mblog_menus_long_picture:true,		    //生成长图
 	mblog_menus_delete:true,				//删除
 	mblog_menus_edit:true,				    //编辑
@@ -61,7 +61,7 @@ const itemMenusConfig = {
 	mblog_menus_promote:true,				//推广
 	mblog_menus_modify_visible:true,		//设置分享范围
 	mblog_menus_copy_url:true,			    //复制链接
-	mblog_menus_follow:true,				//关注
+	mblog_menus_follow:false,				//关注
 	mblog_menus_video_feedback:true,		//播放反馈
 	mblog_menus_shield:true,				//屏蔽
 	mblog_menus_report:true,				//投诉

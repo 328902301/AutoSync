8654c4891ca322b8f660d0733fafb935e1ca8559
JupiterH <51875312+JupiterH@users.noreply.github.com>
2022-12-16 10:30:44 +0800

    Create Network_Info.js

---
+++ Module/Network_Info.js
@@ -0,0 +1,12 @@
+#!name=Network Info Panel
+#!desc=详细的显示你当前的网络信息
+# 更新日期：2022.08.14
+
+[Panel]
+NetInfoPanel=title="",content="",style=info,script-name=net-info-panel.js,update-interval=1
+
+[Script]
+net-info-panel.js=script-path=https://raw.githubusercontent.com/Rabbit-Spec/Surge/Master/Module/Panel/Network-Info/Moore/Network-Info.js,type=generic,timeout=30
+
+[Host]
+ip-api.com = 208.95.112.1

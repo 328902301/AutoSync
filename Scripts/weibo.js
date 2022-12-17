b3ab61a90e19a7689fd1a7568277c5f5f8e32b31
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2022-12-17 00:43:29 +0000

    Replace weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -1,5 +1,5 @@
 // https://github.com/zmqcherish/proxy-script/blob/main/weibo_main.js
-// 2022-12-16 20:21
+// 2022-12-17 08:42
 
 // 主要的选项配置
 const mainConfig = {
@@ -202,7 +202,7 @@ function removeSearch(data) {
     if (item.category === "feed") {
       if (!isAd(item.data)) newItems.push(item);
     } else {
-      if (item.category === "group") continue;
+      // if (item.category === "group") continue;
       if (!checkSearchWindow(item)) newItems.push(item);
     }
   }

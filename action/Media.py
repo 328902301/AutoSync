593b1555e610003f3fd8cae017321ea902bc1df0
ami@ <ami@futurkk.com>
2022-12-22 14:16:06 +0800

    Update and rename SurgeMedia.py to Media.py

--- action/Media.py
+++ action/Media.py
@@ -1,11 +1,7 @@
 import requests
 
-rawBiliBili = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/BiliBili/BiliBili.list").text
-rawBiliBilintl = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/BiliBiliIntl/BiliBiliIntl.list").text
-rawTelegram = requests.get("https://raw.githubusercontent.com/VirgilClyne/GetSomeFries/main/ruleset/ASN.Telegram.list").text
 rawNetflix = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Netflix/Netflix.list").text
 rawDisney = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Disney/Disney.list").text
-rawpv = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/AmazonPrimeVideo/AmazonPrimeVideo.list").text
 
 result = list()
 for rawresult in [rawBiliBili, rawBiliBilintl, rawTelegram, rawNetflix, rawDisney, rawpv]:

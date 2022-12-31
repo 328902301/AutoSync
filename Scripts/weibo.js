f7a4de47a7169d8120280effcc37f275ba0df05c
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2022-12-31 15:37:56 +0000

    Update weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -1,5 +1,5 @@
 // https://github.com/zmqcherish/proxy-script/blob/main/weibo_main.js
-// 2022-12-31 23:25
+// 2022-12-31 23:35
 
 // 主要的选项配置
 const mainConfig = {
@@ -206,7 +206,7 @@ function checkSearchWindow(item) {
     item.data?.card_type === 1005 ||
     item.data?.itemid === "finder_window" ||
     item.data?.itemid === "more_frame" ||
-    item.data?.itemId?.includes(["hot_discussroom", "hot_discuss_topic", "hot_character", "hot_link_mike", "hot_video", "hot_chaohua_list"]) ||
+    item.data?.itemId?.includes("hot_discussroom" || "hot_discuss_topic" || "hot_character" || "hot_link_mike" || "hot_video" || "hot_chaohua_list") ||
     item.data?.mblog?.page_info?.actionlog?.source?.includes("ad")
   );
 }

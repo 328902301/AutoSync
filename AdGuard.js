49f6fa88e52de2bff8c4551b4d3f9b385d747326
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-12-31 15:27:39 +0800

    Create AdGuard.js

---
+++ AdGuard.js
@@ -0,0 +1,23 @@
+/****************************************
+
+项目名称：AdGuard
+下载地址：https://t.cn/A6xe1oaK
+使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*****************************************
+[rewrite_local]
+
+^https:\/\/mobile-api\.adguard\.org\/api\/.+\/ios_validate_receipt\/(.*?) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/AdGuard.js
+
+
+[mitm]
+hostname = mobile-api.adguard.org
+
+****************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023 = {"products":[{"premium_status":"ACTIVE","product_id":"com.adguard.lifetimePurchase"}]}
+
+$done({body : JSON.stringify(chxm1023)});

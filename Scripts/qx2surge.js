da5d273bd3b23c159c0c00306ee5242b6d66e8b0
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-02 09:46:55 +0000

    Replace qx2surge.js

--- Scripts/qx2surge.js
+++ Scripts/qx2surge.js
@@ -152,8 +152,7 @@ ${HeaderRewrite}
 ${MapLocal}
 ${MITM}`.replace(/\;/g, "#");
 
-  //$done({ response: { status: 200, body: body } });
-  $done({ status: "HTTP/1.1 200 OK", body: body });
+  $done({ response: { status: 200, body: body } });
 })().catch((e) => {
   $notification.post(`${e}`, "", "");
   $done();

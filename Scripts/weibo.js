607c9c7e8561622e4fb03b75a6236b40d63b5be7
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-03 02:59:00 +0000

    Replace weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -1,9 +1,16 @@
 // https://github.com/zmqcherish/proxy-script/blob/main/weibo_main.js
-// 2023-01-03 10:55
+// 2023-01-03 10:58
 
 var url = $request.url;
 var body = $response.body;
 let method = getModifyMethod(url);
+
+if (method) {
+  var func = eval(method);
+  let data = JSON.parse(body);
+  new func(data);
+  body = JSON.stringify(data);
+}
 
 // 主要的选项配置
 const mainConfig = {
@@ -510,13 +517,6 @@ function removeTopicTab(data) {
     newTab.push(tab);
   }
   return data;
-}
-
-if (method) {
-  var func = eval(method);
-  let data = JSON.parse(body);
-  new func(data);
-  body = JSON.stringify(data);
 }
 
 $done({ body });

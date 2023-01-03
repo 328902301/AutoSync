9c93c22344ffea2113ce9bb7ca82af8d42cf875c
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-03 03:18:55 +0000

    Replace weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -1,5 +1,5 @@
 // https://github.com/zmqcherish/proxy-script/blob/main/weibo_main.js
-// 2023-01-03 11:08
+// 2023-01-03 11:18
 
 // 主要的选项配置
 const mainConfig = {
@@ -75,7 +75,7 @@ const otherUrls = {
   "/2/statuses/extend": "itemExtendHandler", // 微博详情页
   "/2/video/tiny_stream_video_list": "nextVideoHandler", // 取消自动播放下一个视频
   "/2/video/remind_info": "removeVideoRemind", // tab2 菜单上的假通知
-  "/2/!/huati/discovery_home_bottom_channels": "removeSearchMain" // 超话 tab 顶部广场
+  "/2/!/huati/discovery_home_bottom_channels": "removeTopicTab" // 超话 tab 顶部广场
 };
 
 function getModifyMethod(url) {
@@ -335,9 +335,6 @@ function removeSearch(data) {
 }
 
 function removeSearchMain(data) {
-  if (data.channelInfo.channel_list.length !== 1) {
-    delete data.channelInfo.channel_list[1];
-  }
   let channels = data.channelInfo.channels;
   if (!channels) return data;
   for (let channel of channels) {
@@ -501,13 +498,14 @@ function removeVideoRemind(data) {
 
 // 移除话题 tab 顶部广场
 function removeTopicTab(data) {
+  if (!mainConfig.removeUnusedPart) return;
   let clist = data.channelInfo.channel_list;
   if (!clist) return data;
-  let newTab = [];
+  let newList = [];
   for (let tab of clist) {
-    if (tab.title === "广场") continue;
-    newTab.push(tab);
+    if (tab.title !== "广场") newList.push(tab);
   }
+  data.channelInfo.channel_list = newList;
   return data;
 }
 

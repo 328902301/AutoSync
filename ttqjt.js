e9d1c6128379757ce9ecbb4823ec18333b400f5e
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-01-04 15:25:31 +0800

    Update ttqjt.js

--- ttqjt.js
+++ ttqjt.js
@@ -1,4 +1,4 @@
-/****************************************
+/*************************************
 
 项目名称：图图记账
 下载地址：https://t.cn/A6JZS4a0
@@ -14,7 +14,7 @@
 
 使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
-*****************************************
+**************************************
 
 
 [rewrite_local]
@@ -25,7 +25,7 @@
 
 hostname = api.revenuecat.com
 
-****************************************/
+*************************************/
 
 
 const chxm1023 = {"request_date_ms":1670916721890,"request_date":"2022-12-13T07:32:01Z","subscriber":{"non_subscriptions":{},"first_seen":"2022-12-13T07:15:45Z","original_application_version":"27","entitlement":{},"other_purchases":{},"management_url":null,"subscriptions":{"com.chxm1023.premium.yearly":{"warning":"仅供学习，禁止转载或售卖","wechat":"chxm1023","purchase_date":"2022-09-09T09:09:09Z","expires_date":"2099-09-09T09:09:09Z","original_purchase_date":"2022-09-09T09:09:09Z","feedback":"chxm1023","ownership_type":"PURCHASED"}},"entitlements":{"all":{"wechat":"chxm1023","ownership_type":"PURCHASED","product_identifier":"com.chxm1023.premium.yearly","feedback":"chxm1023","warning":"仅供学习，禁止转载或售卖","original_purchase_date":"2022-09-09T09:09:09Z","purchase_date":"2022-09-09T09:09:09Z","expires_date":"2099-09-09T09:09:09Z"}},"original_purchase_date":"2022-12-13T07:15:15Z","original_app_user_id":"$RCAnonymousID:7937cc279bfe463c9495003857f22faf","last_seen":"2022-12-13T07:18:05Z"}};

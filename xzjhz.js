9375c6ea08157550087107951cae7f97bacd01b0
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-01-04 15:29:53 +0800

    Update xzjhz.js

--- xzjhz.js
+++ xzjhz.js
@@ -1,10 +1,10 @@
-/****************************************
+/*************************************
 
 项目功能：小组件盒子 解锁永久VIP
 下载地址：https://t.cn/A6oDCwJ6
 使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
-*****************************************
+**************************************
 
 [rewrite_local]
 ^https:\/\/widget-box-api\.codefuture\.top\/v1\/users\/me url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/Rewrite/xzjhz.js
@@ -13,7 +13,7 @@
 
 hostname = widget-box-api.codefuture.top
 
-****************************************/
+*************************************/
 
 
 var body = $response.body;

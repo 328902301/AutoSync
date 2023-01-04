d8fd2d9ace4f1371023e471e81162aa70117a0f8
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-04 05:20:23 +0000

    Replace myBlockAds.js

--- Scripts/myBlockAds.js
+++ Scripts/myBlockAds.js
@@ -1,4 +1,4 @@
-// 2023-01-04 13:11
+// 2023-01-04 13:20
 
 var body = $response.body;
 var method = $request.method;
@@ -320,32 +320,52 @@ switch (adAppName(url)) {
   case "高德地图-首页卡片":
     try {
       let obj = JSON.parse(body);
-      if (obj.data && obj.data.mapBizList) {
-        obj.data.mapBizList = Object.values(obj.data.mapBizList).filter((item) => {
-          return !(
-            item.dataKey === "Covid" ||
-            item.dataKey === "CovidMerge" ||
-            item.dataKey === "SddTileOffsiteHotel" ||
-            item.dataKey === "OffsiteHotel" ||
-            item.dataKey === "Offsite" ||
-            item.dataKey === "Winter" ||
-            item.dataKey === "Kids" ||
-            item.dataKey === "SpringV2"
-          );
-        });
-      } else if (obj.data && obj.data.cardList) {
-        obj.data.cardList = Object.values(obj.data.cardList).filter((item) => {
-          return (
-            item.dataKey === "ContinueNavigationCard" ||
-            item.dataKey === "LoginCard" ||
-            item.dataKey === "FrequentLocation"
-          );
-        });
+      let cardList = obj.data.cardList;
+      let newCardList = [];
+      for (let item of cardList) {
+        if (
+          item.dataKey === "ContinueNavigationCard" ||
+          item.dataKey === "LoginCard" ||
+          item.dataKey === "FrequentLocation"
+        ) {
+          newCardList.push(item);
+        } else {
+          continue;
+        }
       }
+      obj.data.cardList = newCardList;
       body = JSON.stringify(obj);
     } catch (error) {
-      console.log(`高德地图-首页卡片, 出现异常`);
+      console.log(`高德地图-我的, 出现异常`);
     }
+    // try {
+    //   let obj = JSON.parse(body);
+    //   if (obj.data && obj.data.mapBizList) {
+    //     obj.data.mapBizList = Object.values(obj.data.mapBizList).filter((item) => {
+    //       return !(
+    //         item.dataKey === "Covid" ||
+    //         item.dataKey === "CovidMerge" ||
+    //         item.dataKey === "SddTileOffsiteHotel" ||
+    //         item.dataKey === "OffsiteHotel" ||
+    //         item.dataKey === "Offsite" ||
+    //         item.dataKey === "Winter" ||
+    //         item.dataKey === "Kids" ||
+    //         item.dataKey === "SpringV2"
+    //       );
+    //     });
+    //   } else if (obj.data && obj.data.cardList) {
+    //     obj.data.cardList = Object.values(obj.data.cardList).filter((item) => {
+    //       return (
+    //         item.dataKey === "ContinueNavigationCard" ||
+    //         item.dataKey === "LoginCard" ||
+    //         item.dataKey === "FrequentLocation"
+    //       );
+    //     });
+    //   }
+    //   body = JSON.stringify(obj);
+    // } catch (error) {
+    //   console.log(`高德地图-首页卡片, 出现异常`);
+    // }
     break;
   case "高德地图-首页顶部消息横幅":
     try {

2df48bff8c6d15ec3ec277f4a616189276a9501b
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-01-04 14:17:26 +0800

    Update bdsp.js

--- bdsp.js
+++ bdsp.js
@@ -1,10 +1,10 @@
-/****************************************
+/*************************************
 
 项目名称：布丁锁屏
 下载地址：https://t.cn/A6o11VGR
 使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
-*****************************************
+**************************************
  
 [rewrite_local]
 
@@ -14,7 +14,7 @@ https?:\/\/screen-lock\.51wnl-cq\.com\/userApi\/saveUser url script-echo-respons
 
 hostname = screen-lock.51wnl-cq.com
 
-****************************************/
+*************************************/
 
 
 var chxm1023 = {"returncode":"SUCCESS","errormsg":"","data":{"id":"chxm1023","nickName":"chxm1023","sign":"chxm1023","vipStatus":1,"endTime":2099,"exchangeCode":null,"expireDate":null}};

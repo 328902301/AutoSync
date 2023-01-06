ed59509a0e47c310c5435b435605c286cdd638f3
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-06 04:44:30 +0000

    Replace myBlockAds.js

--- Scripts/myBlockAds.js
+++ Scripts/myBlockAds.js
@@ -1,4 +1,4 @@
-// 2023-01-06 12:39
+// 2023-01-06 12:44
 
 var body = $response.body;
 var url = $request.url;
@@ -62,12 +62,12 @@ if (!body) {
     case "12306-开屏广告":
       try {
         let obj = JSON.parse(body);
-        if (obj.advertParam && obj.materialsList.length === 1) {
+        if (obj.advertParam && obj.materialsList && obj.materialsList.length === 1) {
           obj.materialsList[0].billId = "1000005";
           obj.materialsList[0].billMaterialsId = "2000005";
           obj.materialsList[0].filePath = "";
           obj.advertParam.skipTime = 1;
-        } else if (obj.materialsList.length > 1) {
+        } else if (obj.materialsList && obj.materialsList.length > 1) {
           for (let i = 0; i < obj.materialsList.length; i++) {
             obj.materialsList[i].filePath = "";
           }

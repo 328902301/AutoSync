ce0d1d041a1ba3bf1d7708e32be7d7517f5b6904
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-06 09:38:10 +0000

    Replace 12306.js

--- Scripts/12306.js
+++ Scripts/12306.js
@@ -3,19 +3,14 @@ if (!$response.body) $done({});
 const url = $request.url;
 
 if (url.includes("/ad.12306.cn/ad/ser/getAdList")) {
-  try {
-    let obj = JSON.parse($response.body);
-    if (obj.advertParam && obj.materialsList && obj.materialsList.length === 1) {
-      obj.materialsList[0].billId = "1000005";
-      obj.materialsList[0].billMaterialsId = "2000005";
-      obj.advertParam.skipTime = 1;
-    } else if (obj.materialsList && obj.materialsList.length > 1) {
-      obj.materialsList = [];
-    }
-    body = JSON.stringify(obj);
-  } catch (error) {
-    console.log(`12306-开屏广告, 出现异常: ` + error);
+  let obj = JSON.parse($response.body);
+  if (obj.advertParam && obj.materialsList && obj.materialsList.length === 1) {
+    obj.materialsList[0].billId = "1000005";
+    obj.materialsList[0].billMaterialsId = "2000005";
+    obj.advertParam.skipTime = 1;
+  } else if (obj.materialsList && obj.materialsList.length > 1) {
+    obj.materialsList = [];
   }
+  body = JSON.stringify(obj);
   $done({ body });
 }
-

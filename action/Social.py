7d7becd0682b53f638d1a7d066f1952bb423c6dc
ami@ <ami@futurkk.com>
2023-01-06 11:38:47 +0800

    Update Social.py

--- action/Social.py
+++ action/Social.py
@@ -3,7 +3,7 @@ import requests
 rawTiktok = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/TikTok/TikTok.list").text
 rawTwitter = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Twitter/Twitter.list").text
 rawInstagram = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Instagram/Instagram.list").text
-rawdouyin1 = requests.get("https://raw.githubusercontent.com/bunizao/TutuBetterRules/tutu/RuleList/DOMAlN/Douyin_ByteDance.list").texy
+rawdouyin1 = requests.get("https://raw.githubusercontent.com/bunizao/TutuBetterRules/tutu/RuleList/DOMAlN/Douyin_ByteDance.list").text
 rawdouyin2 = requests.get("https://raw.githubusercontent.com/bunizao/TutuBetterRules/tutu/RuleList/DOMAlN/Douyin.list").text
 
 result = list()

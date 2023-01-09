1224cb58b61e79e224addfc24a40064dd4441f31
Lãng Khách <13623692+langkhach270389@users.noreply.github.com>
2023-01-09 20:44:16 +0700

    Update itranslate.js

--- scripts/langkhach/itranslate.js
+++ scripts/langkhach/itranslate.js
@@ -1,9 +1,13 @@
 let obj= {
-  "is_period": true,
-  "product_id": "com.itranslate.pro.yearly",
-  "original_transaction_id": "20000627379146",
-  "expires_date_ms": 4099246456000,
-  "bundle_id": "com.outerspaceapps.itranslate",
-  "transaction_id": "20000627379146"
+  "licenses": [
+    {
+      "product_id": "com.itranslate.pro.yearly",
+      "is_trial_period": false,
+      "original_transaction_id": "440001324215869",
+      "transaction_id": "440001324215869",
+      "expires_date_ms": 4099246456000,
+      "bundle_id": "com.outerspaceapps.itranslate"
+    }
+  ]
 }
 $done({body: JSON.stringify(obj)});

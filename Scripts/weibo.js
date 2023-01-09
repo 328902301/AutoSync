a013c010534de55f94c358f1d88e26f0e16f9c4d
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-09 12:04:01 +0000

    Replace weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -1,5 +1,5 @@
 // https://github.com/zmqcherish/proxy-script/blob/main/weibo_main.js
-// 2023-01-09 19:56
+// 2023-01-09 20:03
 
 // 屏蔽用户id获取方法
 // 进入用户主页 选择复制链接 得到类似 `https://weibo.com/u/xxx` 的文本 xxx即为用户id 多个id用英文逗号 `,` 分开
@@ -410,8 +410,12 @@ function removeTopic(data) {
   let items = data.items;
   let newItems = [];
   for (let item of items) {
-    if (item.category !== "card") {
+    if (item.category === "feed") {
       if (!isAd(item.data)) newItems.push(item);
+    } else if (item.category === "card") {
+      item.data = {};
+      delete item.style;
+      delete item.itemExt;
     }
   }
   data.items = newItems;

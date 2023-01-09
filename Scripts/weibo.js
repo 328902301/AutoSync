dedadf0516722a8417fa514d3b3d89d1f9ebfaed
Cube <49463634+RuCu6@users.noreply.github.com>
2023-01-09 21:18:38 +0800

    Update weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -1,5 +1,5 @@
 // https://github.com/zmqcherish/proxy-script/blob/main/weibo_main.js
-// 2023-01-09 21:14
+// 2023-01-09 21:18
 
 // 屏蔽用户id获取方法
 // 进入用户主页 选择复制链接 得到类似 `https://weibo.com/u/xxx` 的文本 xxx即为用户id 多个id用英文逗号 `,` 分开
@@ -364,15 +364,17 @@ function removeSearch(data) {
       }
     }
   }
+  if (data.loadedInfo) {
+    // 去除搜索框填充词
+    if (data.loadedInfo.searchBarContent) {
+      data.loadedInfo.searchBarContent = [];
+    }
+    // 去除搜索背景图片
+    if (data.loadedInfo.headerBack) {
+      data.loadedInfo.headerBack.channelStyleMap = {};
+    }
+  }
   data.items = newItems;
-  // 去除搜索框填充词
-  if (data.loadedInfo && data.loadedInfo.searchBarContent) {
-    data.loadedInfo.searchBarContent = [];
-  }
-  // 去除搜索背景广告
-  if (data.loadedInfo.headerBack) {
-    data.loadedInfo.headerBack.channelStyleMap = {};
-  }
   return data;
 }
 

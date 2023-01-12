b82cf20c2cefa987151df3ea99b7f36dd270b210
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-12 07:56:42 +0000

    Add new file

---
+++ Scripts/webpage/baidu.js
@@ -0,0 +1,4 @@
+let rHead = '<head>'
+let newStyle = '<head><link rel="stylesheet" href="https://limbopro.com/CSS/baidu.css" type="text/css">'
+let body = $response.body.replace(rHead, newStyle);
+$done({ body });

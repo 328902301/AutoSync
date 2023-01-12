e155bfc21cd4927de4c552e4b6bf2d510a8eedde
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-12 08:52:58 +0000

    Delete baidu-zhidao.js

--- Scripts/webpage/baidu-zhidao.js
+++
@@ -1,4 +0,0 @@
-let rHead = '<head>'
-let newStyle = '<head><link rel="stylesheet" href="https://gitlab.com/RuCu6/QuanX/-/raw/main/Css/-zhidao.css" type="text/css" />'
-let body = $response.body.replace(rHead, newStyle);
-$done({ body });

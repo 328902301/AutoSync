3ce41f63ae702bf99181dce67ff21faa7ed27458
Cube <49463634+RuCu6@users.noreply.github.com>
2023-01-13 17:04:51 +0800

    Update amap.js

--- Scripts/amap.js
+++ Scripts/amap.js
@@ -1,4 +1,4 @@
-// 2023-01-13 16:18
+// 2023-01-13 17:00
 
 if (!$response.body) $done({});
 const url = $request.url;
@@ -8,30 +8,35 @@ if (obj.data) {
   if (url.includes("/faas/amap-navigation/main-page")) {
     // 高德地图-首页卡片
     if (obj.data.cardList) {
-      obj.data.cardList = obj.data.cardList.filter(
-        (item) => item.dataKey === "LoginCard"
+      obj.data.cardList = obj.data.cardList.filter((item) =>
+        item.dataKey === "LoginCard"
       );
     }
   } else if (url.includes("/promotion-web/resource")) {
     // 高德地图-打车
-    delete obj.data.banner;
-    delete obj.data.bubble;
-    delete obj.data.icon;
-    delete obj.data.popup;
-    delete obj.data.tips;
+    if (obj.data.banner) delete obj.data.banner;
+    if (obj.data.bubble) delete obj.data.bubble;
+    if (obj.data.icon) delete obj.data.icon;
+    if (obj.data.popup) delete obj.data.popup;
+    if (obj.data.tips) delete obj.data.tips;
   } else if (url.includes("/shield/dsp/profile/index/nodefaasv3")) {
     // 高德地图-我的
     if (obj.data.cardList) {
-      obj.data.cardList = obj.data.cardList.filter(
-        (item) => item.dataKey === "MyOrderCard"
+      obj.data.cardList = obj.data.cardList.filter((item) =>
+        item.dataKey === "MyOrderCard"
       );
     }
   } else if (url.includes("/shield/search/nearbyrec_smart")) {
     // 高德地图-附近
-    delete obj.data.activity; // 横版推广
-    delete obj.data.commodity_rec; // 超值套餐
-    delete obj.data.coupon; // 右下角广告
-    // delete obj.data.scene; // 附近二级菜单
+    if (obj.data.modules) {
+      obj.data.modules = obj.data.modules.filter((item) =>
+        (
+          item === "head" ||
+          item === "search_hot_words" ||
+          item === "feed_rec"
+        )
+      );
+    }
   } else if (url.includes("/valueadded/alimama/splash_screen")) {
     // 高德地图-开屏广告
     if (obj.data.ad) {

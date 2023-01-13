186f98b39734f0f18c936db2709f7f3ce5d3464a
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-13 08:06:13 +0000

    Update amap.js

--- Scripts/amap.js
+++ Scripts/amap.js
@@ -1,4 +1,4 @@
-// 2023-01-13 15:58
+// 2023-01-13 16:06
 
 if (!$response.body) $done({});
 const url = $request.url;
@@ -27,10 +27,11 @@ if (obj.data) {
       );
     }
   } else if (url.includes("/shield/search/nearbyrec_smart")) {
-    delete obj.data.activity;
+    // 高德地图-附近
+    // delete obj.data.activity;
     delete obj.data.commodity_rec;
     delete obj.data.coupon;
-    delete obj.data.scene;
+    // delete obj.data.scene;
   } else if (url.includes("/valueadded/alimama/splash_screen")) {
     // 高德地图-开屏广告
     if (obj.data.ad) {

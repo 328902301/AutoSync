ee6ae27c8618ac80380b684adc59541eac9bebe1
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-13 04:51:04 +0000

    Update amap.js

--- Scripts/amap.js
+++ Scripts/amap.js
@@ -1,4 +1,4 @@
-// 2023-01-13 12:44
+// 2023-01-13 12:50
 
 if (!$response.body) $done({});
 const url = $request.url;
@@ -14,11 +14,11 @@ if (obj.data) {
     }
   } else if (url.includes("/promotion-web/resource")) {
     // 高德地图-打车
-    // delete obj.data.banner;
-    // delete obj.data.bubble;
-    // delete obj.data.icon;
-    // delete obj.data.popup;
-    // delete obj.data.tips;
+    delete obj.data.banner;
+    delete obj.data.bubble;
+    delete obj.data.icon;
+    delete obj.data.popup;
+    delete obj.data.tips;
   } else if (url.includes("/shield/dsp/profile/index/nodefaasv3")) {
     // 高德地图-我的
     if (obj.data.cardList) {

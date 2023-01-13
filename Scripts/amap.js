ffc43b5764614c69299add6e98c0b891ba98cf43
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-13 08:43:46 +0000

    Update amap.js

--- Scripts/amap.js
+++ Scripts/amap.js
@@ -8,8 +8,8 @@ if (obj.data) {
   if (url.includes("/faas/amap-navigation/main-page")) {
     // 高德地图-首页卡片
     if (obj.data.cardList) {
-      obj.data.cardList = obj.data.cardList.filter(
-        (item) => item.dataKey === "LoginCard"
+      obj.data.cardList = obj.data.cardList.filter(item =>
+        item.dataKey === "LoginCard"
       );
     }
   } else if (url.includes("/promotion-web/resource")) {
@@ -22,8 +22,8 @@ if (obj.data) {
   } else if (url.includes("/shield/dsp/profile/index/nodefaasv3")) {
     // 高德地图-我的
     if (obj.data.cardList) {
-      obj.data.cardList = obj.data.cardList.filter(
-        (item) => item.dataKey === "MyOrderCard"
+      obj.data.cardList = obj.data.cardList.filter(item =>
+        item.dataKey === "MyOrderCard"
       );
     }
   } else if (url.includes("/shield/search/nearbyrec_smart")) {

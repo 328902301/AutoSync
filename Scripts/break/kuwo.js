612f4c7ec0761fc9a383ea70c574546341db21c4
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-15 09:01:22 +0000

    Update kuwo.js

--- Scripts/break/kuwo.js
+++ Scripts/break/kuwo.js
@@ -23,7 +23,7 @@ if (url.includes("/a.p")) {
   }
 } else if (url.includes("mgxhtj.kuwo.cn") || url.includes("nmobi.kuwo.cn")) {
   body = $response.body
-    .replace(/<ad\sfocusid="\d+"[^>]\/>*/g, "</>")
+    .replace(/<ad\sfocusid="\d+"[^>]\/>*/g, "")
     .replace(/(<userinfolabel\scontent="\[)[^"]*/g, "$1]");
 } else if (url.includes("searchrecterm.kuwo.cn")) {
   body = '{ content: [{ query_word: "搜索歌曲", desc: "" }] }';

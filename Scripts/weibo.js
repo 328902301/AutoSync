c00856f5a9c2a1561e4658641f48dbb919195af7
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-19 03:51:43 +0000

    Replace weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -1,5 +1,5 @@
 // https://github.com/zmqcherish/proxy-script/blob/main/weibo_main.js
-// 2023-01-19 11:40
+// 2023-01-19 11:51
 
 // 屏蔽用户id获取方法
 // 进入用户主页 选择复制链接 得到类似 `https://weibo.com/u/xxx` 的文本 xxx即为用户id 多个id用英文逗号 `,` 分开
@@ -453,7 +453,11 @@ function checkSearchWindow(item) {
   }
   // 搜索页中间的 热议话题、热门人物
   if (item.category === "group") {
-    return true;
+    if (item.itemId) {
+      return true;
+    } else {
+      return false;
+    }
   }
   if (
     item.data?.card_type === 19 ||

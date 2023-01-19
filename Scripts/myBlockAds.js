ed6c7bddd244a3d489cf7943ffbc5540177cd2e0
Cube <49463634+RuCu6@users.noreply.github.com>
2023-01-19 13:12:12 +0800

    Update myBlockAds.js

--- Scripts/myBlockAds.js
+++ Scripts/myBlockAds.js
@@ -1,8 +1,8 @@
-// 2023-01-19 10:48
+// 2023-01-19 13:11
 
 if (!$response.body) $done({});
 const url = $request.url;
-let obj = JSON.parse(body);
+var body = $response.body;
 
 function getCube(p) {
   if (/^https:\/\/capis(-?\w*)?\.didapinche\.com\/ad\/cx\/startup\?/.test(p)) {
@@ -37,6 +37,7 @@ function getCube(p) {
 switch (getCube(url)) {
   case "嘀嗒出行-开屏广告":
     try {
+      let obj = JSON.parse(body);
       if (obj.hasOwnProperty("startupPages") == true) {
         obj.show_time = 1;
         obj.full_screen = 0;
@@ -49,32 +50,38 @@ switch (getCube(url)) {
         });
         obj.startupPages = startupPages;
       }
+      body = JSON.stringify(obj);
     } catch (error) {
       console.log(`嘀嗒出行-开屏广告, 出现异常: ` + error);
     }
     break;
   case "多点-开屏广告":
     try {
+      let obj = JSON.parse(body);
       for (let i = 0; i < obj["data"]["welcomePage"].length; i++) {
         obj["data"]["welcomePage"][i]["onlineTime"] = 2208960000000;
         obj["data"]["welcomePage"][i]["offlineTime"] = 2209046399000;
       }
+      body = JSON.stringify(obj);
     } catch (error) {
       console.log(`多点-开屏广告, 出现异常: ` + error);
     }
     break;
   case "JavDB":
     try {
+      let obj = JSON.parse(body);
       if (obj.data.splash_ad) {
         obj.data.splash_ad.enabled = false;
         obj.data.splash_ad.overtime = 0;
       }
+      body = JSON.stringify(obj);
     } catch (error) {
       console.log(`JavDB, 出现异常: ` + error);
     }
     break;
   case "京东-开屏广告":
     try {
+      let obj = JSON.parse(body);
       for (let i = 0; i < obj.images.length; i++) {
         for (let j = 0; j < obj.images[i].length; j++) {
           if (obj.images[i][j].showTimes) {
@@ -87,30 +94,36 @@ switch (getCube(url)) {
       }
       obj.countdown = 100;
       obj.showTimesDaily = 0;
+      body = JSON.stringify(obj);
     } catch (error) {
       console.log(`京东-开屏广告, 出现异常: ` + error);
     }
     break;
   case "联享家-开屏广告":
     try {
+      let obj = JSON.parse(body);
       obj.seq = "0";
       obj.reqinterval = 0;
       delete obj.last_ads;
       delete obj.data;
+      body = JSON.stringify(obj);
     } catch (error) {
       console.log(`联享家-开屏广告, 出现异常: ` + error);
     }
     break;
   case "美团外卖-开屏广告":
     try {
+      let obj = JSON.parse(body);
       delete obj.data.startpicture.ad;
       delete obj.data.startpicture.mk;
+      body = JSON.stringify(obj);
     } catch (error) {
       console.log(`美团外卖-开屏广告, 出现异常: ` + error);
     }
     break;
   case "小爱音箱-开屏广告":
     try {
+      let obj = JSON.parse(body);
       let data = [];
       for (let i = 0; i < obj.data.length; i++) {
         let ad = obj.data[i];
@@ -121,24 +134,29 @@ switch (getCube(url)) {
         data.push(ad);
       }
       obj.data = data;
+      body = JSON.stringify(obj);
     } catch (error) {
       console.log(`小爱音箱-开屏广告, 出现异常: ` + error);
     }
     break;
   case "小米商城-开屏广告":
     try {
+      let obj = JSON.parse(body);
       obj.code = 0;
       obj.data.skip_splash = true;
       delete obj.data.splash;
       obj.info = "ok";
       obj.desc = "成功";
+      body = JSON.stringify(obj);
     } catch (error) {
       console.log(`小米商城-开屏广告, 出现异常: ` + error);
     }
     break;
   case "知乎-首页右下角悬浮框":
     try {
+      let obj = JSON.parse(body);
       if ("feed_egg" in obj) obj = {};
+      body = JSON.stringify(obj);
     } catch (error) {
       console.log(`知乎-首页右下角悬浮框, 出现异常: ` + error);
     }
@@ -147,5 +165,4 @@ switch (getCube(url)) {
     break;
 }
 
-body = JSON.stringify(obj);
 $done({ body });

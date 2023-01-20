6b686c621f18726161882d34d81b4154bb4233be
Hsiaoyi Hsu <xreamxu@gmail.com>
2023-01-21 05:55:00 +0800

    [!] kill-active-requests

--- surge/modules/kill-active-requests/index.js
+++ surge/modules/kill-active-requests/index.js
@@ -1,13 +1,23 @@
 !(async () => {
   // console.log(JSON.stringify($network, null, 2))
-  if(!$network.wifi || !$network.wifi.bssid){
-    const { requests = [] } = (await httpAPI('/v1/requests/active', 'GET')) || {}
-    for await (const { id } of requests) {
-      // console.log(id)
-      const res = await httpAPI('/v1/requests/kill', 'POST', { id })
-      // console.log(res)
+  let wifi = $network.wifi && $network.wifi.bssid
+  if (wifi) {
+    // console.log(`现在有 wifi`)
+    $persistentStore.write(wifi, 'last_network')
+  } else {
+    // console.log(`现在无 wifi`)
+    wifi = $persistentStore.read('last_network')
+    // console.log(`但是之前有 wifi`)
+    if (wifi) {
+      const { requests = [] } = (await httpAPI('/v1/requests/active', 'GET')) || {}
+      for await (const { id } of requests) {
+        // console.log(id)
+        const res = await httpAPI('/v1/requests/kill', 'POST', { id })
+        // console.log(res)
+      }
+      $notification.post('网络变化', '打断请求', `${requests.length} 个`)
     }
-    $notification.post('网络变化', '打断请求', `${requests.length} 个`)
+    $persistentStore.write('', 'last_network')
   }
   $done()
 })()

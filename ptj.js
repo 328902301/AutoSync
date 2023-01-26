fabd12fbcff9ea5f6f485032c4b5c94534fa56c3
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-01-26 13:25:14 +0800

    Update ptj.js

--- ptj.js
+++ ptj.js
@@ -8,7 +8,7 @@
 
 [rewrite_local]
 
-https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Pandora.js
+https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/ptj.js
 
 [mitm]
 

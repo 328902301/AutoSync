42602213363c3e004b15188c5c0756cff4ee2934
amiiKyoo <i@am.sb>
2023-01-26 13:54:02 +0800

    Delete Surge for myself directory

--- Surge for myself/JS/reload.js
+++
@@ -1,8 +0,0 @@
-
-$httpAPI("POST", "/v1/profiles/reload", {}, data => {
-    $notification.post("：Are you want to reload Surge?","Sir, I'm already to reload Surge!","")
-    $done({
-        title: "You can reload Surge through me.",
-        content: "Enjoy your day of peace of mind!",
-     })
-    });

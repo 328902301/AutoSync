9af12fe1151d875a536d8a3ca4c995ae2be3b644
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-26 09:25:52 +0000

    Replace bili.js

--- Scripts/bilibili/bili.js
+++ Scripts/bilibili/bili.js
@@ -1,4 +1,4 @@
-// 2023-01-19 18:15
+// 2023-01-26 17:25
 
 if (!$response.body) $done({});
 const url = $request.url;
@@ -7,7 +7,9 @@ let obj = JSON.parse($response.body);
 if (obj.data) {
   // 哔哩哔哩-强制设置的皮肤
   if (url.includes("/x/resource/show/skin")) {
-    if (obj.data.common_equip) delete obj.data.common_equip;
+    if (obj.data.common_equip) {
+      delete obj.data.common_equip;
+    }
   } else if (url.includes("/x/resource/show/tab/v2")) {
     // 哔哩哔哩-标签页
     if (obj.data.tab) {
@@ -55,105 +57,106 @@ if (obj.data) {
     // 494离线缓存 495历史记录 496我的收藏 497稍后再看 741我的钱包 742稿件管理 500联系客服 501设置
     // 622为会员购中心 425开始为概念版id
     const itemList = new Set([396, 397, 398, 399]);
-    obj["data"]["sections_v2"]?.forEach((element, index) => {
-      let items = element["items"].filter((e) => itemList.has(e.id));
-      obj["data"]["sections_v2"][index].button = {};
-      delete obj["data"]["sections_v2"][index].tip_icon;
-      delete obj["data"]["sections_v2"][index].be_up_title;
-      delete obj["data"]["sections_v2"][index].tip_title;
-      for (let i = 0; i < obj["data"]["sections_v2"].length; i++) {
-        if (
-          obj.data.sections_v2[i].title === "推荐服务" ||
-          obj.data.sections_v2[i].title === "更多服务" ||
-          obj.data.sections_v2[i].title === "创作中心"
-        ) {
-          delete obj.data.sections_v2[i].title;
-          delete obj.data.sections_v2[i].type;
+    if (obj.data?.sections_v2) {
+      obj.data.sections_v2.forEach((element) => {
+        element.items = element.items.filter((e) => itemList.includes(e.id));
+        element.button = {};
+        element.be_up_title = "";
+        element.tip_icon = "";
+        element.tip_title = "";
+        if (["推荐服务", "更多服务", "创作中心"].includes(element.title)) {
+          element.title = "";
+          element.type = "";
         }
+      });
+      if (obj.data?.live_tip) {
+        obj.data.live_tip = {};
       }
-      obj["data"]["sections_v2"][index]["items"] = items;
-      delete obj.data.vip_section_v2;
-      delete obj.data.vip_section;
-      delete obj.data.live_tip;
-      delete obj.data.answer;
-      // 开启本地会员标识 2022-03-05 add by ddgksf2013
+      if (obj.data?.answer) {
+        obj.data.answer = {};
+      }
+      obj.data.vip_section = "";
+      obj.data.vip_section_v2 = "";
+      // 开启本地会员标识
       obj.data.vip_type = 2;
       obj.data.vip.type = 2;
       obj.data.vip.status = 1;
       obj.data.vip.vip_pay_type = 1;
       obj.data.vip.due_date = 2208960000; // Unix 时间戳 2040-01-01 00:00:00
-    });
+    }
   } else if (url.includes("/x/v2/account/myinfo")) {
     // 哔哩哔哩-会员清晰度
     obj.data.vip.type = 2;
     obj.data.vip.status = 1;
     obj.data.vip.vip_pay_type = 1;
     obj.data.vip.due_date = 2208960000; // Unix 时间戳 2040-01-01 00:00:00
-  } else if (url.includes("/x/v2/feed/index")) {
-    // 哔哩哔哩-推荐广告
-    if (obj.data.items) {
-      obj.data.items = obj.data.items.filter((i) => {
-        const { card_type: cardType, card_goto: cardGoto } = i;
-        if (cardType && cardGoto) {
-          if (cardType === "banner_v8" && cardGoto === "banner") {
-            // 去除判断条件 首页横版内容全部去掉
-            // if (i.banner_item) {
-            // for (const v of i.banner_item) {
-            //   if (v.type) {
-            //     if (v.type === "ad") return false;
-            //   }
-            // }
-            // return false;
-            // }
-            return false;
-          } else if (
-            cardType === "cm_v2" &&
-            [
-              "ad_web_s",
-              "ad_av",
-              "ad_web_gif",
-              "ad_player",
-              "ad_inline_3d"
-            ].includes(cardGoto)
-          ) {
-            // ad_player大视频广告 ad_web_gif大gif广告 ad_web_s普通小广告 ad_av创作推广广告 ad_inline_3d 上方大的视频3d广告
-            return false;
-          } else if (cardType === "small_cover_v10" && cardGoto === "game") {
-            // 游戏广告
-            return false;
-          } else if (
-            cardType === "cm_double_v9" &&
-            cardGoto === "ad_inline_av"
-          ) {
-            // 创作推广-大视频广告
-            return false;
-          }
-        }
-        return true;
-      });
+  } else if (url.includes("/x/v2/feed/index?")) {
+    // 推荐去广告，最后问号不能去掉，以免匹配到story模式
+    if (obj.data?.items) {
+      obj.data.items = obj.data.items.filter(
+        (item) =>
+          !item.banner_item &&
+          !item.ad_info &&
+          item.card_goto.includes("ad") &&
+          [
+            "small_cover_v2",
+            "large_cover_v1",
+            "large_cover_single_v9"
+          ].includes(item.card_type)
+      );
+    }
+  } else if (url.includes("/x/v2/feed/index/story")) {
+    if (obj.data?.items) {
+      obj.data.items = obj.data.items.filter(
+        (item) => !item.ad_info && item.card_goto.includes("ad")
+      );
     }
   } else if (url.includes("/x/v2/search/square")) {
     // 哔哩哔哩-热搜广告
-    delete obj.data;
+    obj.data = {
+      type: "history",
+      title: "搜索历史",
+      search_hotword_revision: 2
+    };
+    // delete obj.data;
   } else if (url.includes("/x/v2/splash")) {
     // 哔哩哔哩-开屏广告
-    if (obj.data.show) delete obj.data.show;
-  } else if (url.includes("/pgc/page/cinema/tab")) {
+    if (obj.data.show) {
+      delete obj.data.show;
+    }
+  } else if (
+    url.includes("/pgc/page/bangumi") ||
+    url.includes("/pgc/page/cinema/tab")
+  ) {
     // 哔哩哔哩-观影页广告
-    if (obj.result && obj.result.modules) {
-      obj.result.modules?.forEach((module) => {
-        // 头部banner
+    if (obj.result?.modules) {
+      obj.result.modules.forEach((module) => {
         if (module.style.startsWith("banner")) {
-          module.items = module.items.filter(
-            (i) => !(i.source_content && i.source_content.ad_content)
+          module.items = module.items.filter((i) => i.link.includes("play"));
+        } else if (module.style.startsWith("function")) {
+          module.items = module.items.filter((i) =>
+            i.blink.startsWith("bilibili")
           );
+        } else if (module.style.startsWith("tip")) {
+          module.items = [];
         }
       });
     }
   } else if (url.includes("/xlive/app-room/v1/index/getInfoByRoom")) {
     // 哔哩哔哩-直播广告
-    if (obj.data.activity_banner_info)
-      obj["data"]["activity_banner_info"] = null;
+    if (obj.data?.activity_banner_info) {
+      obj.data.activity_banner_info = null;
+    }
+    if (obj.data?.shopping_info) {
+      obj.data.shopping_info = { is_show: 0 };
+    }
+    if (
+      obj.data?.new_tab_info?.outer_list &&
+      obj.data?.new_tab_info?.outer_list.length > 0
+    ) {
+      obj.data.new_tab_info.outer_list =
+        obj.data.new_tab_info.outer_list.filter((i) => i.biz_id !== 33);
+    }
   }
 }
 

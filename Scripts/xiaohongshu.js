bb16329cd56a8f3d1c84ca74e5a3bd6b36c21b19
Cube <49463634+RuCu6@users.noreply.github.com>
2023-01-28 13:19:57 +0800

    Update xiaohongshu.js

--- Scripts/xiaohongshu.js
+++ Scripts/xiaohongshu.js
@@ -1,4 +1,4 @@
-// 2023-01-28 12:33
+// 2023-01-28 13:20
 
 if (!$response.body) $done({});
 const url = $request.url;
@@ -29,9 +29,22 @@ if (obj.data) {
   } else if (url.includes("/v6/homefeed")) {
     // 小红书-信息流广告
     if (obj.data) {
-      obj.data = obj.data.filter(
-        (d) => !d.model_type === "live_v2" && !d.ads_info && !d.card_icon
-      );
+      let newItems = [];
+      for (let item of obj.data) {
+        // 去除直播
+        if (item.model_type === "live_v2") {
+          continue;
+          // 去除赞助,带货
+        } else if (item.ads_info) {
+          continue;
+          // 去除带货
+        } else if (item.card_icon) {
+          continue;
+        } else {
+          newItems.push(item);
+        }
+      }
+      obj.data = newItems;
     }
   }
 }

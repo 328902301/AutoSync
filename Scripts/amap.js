c96d21b799a0dddf847599a6595bcbe3bfbbde01
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-29 11:25:59 +0000

    Replace amap.js

--- Scripts/amap.js
+++ Scripts/amap.js
@@ -103,7 +103,7 @@ if (obj.data) {
       // "feedback" // 问题反馈
     ];
     if (obj.data.modules) {
-      obj.data.modules = obj.data.modules.filter((i) => !i.includes(item));
+      obj.data.modules = obj.data.modules.filter((i) => i.indexOf(item) === -1);
     }
   } else if (url.includes("/valueadded/alimama/splash_screen")) {
     // 开屏广告

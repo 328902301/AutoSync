d4b7eab47015f2413baa0689c2e96060ef9a3d8a
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-01-30 11:58:04 +0000

    Replace adrive.js

--- Scripts/adrive.js
+++ Scripts/adrive.js
@@ -1,4 +1,4 @@
-// 2023-01-30 19:48
+// 2023-01-30 19:58
 
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
@@ -7,7 +7,7 @@ let item = [
   "recentUsed", // 最近在看
   "coreFeatures", // 顶部图标
   "activities", // 精选活动
-  "myBackup", // 我的备份
+  // "myBackup", // 我的备份
   // "recentSaved", // 最近转存
   "signIn" // 顶部签到
 ];

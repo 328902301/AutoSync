ba46081bc4a0230f2a675a5f244233d7c2978010
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-02-04 13:27:37 +0000

    Update weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -1,5 +1,5 @@
 // https://github.com/zmqcherish/proxy-script/blob/main/weibo_main.js
-// 2023-02-04 16:35
+// 2023-02-04 21:28
 
 // 屏蔽用户id获取方法
 // 进入用户主页 选择复制链接 得到类似 `https://weibo.com/u/xxx` 的文本 xxx即为用户id 多个id用英文逗号 `,` 分开

07b5508531a7aed3abb1412d319ae97424f1ad78
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-02-04 02:54:37 +0000

    Replace weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -4,7 +4,7 @@ if (!$response.body) $done({});
 const url = $request.url;
 
 if (url.includes("/interface/sdk/sdkad.php")) {
-  let obj = JSON.parse(body.substring(0, body.length - 2));
+  let obj = JSON.parse($response.body.substring(0, body.length - 2));
   if (obj.needlocation) {
     obj.needlocation = false;
   }
@@ -40,6 +40,15 @@ if (url.includes("/interface/sdk/sdkad.php")) {
   // 首页签到
   if (url.includes("/2/checkin/show")) {
     obj.show = 0;
+  } else if (url.includes("/wbapplua/wbpullad.lua")) {
+    if (obj.cached_ad.ads) {
+      for (let item of obj.cached_ad.ads) {
+        item.start_date = 2208960000; // Unix 时间戳 2040-01-01 00:00:00
+        item.show_count = 0;
+        item.duration = 31536000; // 60 * 60 * 24 * 365 = 31536000
+        item.end_date = 2209046399; // Unix 时间戳 2040-01-01 23:59:59
+      }
+    }
   }
 }
 

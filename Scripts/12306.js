5248bd12799ac136c6b7db6014de60dca31a4948
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-02-06 00:38:33 +0000

    Update 12306.js

--- Scripts/12306.js
+++ Scripts/12306.js
@@ -7,10 +7,8 @@ let obj = JSON.parse($response.body);
 if (url.includes("/ad.12306.cn/ad/ser/getAdList")) {
   if (obj.materialsList) {
     if (obj.advertParam && obj.materialsList.length === 1) {
-      obj.materialsList[0].billId = "1000005";
-      obj.materialsList[0].billMaterialsId = "2000005";
-      obj.materialsList[0].filePath = "";
-      obj.advertParam.skipTime = 1;
+      obj.materialsList[0].filePath = "h";
+      obj.advertParam.skipTime = 0;
     } else if (obj.materialsList.length > 1) {
       obj.materialsList = [];
     }

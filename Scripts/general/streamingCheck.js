82e260b640c8af43c3c54f7b080344ae20c21d41
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-02-11 01:29:18 +0000

    Replace streamingCheck.js

--- Scripts/general/streamingCheck.js
+++ Scripts/general/streamingCheck.js
@@ -1,13 +1,8 @@
 /***
 引用地址 https://github.com/KOP-XIAO/QuantumultX/blob/master/Scripts/streaming-ui-check.js
 
-Thanks to & modified from 
-1. https://gist.githubusercontent.com/Hyseen/b06e911a41036ebc36acf04ddebe7b9a/raw/nf_check.js
-2. https://github.com/AtlantisGawrGura/Quantumult-X-Scripts/blob/main/media.js
-3. https://github.com/CoiaPrant/MediaUnlock_Test/blob/main/check.sh
-
 For Quantumult-X 598+ ONLY!!
-2022-02-22 - 14:20
+2022-12-16
 
 [task_local]
 event-interaction https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/streaming-ui-check.js, tag=流媒体-解锁查询, img-url=checkmark.seal.system, enabled=true
@@ -18,12 +13,13 @@ const BASE_URL_YTB = "https://www.youtube.com/premium";
 const BASE_URL_DISNEY = "https://www.disneyplus.com";
 const BASE_URL_Dazn = "https://startup.core.indazn.com/misl/v5/Startup";
 const BASE_URL_Param = "https://www.paramountplus.com/";
-const FILM_ID = 81215567;
+const FILM_ID = 80062035;
 const BASE_URL_Discovery_token =
   "https://us1-prod-direct.discoveryplus.com/token?deviceId=d1a4a5d25212400d1e6985984604d740&realm=go&shortlived=true";
 const BASE_URL_Discovery = "https://us1-prod-direct.discoveryplus.com/users/me";
 const link = {
-  "media-url": "https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/img/southpark/7.png"
+  "media-url":
+    "https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/img/southpark/7.png"
 };
 const policy_name = "Netflix"; //填入你的 netflix 策略组名
 const arrow = " ➟ ";
@@ -180,7 +176,6 @@ let result = {
   Discovery: "<b>Discoveryᐩ: </b>检测失败，请重试 ❗️"
   //"Google": "Google 定位: 检测失败，请重试"
 };
-
 const message = {
   action: "get_policy_state",
   content: $environment.params
@@ -195,22 +190,30 @@ const message = {
     testNf(FILM_ID),
     testDiscovery()
   ]);
-  //console.log(result["Netflix"])
-  //console.log(`testDisneyPlus: region=${region}, status=${status}`)
+  // console.log("NetFlix Result:"+result["Netflix"])
+  // console.log(`testDisneyPlus: region=${region}, status=${status}`)
   if (status == STATUS_COMING) {
-    ////console.log(1)
+    // console.log(1)
     result["Disney"] =
-      "<b>Disneyᐩ:</b> 即将登陆 ➟ " + "⟦" + flags.get(region.toUpperCase()) + "⟧ ⚠️";
+      "<b>Disneyᐩ:</b> 即将登陆 ➟ " +
+      "⟦" +
+      flags.get(region.toUpperCase()) +
+      "⟧ ⚠️";
   } else if (status == STATUS_AVAILABLE) {
-    ////console.log(2)
-    result["Disney"] = "<b>Disneyᐩ:</b> 支持 ➟ " + "⟦" + flags.get(region.toUpperCase()) + "⟧ 🎉";
-    //console.log(result["Disney"])
+    // console.log(2)
+    result["Disney"] =
+      "<b>Disneyᐩ:</b> 支持 ➟ " +
+      "⟦" +
+      flags.get(region.toUpperCase()) +
+      "⟧ 🎉";
+    // console.log(result["Disney"])
   } else if (status == STATUS_NOT_AVAILABLE) {
-    ////console.log(3)
+    // console.log(3)
     result["Disney"] = "<b>Disneyᐩ:</b> 未支持 🚫 ";
   } else if (status == STATUS_TIMEOUT) {
     result["Disney"] = "<b>Disneyᐩ:</b> 检测超时 🚦 ";
   }
+
   let content =
     "------------------------------" +
     "</br>" +
@@ -237,7 +240,7 @@ const message = {
   $configuration.sendMessage(message).then(
     (resolve) => {
       if (resolve.error) {
-        //console.log(resolve.error);
+        // console.log(resolve.error);
         $done();
       }
       if (resolve.ret) {
@@ -268,7 +271,7 @@ const message = {
           content +
           `</p>`;
         //$notify(typeof(output),output)
-        //console.log(output);
+        // console.log(output);
         $done({ title: result["title"], htmlMessage: content });
       }
       //$done();|
@@ -283,7 +286,7 @@ const message = {
   $configuration.sendMessage(message).then(
     (resolve) => {
       if (resolve.error) {
-        //console.log(resolve.error);
+        // console.log(resolve.error);
         $done();
       }
       if (resolve.ret) {
@@ -314,7 +317,7 @@ const message = {
           content +
           `</p>`;
         //$notify(typeof(output),output)
-        //console.log(output);
+        // console.log(output);
         $done({ title: result["title"], htmlMessage: content });
       }
       //$done();|
@@ -324,6 +327,7 @@ const message = {
       $done();
     }
   );
+
   $done({
     title: result["title"],
     htmlMessage:
@@ -339,18 +343,19 @@ const message = {
 async function testDisneyPlus() {
   try {
     let { region, cnbl } = await Promise.race([testHomePage(), timeout(7000)]);
-    //console.log(`homepage: region=${region}, cnbl=${cnbl}`)
+    // console.log(`homepage: region=${region}, cnbl=${cnbl}`)
     // 即将登陆
     //  if (cnbl == 2) {
     //    return { region, status: STATUS_COMING }
     //  }
-    let { countryCode, inSupportedLocation } = await Promise.race([
+    let { countryCode, inSupportedLocation, accessToken } = await Promise.race([
       getLocationInfo(),
       timeout(7000)
     ]);
-    //console.log(`getLocationInfo: countryCode=${countryCode}, inSupportedLocation=${inSupportedLocation}`)
+    // console.log(`getLocationInfo: countryCode=${countryCode}, inSupportedLocation=${inSupportedLocation}`)
+
     region = countryCode ?? region;
-    //console.log( "region:"+region)
+    // console.log( "region:"+region)
     // 即将登陆
     if (inSupportedLocation === false || inSupportedLocation === "false") {
       return { region, status: STATUS_COMING };
@@ -358,17 +363,30 @@ async function testDisneyPlus() {
       // 支持解锁
       return { region, status: STATUS_AVAILABLE };
     }
-  } catch (error) {
-    //console.log("error:"+error)
-    // 不支持解锁
-    if (error === "Not Available") {
-      //console.log("不支持")
+
+    let support = await Promise.race([
+      testPublicGraphqlAPI(accessToken),
+      timeout(7000)
+    ]);
+    if (!support) {
       return { status: STATUS_NOT_AVAILABLE };
     }
+    // 支持解锁
+    return { region, status: STATUS_AVAILABLE };
+  } catch (error) {
+    // console.log("error:"+error)
+
+    // 不支持解锁
+    if (error === "Not Available") {
+      // console.log("不支持")
+      return { status: STATUS_NOT_AVAILABLE };
+    }
+
     // 检测超时
     if (error === "Timeout") {
       return { status: STATUS_TIMEOUT };
     }
+
     return { status: STATUS_ERROR };
   }
 }
@@ -381,7 +399,8 @@ function getLocationInfo() {
       opts: opts,
       headers: {
         "Accept-Language": "en",
-        Authorization: "ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84",
+        Authorization:
+          "ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84",
         "Content-Type": "application/json",
         "User-Agent": UA
       },
@@ -394,15 +413,15 @@ function getLocationInfo() {
             attributes: {
               browserName: "chrome",
               browserVersion: "94.0.4606",
-              manufacturer: "microsoft",
+              manufacturer: "apple",
               model: null,
-              operatingSystem: "windows",
-              operatingSystemVersion: "10.0",
+              operatingSystem: "macintosh",
+              operatingSystemVersion: "10.15.7",
               osDeviceIds: []
             },
             deviceFamily: "browser",
             deviceLanguage: "en",
-            deviceProfile: "windows"
+            deviceProfile: "macosx"
           }
         }
       })
@@ -411,17 +430,20 @@ function getLocationInfo() {
     $task.fetch(opts0).then(
       (response) => {
         let data = response.body;
-        //console.log("locationinfo:"+response.statusCode)
+        // console.log("locationinfo:"+response.statusCode)
         if (response.statusCode !== 200) {
-          //console.log('getLocationInfo: ' + data)
+          // console.log('getLocationInfo: ' + data)
           reject("Not Available");
           return;
         } else {
           let {
-            inSupportedLocation,
-            location: { countryCode }
-          } = JSON.parse(data)?.extensions?.sdk?.session;
-          resolve({ inSupportedLocation, countryCode });
+            token: { accessToken },
+            session: {
+              inSupportedLocation,
+              location: { countryCode }
+            }
+          } = JSON.parse(data)?.extensions?.sdk;
+          resolve({ inSupportedLocation, countryCode, accessToken });
         }
       },
       (reason) => {
@@ -445,8 +467,11 @@ function testHomePage() {
     $task.fetch(opts0).then(
       (response) => {
         let data = response.body;
-        //console.log("homepage"+response.statusCode)
-        if (response.statusCode !== 200 || data.indexOf("unavailable") !== -1) {
+        // console.log("DisneyPlus: homepage"+response.statusCode)
+        if (
+          response.statusCode !== 200 ||
+          data.indexOf("not available in your region") !== -1
+        ) {
           reject("Not Available");
           return;
         } else {
@@ -457,10 +482,40 @@ function testHomePage() {
           } else {
             let region = match[1];
             let cnbl = match[2];
-            ////console.log("homepage"+region+cnbl)
+            // console.log("homepage"+region+cnbl)
             resolve({ region, cnbl });
           }
         }
+      },
+      (reason) => {
+        reject("Error");
+        return;
+      }
+    );
+  });
+}
+
+function testPublicGraphqlAPI(accessToken) {
+  return new Promise((resolve, reject) => {
+    let opts = {
+      url: "https://disney.api.edge.bamgrid.com/v1/public/graphql",
+      headers: {
+        "Accept-Language": "en",
+        Authorization: accessToken,
+        "Content-Type": "application/json",
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"
+      },
+      body: JSON.stringify({
+        query:
+          "query($preferredLanguages: [String!]!, $version: String) {globalization(version: $version) { uiLanguage(preferredLanguages: $preferredLanguages) }}",
+        variables: { version: "1.5.0", preferredLanguages: ["en"] }
+      })
+    };
+
+    $task.fetch(opts).then(
+      (response) => {
+        resolve(response.status === 200);
       },
       (reason) => {
         reject("Error");
@@ -492,16 +547,16 @@ function testNf(filmId) {
     $task.fetch(option).then(
       (response) => {
         //$notify("nf:"+response.statusCode)
-        //console.log("nf:"+response.statusCode)
+        // console.log("nf:"+response.statusCode)
         if (response.statusCode === 404) {
           result["Netflix"] = "<b>Netflix: </b>支持自制剧集 ⚠️";
-          //console.log("nf:"+result["Netflix"])
+          // console.log("nf:"+result["Netflix"])
           resolve("Not Found");
           return;
         } else if (response.statusCode === 403) {
-          ////console.log("nfnf")
+          // console.log("nfnf")
           result["Netflix"] = "<b>Netflix: </b>未支持 🚫";
-          //console.log("nf:"+result["Netflix"])
+          // console.log("nf:"+result["Netflix"])
           //$notify("nf:"+result["Netflix"])
           resolve("Not Available");
           return;
@@ -512,9 +567,13 @@ function testNf(filmId) {
           if (region == "title") {
             region = "us";
           }
-          //console.log("nf:"+region)
+          // console.log("nf:"+region)
           result["Netflix"] =
-            "<b>Netflix: </b>完整支持" + arrow + "⟦" + flags.get(region.toUpperCase()) + "⟧ 🎉";
+            "<b>Netflix: </b>完整支持" +
+            arrow +
+            "⟦" +
+            flags.get(region.toUpperCase()) +
+            "⟧ 🎉";
           //$notify("nf:"+result["Netflix"])
           resolve("nf:" + result["Netflix"]);
           return;
@@ -522,7 +581,7 @@ function testNf(filmId) {
       },
       (reason) => {
         result["Netflix"] = "<b>Netflix: </b>检测超时 🚦";
-        //console.log(result["Netflix"])
+        // console.log(result["Netflix"])
         resolve("timeout");
       }
     );
@@ -542,15 +601,19 @@ function testYTB() {
   $task.fetch(option).then(
     (response) => {
       let data = response.body;
-      //console.log("ytb:"+response.statusCode)
+      // console.log("ytb:"+response.statusCode)
       if (response.statusCode !== 200) {
         //reject('Error')
         result["YouTube"] = "<b>YouTube Premium: </b>检测失败 ❗️";
-      } else if (data.indexOf("Premium is not available in your country") !== -1) {
+      } else if (
+        data.indexOf("Premium is not available in your country") !== -1
+      ) {
         //resolve('Not Available')
         result["YouTube"] = "<b>YouTube Premium: </b>未支持 🚫";
-      } else if (data.indexOf("Premium is not available in your country") == -1) {
-        ////console.log(data.split("countryCode")[1])
+      } else if (
+        data.indexOf("Premium is not available in your country") == -1
+      ) {
+        // console.log(data.split("countryCode")[1])
         let region = "";
         let re = new RegExp('"GL":"(.*?)"', "gm");
         let ret = re.exec(data);
@@ -563,8 +626,12 @@ function testYTB() {
         }
         //resolve(region)
         result["YouTube"] =
-          "<b>YouTube Premium: </b>支持 " + arrow + "⟦" + flags.get(region.toUpperCase()) + "⟧ 🎉";
-        //console.log("ytb:"+region+ result["YouTube"])
+          "<b>YouTube Premium: </b>支持 " +
+          arrow +
+          "⟦" +
+          flags.get(region.toUpperCase()) +
+          "⟧ 🎉";
+        // console.log("ytb:"+region+ result["YouTube"])
       }
     },
     (reason) => {
@@ -601,27 +668,31 @@ function testDazn() {
       let data = response.body;
       //data = extra
       let header = JSON.stringify(response.headers);
-      //console.log("Dazn:"+response.statusCode)
-      ////console.log("Dazn:"+data)
+      // console.log("Dazn:"+response.statusCode)
+      // console.log("Dazn:"+data)
       //$done(data)
       if (response.statusCode !== 200) {
         //reject('Error')
         result["Dazn"] = "<b>Dazn: </b>检测失败 ❗️";
       } else if (response.statusCode == 200) {
-        ////console.log(data.split("countryCode")[1])
-        ////console.log(data)
+        // console.log(data.split("countryCode")[1])
+        // console.log(data)
         let region = "";
         let re = new RegExp('"GeolocatedCountry":"(.*?)"', "gm");
         let ret = re.exec(data);
         if (ret != null && ret.length === 2) {
           region = ret[1];
           result["Dazn"] =
-            "<b>Dazn: </b>支持 " + arrow + "⟦" + flags.get(region.toUpperCase()) + "⟧ 🎉";
+            "<b>Dazn: </b>支持 " +
+            arrow +
+            "⟦" +
+            flags.get(region.toUpperCase()) +
+            "⟧ 🎉";
         } else {
           result["Dazn"] = "<b>Dazn: </b>未支持 🚫";
         }
         //resolve(region)
-        //console.log("Dazn:"+region+ result["Dazn"])
+        // console.log("Dazn:"+region+ result["Dazn"])
       }
     },
     (reason) => {
@@ -644,7 +715,7 @@ function testParam() {
   $task.fetch(option).then(
     (response) => {
       //let data = response.body
-      //console.log("Paramountᐩ:"+response.statusCode)
+      // console.log("Paramountᐩ:"+response.statusCode)
       if (response.statusCode == 200) {
         //reject('Error')
         result["Paramount"] = "<b>Paramountᐩ: </b>支持 🎉 ";
@@ -652,7 +723,7 @@ function testParam() {
         //resolve('Not Available')
         result["Paramount"] = "<b>Paramountᐩ: </b>未支持 🚫";
       }
-      //console.log("Paramountᐩ:"+ result["Paramount"])
+      // console.log("Paramountᐩ:"+ result["Paramount"])
     },
     (reason) => {
       result["Paramount"] = "<b>Paramountᐩ: </b>检测超时 🚦";
@@ -675,10 +746,10 @@ function testDiscovery() {
     };
     $task.fetch(option).then(
       (response) => {
-        //console.log("GetToken:"+response.statusCode)
+        // console.log("GetToken:"+response.statusCode)
         if (response.statusCode == 200) {
           let data = JSON.parse(response.body);
-          //console.log(data)
+          // console.log(data)
           let token = data["data"]["attributes"]["token"];
           const cookievalid = `_gcl_au=1.1.858579665.1632206782; _rdt_uuid=1632206782474.6a9ad4f2-8ef7-4a49-9d60-e071bce45e88; _scid=d154b864-8b7e-4f46-90e0-8b56cff67d05; _pin_unauth=dWlkPU1qWTRNR1ZoTlRBdE1tSXdNaTAwTW1Nd0xUbGxORFV0WWpZMU0yVXdPV1l6WldFeQ; _sctr=1|1632153600000; aam_fw=aam%3D9354365%3Baam%3D9040990; aam_uuid=24382050115125439381416006538140778858; st=${token}; gi_ls=0; _uetvid=a25161a01aa711ec92d47775379d5e4d; AMCV_BC501253513148ED0A490D45%40AdobeOrg=-1124106680%7CMCIDTS%7C18894%7CMCMID%7C24223296309793747161435877577673078228%7CMCAAMLH-1633011393%7C9%7CMCAAMB-1633011393%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1632413793s%7CNONE%7CvVersion%7C5.2.0; ass=19ef15da-95d6-4b1d-8fa2-e9e099c9cc38.1632408400.1632406594`;
           let option1 = {
@@ -695,33 +766,34 @@ function testDiscovery() {
           };
           $task.fetch(option1).then(
             (response) => {
-              //console.log("Check:"+response.statusCode)
+              // console.log("Discovery+ Check:"+response.statusCode)
               let data = JSON.parse(response.body);
-              let locationd = data["data"]["attributes"]["currentLocationTerritory"];
+              let locationd =
+                data["data"]["attributes"]["currentLocationTerritory"];
               if (locationd == "us") {
                 result["Discovery"] = "<b>Discoveryᐩ: </b>支持 🎉 ";
-                //console.log("支持Discoveryᐩ")
+                // console.log("支持Discoveryᐩ")
                 resolve("支持Discoveryᐩ");
                 return;
               } else {
                 result["Discovery"] = "<b>Discoveryᐩ: </b>未支持 🚫";
-                //console.log("不支持Discoveryᐩ")
+                // console.log("不支持Discoveryᐩ")
                 resolve("不支持Discoveryᐩ");
                 return;
               }
             },
             (reason) => {
-              //console.log("Check-Error"+reason)
+              // console.log("Check-Error"+reason)
               resolve("discovery failed");
             }
           );
         } else {
-          //console.log("GetToken-Error"+reason)
+          // console.log("GetToken-Error"+reason)
           resolve("discovery failed");
         }
       },
       (reason) => {
-        //console.log("GetToken-Error"+reason)
+        // console.log("GetToken-Error"+reason)
         resolve("discovery failed");
       }
     );

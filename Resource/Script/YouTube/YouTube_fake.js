7493a56cfc053491496b1165844b1b02424a20bd
lodepuly <admin@qq.com>
2023-02-11 21:34:28 +0800

    删除旧版YouTube去广告

--- Resource/Script/YouTube/YouTube_fake.js
+++
@@ -1,8 +0,0 @@
-/*
-脚本作者：Maasea
-引用地址：https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/youtube-fake.js
-*/
-let statusCode = 204
-const isQuanX = typeof $task !== "undefined";
-if (isQuanX) statusCode = "HTTP/1.1 204 No Content"
-$done({ status: statusCode })

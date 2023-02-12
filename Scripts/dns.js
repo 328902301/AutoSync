3c776030828489d3f37b1ecb8a4e191106214029
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-02-12 00:38:31 +0000

    Replace dns.js

--- Scripts/dns.js
+++ Scripts/dns.js
@@ -1,9 +1,9 @@
 /*
 编辑配置文件，在 `[task_local]` 处添加此定时脚本
-每天21点清理一次qx的dns缓存
+每天4点清理一次qx的dns缓存
 
 [task_local]
-0 21 * * * https://gitlab.com/RuCu6/QuanX/-/raw/main/Scripts/dns.js, tag=Clean DNS Cache, img-url=https://gitlab.com/RuCu6/QuanX/-/raw/main/Icons/app/qx.png, enabled=true
+0 0 4 * * * https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/dns.js, tag=Clean DNS Cache, img-url=https://raw.githubusercontent.com/RuCu6/QuanX/main/Icons/app/qx.png, enabled=true
 */
 
 const message = {

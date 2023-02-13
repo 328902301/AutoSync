9101400703f2454059da42051aed63b796e8658b
RuCu6 <12550983-RuCu6@users.noreply.gitlab.com>
2023-02-13 09:29:29 +0000

    Update weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -1,4 +1,4 @@
-// 2023-02-13 17:25
+// 2023-02-13 17:30
 
 const url = $request.url;
 if (!$response.body) $done({});
@@ -92,17 +92,19 @@ if (url.includes("/interface/sdk/sdkad.php")) {
           if (!isAd(item.data)) {
             // 超话社区,头像挂件,勋章,评论气泡
             if (item.data.user) {
-              if (
-                item.data.user.name === "超话社区" ||
-                item.data.user.name === "微博视频"
-                ) {
-                continue;
-              }
               removeAvatar(item.data.user);
             }
             // 评论气泡
             if (item.data?.comment_bubble) {
               delete item.data.comment_bubble;
+            }
+            // 微博伪装评论,过滤不当言论提示
+            if (
+              item.data.user.name === "超话社区" ||
+              item.data.user.name === "微博视频" ||
+              item.type === 6
+              ) {
+              continue;
             }
             newItems.push(item);
           }

e36876e75f2e2a1049142132c662ccfbcf5c9601
king <77053523+nameking77@users.noreply.github.com>
2023-02-14 15:34:01 +0800

    Create Keep.js

---
+++ Keep.js
@@ -0,0 +1,22 @@
+
+/*
+ *
+ *
+脚本功能：keep
+脚本作者：King
+网站地址：
+更新时间：2023.2.14
+脚本发布：https://t.me/yqc_123
+问题反馈：https://t.me/yqc_777
+使用声明：⚠️此脚本仅供学习与交流，
+        请勿转载与贩卖！⚠️⚠️⚠️
+********************************
+[rewrite_local]
+#antv
+^http[s]?:\/\/api.gotokeep.com.*$ url script-request-header https://raw.githubusercontent.com/nameking77/Qx/main/Keep.js
+[mitm] 
+hostname = 
+
+
+
+*/
